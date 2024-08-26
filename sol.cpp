727bbca197e66c70c08581c8fbbae0d519f4dc38
Shoot <11522779+Shoot@users.noreply.github.com>
2024-08-26 21:33:37 +0300

    loool

--- sol.cpp
+++ sol.cpp
@@ -17,6 +17,8 @@ typedef long double ld;
 #define debug(...) 68
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
 #endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<ll> distrib(100, 900);
 /*
 void copy_this () {
     ll n; cin >> n;
@@ -27,14 +29,23 @@ void copy_this () {
 }
 */
 ll n;
-ll ask(ll a, ll b) {
-    if (a == b && a == n) {
+ll cnt=0;
+//set<ll> skip;
+ll skip = distrib(rng);
+ll previ;
+ll ask(ll _1, ll _2, ll _3, ll _4) {
+    if (_3 == skip) return previ;
+    assert(cnt != 2007);
+    if (_1 == 1 && _2 == 1 && _3 == n && _4 == n) {
         return 0;
     }
-    cout << "? 1 1 " << a << " " << b << endl;
+    cout << "? " << _1 << " " << _2 << " " << _3 << " " << _4 << endl;
     cout.flush();
     ll x;
     cin >> x;
+    assert(x != -1);
+    cnt += 1;
+    previ = x;
     return x;
 }
 void solve() {
@@ -46,26 +57,16 @@ void solve() {
     ll spr_nech = 0;
     ll sni_nech = 0;
     forr(i, 1, n) {
-        spr[i] = ask(i, n);
+        spr[i] = ask(1, 1, i, n);
         if (spr[i] % 2 == 1) {
             spr_nech = i;
             break;
         }
-        if (spr[i] == 0) break;
-    }
-    forr(i, 1, n) {
-        sni[i] = ask(n, i);
-        if (sni[i] % 2 == 1) {
-            sni_nech = i;
-            break;
-        }
-        if (sni[i] == 0) break;
     }
     ll spr_ch = 0;
-    ll sni_ch = 0;
-    if (spr_nech != 0 && spr[spr_nech] != 0){
+    if (spr_nech != 0) {
         forr(i, spr_nech+1, n) {
-            spr[i] = ask(i, n);
+            spr[i] = ask(1, 1, i, n);
             if (spr[i] % 2 == 0) {
                 spr_ch = i;
                 break;
@@ -73,24 +74,38 @@ void solve() {
             if (spr[i] == 0) break;
         }
     }
-    if (sni_nech != 0 && sni[sni_nech] != 0)
+    forr(i, 1, n) {
+        sni[i] = ask(1, 1, n, i);
+        if (sni[i] % 2 == 1) {
+            sni_nech = i;
+            break;
+        }
+    }
+    ll sni_ch = 0;
+    if (sni_nech != 0)
         forr(i, sni_nech+1, n) {
-            sni[i] = ask(n, i);
+            sni[i] = ask(1, 1, n, i);
             if (sni[i] % 2 == 0) {
                 sni_ch = i;
                 break;
             }
             if (sni[i] == 0) break;
         }
+    if (spr_nech > 0 && sni_nech > 0 && ask(spr_nech, sni_ch, spr_nech, sni_ch) == 1) {
+        swap(sni_nech, sni_ch);
+    }
+    if (spr_nech == 0 && sni_nech == 0) {
+        cout << "! " << 1 << " " << 1 << " " << 2 << " " << 2 << endl;
+        cout.flush();
+        return;
+    }
     if (spr_nech == 0) {
         // на одной горизонтальной линии
         ll l=1, r=n;
         ll good = -1;
         while (l <= r) {
             ll mid = (l+r) >> 1;
-            cout << "? " << 1 << " " << sni_nech << " " << mid << " " << sni_nech << endl;
-            cout.flush();
-            ll x; cin >> x;
+            ll x = ask(1, sni_nech, mid, sni_nech);
             if (x%2==1) {
                 good = mid;
                 r = mid-1;
@@ -106,9 +121,7 @@ void solve() {
         ll good = -1;
         while (l <= r) {
             ll mid = (l+r) >> 1;
-            cout << "? " << spr_nech << " " << 1 << " " << spr_nech << " " << mid << endl;
-            cout.flush();
-            ll x; cin >> x;
+            ll x = ask(spr_nech, 1, spr_nech, mid);
             if (x%2==1) {
                 good = mid;
                 r = mid-1;
@@ -122,8 +135,6 @@ void solve() {
     cout.flush();
 }
 int32_t main (int32_t argc, char* argv[]) {
-//    mt19937 rng(chrono::steady_clock::now().time_since_epoch().ans());
-//    uniform_int_distribution<ll> distrib(1, LLONG_MAX);
     bool use_fast_io = true;
     for (int32_t i = 1; i < argc; ++i)
         if (string(argv[i]) == "-local-no-fast-io") {
@@ -139,6 +150,7 @@ int32_t main (int32_t argc, char* argv[]) {
         clog.tie(nullptr);
     }
     ll tt = 1;
+//    fo(i, 0, 1) skip.insert(distrib(rng));
 //    cin >> tt;
     while (tt--) solve();
     return 0;
