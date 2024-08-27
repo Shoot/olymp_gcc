4f6fa867df42602d697af1b1d5b6e6260ff1e6c8
Shoot <11522779+Shoot@users.noreply.github.com>
2024-08-27 21:43:26 +0300

    pos (task #4)

--- sol.cpp
+++ sol.cpp
@@ -30,127 +30,68 @@ void copy_this () {
     vector<ll> a(n); fo(i, 0, n) cin >> a[i];
 }
 */
-struct Point {
-    ll x, y;
-    Point() {
-        x = 0;
-        y = 0;
-    }
-};
-struct Seg {
-    Point a, b;
-    Seg(Point a, Point b) {
-        this->a = a;
-        this->b = b;
-    }
-    Seg() = default;
-};
+
 void solve () {
     ll n; cin >> n;
-    vector<Point> ps(n);
-    vector<Seg> ss(n);
-    fo(i, 0, n) {
-        cin >> ps[i].x >> ps[i].y;
-    }
-    fo(i, 0, n) {
-        ss[i].a = ps[i];
-        ss[i].b = ps[(i+1)%n];
-    }
-    ll mini = LLONG_MAX;
-    ll mini_i = -1;
-    fo(i, 0, n) {
-        if (ss[i].a.x < mini) {
-            mini_i = i;
-            mini = ss[i].a.x;
+    vector<vector<ll>> sm (n+1);
+    forr(i, 1, n) {
+        string s;
+        cin >> s;
+        forr(j, i+1, s.size()) {
+            if (s[j-1] == '1') {
+                sm[i].push_back(j);
+                sm[j].push_back(i);
+            }
         }
     }
-    rotate(ss.begin(), ss.begin()+mini_i, ss.end());
-    for (Seg _: ss) {
-        clog << _.a.x << "," << _.a.y << " -> " << _.b.x << "," << _.b.y << endl;
+    ll maxi = 0;
+    unordered_map<ll, ll> cnt;
+    forr(i, 1, n){
+        maxi = max(maxi, (ll)sm[i].size());
+        cnt[(ll)sm[i].size()]+=1;
     }
-    ll mnozh = 0;
-    double tg_first = (double)(ss[0].b.y-ss[0].a.y)/((double)(ss[0].b.x-ss[0].a.x) + 1e-8);
-    double tg_last = (double)(ss[n-1].a.y-ss[n-1].b.y)/((double)(ss[n-1].a.x-ss[n-1].b.x) + 1e-8);
-    double diff = tg_first-tg_last;
-    clog << "Tangs: " << tg_first  << " " <<  tg_last << endl;
-    assert(abs(diff) > 1e-8);
-    if (diff > 0) {
-        mnozh = 1;
-    } else {
-        mnozh = -1;
+    if (maxi > 2) {
+        cout << -1 << endl;
+        return;
     }
-    assert(mnozh == -1);
-    clog << "mnozh = " << mnozh << (mnozh == 1 ? " (dx > 0 -> krisha)" : " (dx < 0 -> krisha)") << endl;
-    ll tot = 0;
-    vector<vector<ll>> simplified;
-    vector<pair<bool, bool>> predsledy;
-    fo(i, 0, n) {
-        clog << ss[i].a.x << "," << ss[i].a.y << " -> " << ss[i].b.x << "," << ss[i].b.y << endl;
-        ll preddx;
-        ll preddy;
-        if (i == 0) preddx = ss[n-1].b.x-ss[n-1].a.x;
-        else preddx = ss[i-1].b.x-ss[i-1].a.x;
-        if (i == 0) preddy = ss[n-1].b.y-ss[n-1].a.y;
-        else preddy = ss[i-1].b.y-ss[i-1].a.y;
-        ll sleddx;
-        ll sleddy;
-        if (i == 0) sleddx = ss[0].b.x-ss[0].a.x;
-        else sleddx = ss[i+1].b.x-ss[i+1].a.x;
-        if (i == n-1) sleddy = ss[0].b.y-ss[0].a.y;
-        else sleddy = ss[i+1].b.y-ss[i+1].a.y;
-        ll dy = ss[i].b.y-ss[i].a.y;
-        ll dx = ss[i].b.x-ss[i].a.x;
-        double tg = (double)dy/((double)dx + 1e-8);
-        if (mnozh*dx < 0) {
-            clog << "prsl: " << preddy << "," << sleddy<< endl;
-            clog << "dno" << endl;
-            if (simplified.empty() || mnozh*preddx >= 0) {
-                double predtg = (double)(preddy)/((double) preddx + 1e-8);
-                clog << "predtg = " << predtg << endl;
-                predsledy.emplace_back(preddy<0 && abs(predtg) > abs(tg), 0);
-                simplified.emplace_back();
-                clog << "+" << endl;
-            }
-            if (dy != 0) {
-                simplified[simplified.size()-1].push_back(dy);
-            }
-            double sledtg = (double)(sleddy)/((double) sleddx + 1e-8);
-            clog << "sledtg = " << sledtg << endl;
-            predsledy[predsledy.size()-1].second = sleddy>0 && abs(sledtg) > abs(tg);
-        } else {
-            clog << "krisha/vert" << endl;
-        }
-    }
-    ll predsledy_i = -1;
-    for (const auto& dno: simplified) {
-        predsledy_i += 1;
-        for (auto edge: dno) {
-            clog << edge << "; ";
-        }
-        if (dno.empty()) {
-            if (predsledy[predsledy_i].first && predsledy[predsledy_i].second) {
-                tot += 1;
-                clog << " --> 1" << endl;
-            } else {
-                clog << " --> 0" << endl;
-            }
-            continue;
+    forr(i, 1, n) {
+        clog << i << ": ";
+        for (ll j: sm[i]) {
+            clog << j << ' ';
         }
         clog << endl;
-        if (predsledy[predsledy_i].first) {
-            tot += dno[0] > 0;
-        }
-        if (predsledy[predsledy_i].second) {
-            tot += dno[dno.size()-1] < 0;
-        }
-        fo(i, 0, dno.size()-1) {
-            if ((dno[i] < 0) && (dno[i+1] > 0)) {
-                tot += 1;
+    }
+    for (auto [i, j]: cnt) {
+        clog << i << " x " << j << endl;
+    }
+    while (cnt[1] > 0) {
+        forr(i, 1, n) {
+            if ((ll)sm[i].size() == 1) {
+                queue<ll> q;
+                q.push(i);
+                while (!q.empty()) {
+                    ll de = q.back(); q.pop();
+                    clog << "new: " << de << endl;
+                    clog << "size: " << (ll)sm[de].size() << endl;
+                    if (sm[de].empty()) continue;
+                    for (ll nxt: sm[de]) {
+                        clog << "nxt: " << nxt << endl;
+                        q.push(nxt);
+                    }
+                    cnt[(ll)sm[de].size()] -= 1;
+                    sm[de].clear();
+                }
             }
         }
     }
-//    assert(tot != 0);
-    cout << max(1ll, tot) << endl;
+    for (auto [i, j]: cnt) {
+        assert(j >= 0);
+    }
+    if (cnt[2] > 0) {
+        cout << -1 << endl;
+    } else {
+        cout << n << endl;
+    }
 }
 int32_t main (int32_t argc, char* argv[]) {
     bool use_fast_io = true;
