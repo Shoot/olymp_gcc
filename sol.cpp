da19f0a97e074dfa44a130c3a47b345c51e719a9
Shoot <11522779+Shoot@users.noreply.github.com>
2024-08-30 22:08:27 +0300

    saaave

--- sol.cpp
+++ sol.cpp
@@ -172,14 +172,14 @@ void solve () {
     if (inds.size() == n) {
         maxi = n;
     }
-    fo(i, 0, n-2) {
-        if (a[i+2]-a[i]>1) {
-            if (pref[i]+suff[i+2]+1 > maxi) {
-                cout << i << ":)" << endl;
-                maxi = pref[i]+suff[i+2]+1;
-            }
-        }
-    }
+//    fo(i, 0, n-2) {
+//        if (a[i+2]-a[i]>1) {
+//            if (pref[i]+suff[i+2]+1 > maxi) {
+//                cout << i << ":)" << endl;
+//                maxi = pref[i]+suff[i+2]+1;
+//            }
+//        }
+//    }
     clog << "maxi: " << maxi << endl;
     cout << maxi << endl;
     ll ogmaxi = 0;
