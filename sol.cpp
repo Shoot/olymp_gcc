a8b97b383394d8c01c59b092095cd096f1407157
Shoot <11522779+Shoot@users.noreply.github.com>
2024-09-01 15:41:05 +0300

    2d to ensure minimum is on requesting side not in requested side

--- sol.cpp
+++ sol.cpp
@@ -19,7 +19,7 @@ typedef long double ld;
 #endif
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 uniform_int_distribution<ll> distrib(1, 11);
-constexpr ll N = (ll)(2005);
+constexpr ll N = (ll)(1 << 3);
 constexpr ll MOD99 = 998244353;
 constexpr ll MOD7 = 1e9 + 7;
 /*
@@ -31,127 +31,81 @@ void copy_this () {
     vector<ll> a(n); fo(i, 0, n) cin >> a[i];
 }
 */
+
 ll n;
 ll x;
-struct shit {
-    ll target, contrib, start;
-};
-void solve () {
-    ll cnt_DELETE = 0;
-    cin >> n >> x;
-    unordered_set<ll> st_DELETE;
-    ll prev_DELETE=0;
-    ll cnt_prev_DELETE=0;
-    ll a[n]; fo(i, 0, n) {
-        cin >> a[i];
-        if (prev_DELETE > a[i]) {
-            cnt_prev_DELETE += 1;
-        }
-        prev_DELETE = a[i];
-    }
-    if (cnt_prev_DELETE > 1e4) {
-        assert(false);
-    }
-    ll b[n]; fo(i, 0, n) cin >> b[i];
-    ll b_pref[n];
-    b_pref[0] = 0;
-    forr(i, 1, n) b_pref[i] = b_pref[i-1]+b[i-1];
-    stack<ll> stck;
-    ll nxt[n];
-    roff(i, n-1, 0) {
-        while(!stck.empty() && a[stck.top()] >= a[i]) {
-            stck.pop();
-        }
-        if (!stck.empty()) {
-            nxt[i] = stck.top()-1;
+ll count(ll l, ll r, const ll a[], const ll b[]) {
+    clog << l << " to " << r << endl;
+    if (l == r) {
+        if (a[l]+b[l] <= x) {
+            clog << l << " to " << r << ": " << 1 << endl;
+            return 1;
         } else {
-            nxt[i] = n - 1;
+            clog << l << " to " << r << ": " << 0 << endl;
+            return 0;
         }
-        stck.push(i);
     }
-//    clog << "array \"nxt min decrease wont be until\": " << endl;
-    fo(i, 0, n) {
-//        clog << nxt[i] << ' ';
-    }
-//    clog << endl;
     ll tot = 0;
-    unordered_map<ll, ll> optimal_bisearch_l;
-    vector<shit> jump(n);
-    unordered_set<ll> jumps;
-    fo(i, 0, n) {
-        ll this_one = 0;
-        ll vozr_end_by_ind = i-1;
-//        clog << "i: " << i << endl;
-        bool can_jump = true;
-        if (jumps.contains(nxt[vozr_end_by_ind+1])) {
-            ll jmpcntrb = jump[nxt[vozr_end_by_ind+1]].contrib;
-            ll jmpstart = jump[nxt[vozr_end_by_ind+1]].start;
-            ll jmptarg = jump[nxt[vozr_end_by_ind+1]].target;
-//            clog << "found jump by r = " << nxt[vozr_end_by_ind+1] << ": " << jump[nxt[vozr_end_by_ind+1]].target << " " << jump[nxt[vozr_end_by_ind+1]].start << " " << jump[nxt[vozr_end_by_ind+1]].contrib << endl;
-//            clog << "damn, konec pred moved " << vozr_end_by_ind << " -> " << jump[nxt[vozr_end_by_ind+1]].target << endl;
-            this_one = jmpcntrb-abs(jmpstart-i);
-            vozr_end_by_ind = jmptarg;
-        }
-        vector<ll> rki;
-
-        while (vozr_end_by_ind != n-1) {
-            cnt_DELETE += 1;
-//            if (cnt_DELETE >= 1e7 && jumps.size() == 0) {
-//                break;
-//            }
-//            clog << "new vozr_end_by_ind: " << vozr_end_by_ind << endl;
-            ll minim = a[vozr_end_by_ind+1];
-            ll r = nxt[vozr_end_by_ind+1];
-            rki.push_back(r);
-            if (optimal_bisearch_l.contains(r) && optimal_bisearch_l[r] == r) {
-                this_one += r-vozr_end_by_ind;
-//                clog << "contrib: " << r-(vozr_end_by_ind+1)+1 << endl;
-//                clog << "(good: " << r << ")" << endl;
-                optimal_bisearch_l[nxt[vozr_end_by_ind+1]] = r;
-                vozr_end_by_ind = nxt[vozr_end_by_ind+1];
-                continue;
-            }
-            ll l = vozr_end_by_ind+1;
-            if (optimal_bisearch_l.contains(r)) {
-//                assert(optimal_bisearch_l[r] >= l);
-                l = max(l, optimal_bisearch_l[r]);
-            }
-            ll r_=r;
-            ll good = -666;
-            while (l <= r) {
-                ll mid = (l+r) >> 1;
-                if (b_pref[mid+1]-b_pref[i]+minim <= x) {
-                    l = mid + 1;
-                    good = mid;
-                } else {
-                    r = mid-1;
-                }
-            }
-            if (good != -666) {
-                this_one += good-vozr_end_by_ind;
-//                clog << "contrib: " << good-(vozr_end_by_ind+1)+1 << endl;
-//                clog << "(good: " << good << ")" << endl;
-                optimal_bisearch_l[nxt[vozr_end_by_ind+1]] = good;
-            }
-            if (good != r_) {
-                can_jump = false;
-            }
-            if (can_jump) {
-                for (ll rka: rki) {
-                    jump[rka].target = good;
-                    jump[rka].contrib = this_one;
-                    jump[rka].start = i;
-//                    clog << "added " << rka << " -> " << good << endl;
-                    jumps.insert(rka);
-                }
-                rki.clear();
-            }
-            vozr_end_by_ind = nxt[vozr_end_by_ind+1];
-        }
-//        clog << i << " contributes " << this_one << endl;
-        tot += this_one;
+    ll mid = (l+r) >> 1;
+    vector<ll> rs(r-(mid+1)+1);
+    vector<ll> rm(r-(mid+1)+1);
+    vector<ll> ls(r-(mid+1)+1);
+    vector<ll> lm(r-(mid+1)+1);
+    ll cum = 0;
+    ll mini = LLONG_MAX;
+    forr(i, 0, r-(mid+1)) {
+        cum += a[i+mid+1];
+        rs[i] = cum;
+        clog << "rs: " << (ll)(cum%10000) << endl;
+        mini = min(mini, b[i+mid+1]);
+        rm[i] = mini;
     }
-    cout << tot << endl;
+    cum = 0;
+    mini = LLONG_MAX;
+    forr(i, 0, r-(mid+1)) {
+        clog << i << " " << mid-i << endl;
+        cum += a[mid-i];
+        ls[i] = cum;
+        mini = min(mini, b[mid-i]);
+        lm[i] = mini;
+    }
+    ordered_set<ll> rs_st;
+    ordered_set<ll> ls_st;
+    fo(i, 0, r-(mid+1)) {
+        cout << rs[i] << endl;
+        cout << ls[i] << endl;
+        rs_st.insert(rs[i]);
+        ls_st.insert(ls[i]);
+    }
+    fo(i, 0, n) {
+        tot += rs_st.order_of_key(-1);
+//        tot += rs_st.order_of_key(x-lm[i]-ls[i]);
+    }
+//    fo(i, 0, n) {
+//        tot += ls_st.order_of_key(x-rm[i]-rs[i]);
+//    }
+    clog << l << " to " << r << ": " << tot << endl;
+    return tot + count(l, mid, a, b)+count(mid+1, r, a, b);
+}
+ll a[N];
+ll b[N];
+void solve () {
+    cin >> n >> x;
+    ll oldn = n;
+    n = N;
+    fo(i, 0, oldn) {
+        cin >> a[i];
+    }
+    fo(i, 0, oldn) {
+        cin >> b[i];
+    }
+    forr(i, oldn, N-1) {
+        a[i] = INT_MAX;
+    }
+    forr(i, oldn, N-1) {
+        b[i] = INT_MAX;
+    }
+    cout << count(0, N-1, a, b) << endl;
 }
 int32_t main (int32_t argc, char* argv[]) {
     bool use_fast_io = true;
