a31736f5183bd22e95341311b13d39b140ab04f5
Shoot <11522779+Shoot@users.noreply.github.com>
2024-09-03 03:12:56 +0300

    __int128

--- sol.cpp
+++ sol.cpp
@@ -18,7 +18,7 @@ typedef long double ld;
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math,trapv")
 #endif
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-uniform_int_distribution<ll> distrib(1ll, 20ll);
+uniform_int_distribution<ll> distrib(20ll, 20ll);
 constexpr ll MOD7 =  999987301;
 constexpr ll MOD99 = 989989103;
 constexpr ll MOD33 = 979989193;
@@ -52,7 +52,7 @@ ll powm(ll a, ll b, ll MOD){
     }
     return d;
 }
-ll MAXN = 2e5+1;
+ll MAXN = 2e5+10;
 ll base = MAXN;
 ll revbase7 = powm(base, MOD7-2, MOD7);
 ll revbase99 = powm(base, MOD99-2, MOD99);
@@ -72,8 +72,11 @@ void copy_this () {
     vector<ll> a(n); fo(i, 0, n) cin >> a[i];
 }
 */
+ll m=2e4;
+ll k=2e5;
 void solve() {
     fo(i, 1, MAXN) {
+        assert(i-1 < base_power7.size());
         base_power7[i] = (base_power7[i-1]*base)%MOD7;
         base_power99[i] = (base_power99[i-1]*base)%MOD99;
         base_power33[i] = (base_power33[i-1]*base)%MOD33;
@@ -81,36 +84,41 @@ void solve() {
         base_rev_power99[i] = (base_rev_power99[i-1]*revbase99)%MOD99;
         base_rev_power33[i] = (base_rev_power33[i-1]*revbase33)%MOD33;
     }
-    ll n; cin >> n;
-    vector<ll> a(n);
-    fo(i, 0, n) {
-        cin >> a[i];
-    }
-    ll m=3e4-10;
-    cin >> m;
+//    ll n; cin >> n;
+//    vector<ll> a(n);
+//    fo(i, 0, n) {
+//        cin >> a[i];
+//    }
+//    cin >> m;
     vector<ll> b(m);
     vector<ll> pref_b_7(m+1, 0);
     vector<ll> pref_b_99(m+1, 0);
     vector<ll> pref_b_33(m+1, 0);
     vector<ll> pref_b_44(m+1, 0);
     fo(i, 0, m) {
-//        b[i] = distrib(rng);
-//        clog << b[i] << ' ';
-        cin >> b[i];
+        assert(i < b.size());
+        b[i] = distrib(rng);
+        clog << b[i] << ' ';
+//        cin >> b[i];
+        assert(i+1 < pref_b_7.size());
+        assert(i < b.size());
+        assert(i < base_power7.size());
         pref_b_7[i+1] = (pref_b_7[i]+(b[i]*base_power7[i])%MOD7)%MOD7;
         pref_b_99[i+1] = (pref_b_99[i]+(b[i]*base_power99[i])%MOD99)%MOD99;
         pref_b_33[i+1] = (pref_b_33[i]+(b[i]*base_power33[i])%MOD33)%MOD33;
     }clog << endl;
-    ll k=3e4-10;
-    cin >> k;
+//    cin >> k;
     vector<ll> c(k);
     vector<ll> pref_c_7(k+1, 0);
     vector<ll> pref_c_99(k+1, 0);
     vector<ll> pref_c_33(k+1, 0);
     fo(i, 0, k) {
-//        c[i] = distrib(rng);
-//        clog << c[i] << ' ';
-        cin >> c[i];
+        c[i] = distrib(rng);
+        clog << c[i] << ' ';
+//        cin >> c[i];
+        assert(i+1 < pref_c_7.size());
+        assert(i < c.size());
+        assert(i < base_power7.size());
         pref_c_7[i+1] = (pref_c_7[i]+(c[i]*base_power7[i])%MOD7)%MOD7;
         pref_c_99[i+1] = (pref_c_99[i]+(c[i]*base_power99[i])%MOD99)%MOD99;
         pref_c_33[i+1] = (pref_c_33[i]+(c[i]*base_power33[i])%MOD33)%MOD33;
@@ -127,11 +135,17 @@ void solve() {
         unordered_set<ll> c_st99;
         unordered_set<ll> c_st33;
         fo(i, 0, m+1-mid) {
+            assert(i+mid < pref_b_7.size());
+            assert(i < pref_b_7.size());
+            assert(i < base_rev_power7.size());
             b_st7.insert((((pref_b_7[i+mid]-pref_b_7[i]+MOD7)%MOD7)*base_rev_power7[i])%MOD7);
             b_st99.insert((((pref_b_99[i+mid]-pref_b_99[i]+MOD99)%MOD99)*base_rev_power99[i])%MOD99);
             b_st33.insert((((pref_b_33[i+mid]-pref_b_33[i]+MOD33)%MOD33)*base_rev_power33[i])%MOD33);
         }
         fo(i, 0, k+1-mid) {
+            assert(i+mid < pref_c_7.size());
+            assert(i < pref_c_7.size());
+            assert(i < base_rev_power7.size());
             c_st7.insert((((pref_c_7[i+mid]-pref_c_7[i]+MOD7)%MOD7)*base_rev_power7[i])%MOD7);
             c_st99.insert((((pref_c_99[i+mid]-pref_c_99[i]+MOD99)%MOD99)*base_rev_power99[i])%MOD99);
             c_st33.insert((((pref_c_33[i+mid]-pref_c_33[i]+MOD33)%MOD33)*base_rev_power33[i])%MOD33);
@@ -168,59 +182,59 @@ void solve() {
             r = mid-1;
         }
     }
-//    clog << "ans: " << good << endl;
-//    auto stop = chrono::high_resolution_clock::now();
-//    auto duration = duration_cast<chrono::microseconds>(stop - start_time);
-//    clog << "Time taken by function: " << duration.count() << " microseconds" << endl;
-//    ll OG = og_counter(b, c);
-//    clog << good << " " << OG << endl;
-//    assert(good == OG);
-    if (good > 0) {
-        clog << "peresech_size: " << good << endl;
-        cout << m-good+k-good << endl;
-    } else {
-        clog << "lets see" << endl;
-        ll dp[MAXN];
-        fo(i, 0, MAXN) {
-            dp[i] = INT_MAX;
-        }
-        fo(i, 0, m) {
-            dp[b[i]] = m-1;
-        }
-
-        while(true) {
-            forr(i, 1, n-1) {
-                dp[a[i]] = min(dp[a[i]], dp[a[i-1]]+2);
-            }
-            roff(i, n-2, 0) {
-                dp[a[i]] = min(dp[a[i]], dp[a[i+1]]+2);
-            }
-            bool enough = true;
-            forr(i, 1, n-1) {
-                if (dp[a[i]] != min(dp[a[i]], dp[a[i-1]]+2)) {
-                    enough = false;
-                }
-            }
-            roff(i, n-2, 0) {
-                if (dp[a[i]] != min(dp[a[i]], dp[a[i+1]]+2)) {
-                    enough = false;
-                }
-            }
-            if (enough) break;
-            else clog << "not enough" << endl;
-        }
-
-        fo(i, 0, k) {
-            assert(dp[b[i]] == m-1);
-        }
-        ll res = INT_MAX;
-        fo(i, 0, k) {
-            res = min(res, dp[c[i]]+k-1);
-        }
-        assert(res > 0);
-        assert(res < 4.1e5);
-        cout << res << endl;
-    }
+    clog << "ans: " << good << endl;
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = duration_cast<chrono::microseconds>(stop - start_time);
+    clog << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    ll OG = og_counter(b, c);
+    clog << good << " " << OG << endl;
+    assert(good == OG);
+//    if (good > 0) {
+//        clog << "peresech_size: " << good << endl;
+//        cout << m-good+k-good << endl;
+//    } else {
+//        clog << "lets see" << endl;
+//        ll dp[MAXN];
+//        fo(i, 0, MAXN) {
+//            dp[i] = INT_MAX;
+//        }
+//        fo(i, 0, m) {
+//            dp[b[i]] = m-1;
+//        }
+//
+//        while(true) {
+//            forr(i, 1, n-1) {
+//                dp[a[i]] = min(dp[a[i]], dp[a[i-1]]+2);
+//            }
+//            roff(i, n-2, 0) {
+//                dp[a[i]] = min(dp[a[i]], dp[a[i+1]]+2);
+//            }
+//            bool enough = true;
+//            forr(i, 1, n-1) {
+//                if (dp[a[i]] != min(dp[a[i]], dp[a[i-1]]+2)) {
+//                    enough = false;
+//                }
+//            }
+//            roff(i, n-2, 0) {
+//                if (dp[a[i]] != min(dp[a[i]], dp[a[i+1]]+2)) {
+//                    enough = false;
+//                }
+//            }
+//            if (enough) break;
+//            else clog << "not enough" << endl;
+//        }
+//
+//        fo(i, 0, k) {
+//            assert(dp[b[i]] == m-1);
+//        }
+//        ll res = INT_MAX;
+//        fo(i, 0, k) {
+//            res = min(res, dp[c[i]]+k-1);
+//        }
+//        assert(res > 0);
+//        assert(res < 4.1e5);
+//        cout << res << endl;
+//    }
 }
 int32_t main (int32_t argc, char* argv[]) {
     bool use_fast_io = true;
