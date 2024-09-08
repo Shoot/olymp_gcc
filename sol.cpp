6e472e13ac83864e3917a7f3bb0a163d5206ea0d
Shoot <11522779+Shoot@users.noreply.github.com>
2024-09-09 01:54:30 +0300

    1e7 ops, ez

--- sol.cpp
+++ sol.cpp
@@ -219,29 +219,24 @@ void solve() {
         return;
     }
     vector<vector<answer>> answ (k);
-    ld sorts_take = 0;
 
     fo(ii, 0, k) {
         answ[ii].resize(max_length[ii]+1);
         vector<ll> first_tree (N, 0);
         vector<ll> second_tree (N, 0);
-        auto start_sort = chrono::high_resolution_clock::now();
         sort(all(G[ii].first), [](shit a, shit b) {
             return a.chet < b.chet;
         });
         sort(all(G[ii].second), [](shit a, shit b) {
             return a.nechet < b.nechet;
         });
-        auto stop_sort = chrono::high_resolution_clock::now();
-        auto duration = duration_cast<chrono::microseconds>(stop_sort - start_sort);
-        sorts_take += duration.count();
-        auto duration_all = duration_cast<chrono::microseconds>(stop_sort - start);
-        if (duration_all.count() > 2e5) {
-            assert(sorts_take*5 < duration_all.count());
-        }
         ll curr_ans=0;
         ll adding_first = 0;
         ll adding_second = 0;
+        ld SSSS = 1;
+        while (SSSS++ < 1e7/20/(ld)k) {
+            getll(0, N-100, first_tree);
+        }
         while (curr_ans <= max_length[ii]) {
             if (curr_ans%2 == 0) {
                 answ[ii][curr_ans].less__any = getll(0, N-100, first_tree);
