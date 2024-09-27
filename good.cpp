d7d34024245697d39f9cf9ea83696a197fb1f6bf
Shoot <11522779+Shoot@users.noreply.github.com>
2024-09-27 10:55:07 +0300

    nvm

--- good.cpp
+++ good.cpp
@@ -1,178 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pll = pair<ll, ll>;
-using ld = long double;
-using qll = queue<ll>;
-using vll = vector<ll>;
-using qld = queue<ld>;
-using vld = vector<ld>;
-using qpll = queue<pll>;
-using vpll = vector<pll>;
-ostream& endl(ostream& os) {
-    return os << '\n';
-}
-#ifdef LOCAL
-#include <algo/debug.h>
-#else
-//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math,trapv")
-#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
-#endif
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-uniform_int_distribution<ll> distrib(2ll, 10ll);
-constexpr ll MOD = 1e9+7;
-void in(vector<ll> & a) {
-    for (auto & x : a) cin >> x;
-}
-void in(vector<ll> & a, ll l, ll r) {
-    for (ll i=l; i < r; i+=1) {
-        cin >> a[i];
-    }
-}
-void inn(vector<ll> & a, ll l, ll rr) {
-    for (ll i=l; i <= rr; i+=1) {
-        cin >> a[i];
-    }
-}
-ll powm(ll a, ll b) {
-    assert(b >= 0);
-    ll d = 1;
-    while (b) {
-        if (b&1) d = (d*a) % MOD;
-        b >>= 1;
-        a = (a*a) % MOD;
-    }
-    return d;
-}
-ll powm(ll a, ll b, ll MOD) {
-    assert(b >= 0);
-    ll d = 1;
-    while (b) {
-        if (b&1) d = (d*a) % MOD;
-        b >>= 1;
-        a = (a*a) % MOD;
-    }
-    return d;
-}
-ll poww(ll a, ll b) {
-    assert(b >= 0);
-    ll d = 1;
-    while (b) {
-        if (b&1) d = (d*a);
-        b >>= 1;
-        a = (a*a);
-    }
-    return d;
-}
-ll mul(ll a, ll b) {
-    return (a*b)%MOD;
-}
-ll sum(ll a, ll b) {
-    return (a+b)%MOD;
-}
-ll sub(ll a, ll b) {
-    return (a-b+100*MOD)%MOD;
-}
-ll fj0dsq983gf8(ll index, vector<ll> & tree)  {
-    index += 1;
-    ll sum = 0;
-    while (index > 0) {
-        sum += tree[index-1];
-        index -= index & -index;
-    }
-    return sum;
-} // zero-indexed!!!
-ll get_sum_ft(ll left, ll right, vector<ll> & tree) {
-    ll n = (ll)tree.size();
-    if (!(left <= right)) return 0;
-    assert(left <= right);
-    if (right >= n) {
-        clog << "FENWICK ALERT: R >= tree.size() (IT'S ZERO INDEXED !!!)" << endl;
-        right = n-1;
-    }
-    ll ans = fj0dsq983gf8(right, tree);
-    if (left-1 >= 0) {
-        ans -= fj0dsq983gf8(left - 1, tree);
-    }
-    return ans;
-} // zero-indexed!!!
-void inc_ft(ll index, ll inc, vector<ll> & tree) {
-    ll n = (ll)tree.size();
-    assert(index >= 0);
-    assert(index < n);
-    index += 1;
-    while (index < n) {
-        tree[index] += inc;
-        index += index & -index;
-    }
-} // zero-indexed!!!
-void build_ft(vector<ll> & a, vector<ll> & tree) {
-    ll n = (ll)tree.size();
-    assert(tree.size() == a.size());
-    for (ll i = 0; i < n; i++) {
-        tree[i] += a[i];
-        ll r = i | (i + 1);
-        if (r < n) tree[r] += tree[i];
-    }
-} // zero-indexed!!!
-/*
-void copy_this () {
-    ll n; cin >> n;
-    ll n, k; cin >> n >> k;
-    ll n, q; cin >> n >> q;
-    ll a[n]; for (ll i=0; i < n; i+=1) cin >> a[i];
-    vector<ll> a(n); for (ll i=0; i < n; i+=1) cin >> a[i];
-}
-*/
-const ll bitset_max = 2e7+100;
-void solve() {
-    ll n; cin >> n;
-    ll start = n * n;
-    ll end = n * n + n;
-    bitset<bitset_max> isPrimeMinusStart;
-    bitset<bitset_max> isPrime_sieve;
-    isPrime_sieve.set();
-    isPrimeMinusStart.set();
-    isPrime_sieve[1] = false;
-    if (start == 1) isPrimeMinusStart[1] = false;
-    ll prime_limit = sqrt(n * n + n) + 1;
-    for (ll i=2; i <= prime_limit; i++) {
-        if (isPrime_sieve[i]) {
-            for (ll nxt = i*i; nxt <= prime_limit; nxt += i) {
-                isPrime_sieve[i] = false;
-            }
-            for (ll nxtbig = max(i*i, start+(i-start%i)%i); nxtbig <= end; nxtbig+=i) {
-                isPrimeMinusStart[nxtbig-start] = false;
-            }
-        }
-    }
-    ll ans = 0;
-    for (ll i=start; i <= end; i+=1) {
-//        if (isPrimeMinusStart[i-start]) cout << i << endl;
-        ans += isPrimeMinusStart[i-start];
-    }
-    cout << ans << endl;
-}
+const int SQRT_MAXN = 100000; // корень из максимального значения N
+const int S = 10000;
+bool nprime[SQRT_MAXN], bl[S];
+int primes[SQRT_MAXN], cnt;
 
-int32_t main(int32_t argc, char* argv[]) {
-    bool use_fast_io = true;
-    for (int32_t i = 1; i < argc; ++i)
-        if (string(argv[i]) == "-local-no-fast-io") {
-            use_fast_io = false;
-//            cout << "No fastIO" << endl;
-            break;
+int main() {
+
+    int n;
+    cin >> n;
+    int nsqrt = (int) sqrt (n + .0);
+    for (int i=2; i<=nsqrt; ++i)
+        if (!nprime[i]) {
+            primes[cnt++] = i;
+            if (i * 1ll * i <= nsqrt)
+                for (int j=i*i; j<=nsqrt; j+=i)
+                    nprime[j] = true;
         }
-    if (use_fast_io) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        cerr.tie(nullptr);
-        clog.tie(nullptr);
+
+    int result = 0;
+    for (int k=0, maxk=n/S; k<=maxk; ++k) {
+        memset (bl, 0, sizeof bl);
+        int start = k * S;
+        for (int i=0; i<cnt; ++i) {
+            int start_idx = max(2, (start + primes[i] - 1) / primes[i]);
+            cout << primes[i] << ": start = " << start_idx*primes[i] << " ( start_of_block = " << start << endl;
+            int mystart = max(primes[i]*primes[i], start+(primes[i]-start%primes[i])%primes[i]);
+            cout << "mystart:" << mystart << endl;
+//            assert(start_idx*primes[i] == mystart);
+            int j = mystart - start;
+            for (; j<S; j+=primes[i])
+                bl[j] = true;
+        }
+        if (k == 0)
+            bl[0] = bl[1] = true;
+        for (int i=0; i<S && start+i<=n; ++i)
+            if (!bl[i]) {
+                result += 1;
+                cout << start + i << endl;
+            }
     }
-    ll tt = 1;
-//    cin >> tt;
-    while (tt--) {
-        solve();
-    }
-    return 0;
+    cout << result;
 }
