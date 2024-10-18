d71376e79f02359b6de42fa161f2a71da15ad2ba
Shoot <11522779+Shoot@users.noreply.github.com>
2024-10-19 01:12:41 +0300

    stack of records, nxt greater, https://codeforces.com/contest/1779/problem/D

--- sol.cpp
+++ sol.cpp
@@ -1,43 +1,274 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> a (n, vector<int>(m, 0));
-    for (int i = 0; i < n; i += 1) {
-        for (int j = 0; j < m; j += 1) {
-            cin >> a[i][j];
+using vbo = vector<bool>;
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using ld = long double;
+using qll = queue<ll>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using qld = queue<ld>;
+using vld = vector<ld>;
+using qpll = queue<pll>;
+using vpll = vector<pll>;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ostream& endl(ostream& os) {
+    return os << '\n';
+}
+#define all(xxx) xxx.begin(), xxx.end()
+#define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
+template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {
+    os << A.fi << " " << A.se;
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+    for (size_t i = 0; i < A.size(); i++) {
+        if (i) os << " ";
+        os << A[i];
+    }
+    return os;
+}
+void scan(ll &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(long double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &a) {for(auto &i : a) scan(i);}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+void print() {
+    cout << "\n";
+}
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+    cout << head;
+    if (sizeof...(Tail)) cout << " ";
+    print(forward<Tail>(tail)...);
+}
+#ifdef LOCAL
+#include <algo/debug.h>
+#else
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math,trapv")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
+#endif
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<ll> distrib(1ll, 12ll);
+//constexpr ll MOD = 1e9+7;
+constexpr ll MOD = 998244353;
+void in(vector<ll> & a) {
+    for (auto & x : a) cin >> x;
+}
+void in(vector<ll> & a, ll l, ll r) {
+    for (ll i=l; i < r; i+=1) {
+        cin >> a[i];
+    }
+}
+void inn(vector<ll> & a, ll l, ll rr) {
+    for (ll i=l; i <= rr; i+=1) {
+        cin >> a[i];
+    }
+}
+ll powm(ll a, ll b) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*a) % MOD;
+        b >>= 1;
+        a = (a*a) % MOD;
+    }
+    return d;
+}
+ll powm(ll a, ll b, ll MODD) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*a) % MODD;
+        b >>= 1;
+        a = (a*a) % MODD;
+    }
+    return d;
+}
+ll poww(ll a, ll b) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*a);
+        b >>= 1;
+        a = (a*a);
+    }
+    return d;
+}
+ld poww(ld a, ll b) {
+    assert(b >= 0);
+    ld d = 1;
+    while (b) {
+        if (b&1) d = (d*a);
+        b >>= 1;
+        a = (a*a);
+    }
+    return d;
+}
+ll mul(ll a, ll b) {
+    return (a*b)%MOD;
+}
+ll mul(ll a, ll b, ll MODD) {
+    return (a*b)%MODD;
+}
+ll sum(ll a, ll b) {
+    return (a+b)%MOD;
+}
+ll sum(ll a, ll b, ll MODD) {
+    return (a+b)%MODD;
+}
+ll sub(ll a, ll b) {
+    return (a-(b%MOD)+MOD)%MOD;
+}
+ll sub(ll a, ll b, ll MODD) {
+    return (a-(b%MODD)+MODD)%MODD;
+}
+ll fj0dsq983gf8(ll index, vector<ll> & tree)  {
+    index += 1;
+    ll sum = 0;
+    while (index > 0) {
+        sum += tree[index-1];
+        index -= index & -index;
+    }
+    return sum;
+} // zero-indexed!!!
+ll get_sum_ft(ll left, ll right, vector<ll> & tree) {
+    ll n = (ll)tree.size();
+    if (left > right) return 0;
+    assert(left <= right);
+    if (right >= n) {
+        clog << "FENWICK ALERT: R >= tree.size() (IT'S ZERO INDEXED !!!)" << endl;
+        right = n-1;
+    }
+    ll ans = fj0dsq983gf8(right, tree);
+    if (left-1 >= 0) {
+        ans -= fj0dsq983gf8(left - 1, tree);
+    }
+    return ans;
+} // zero-indexed!!!
+void inc_ft(ll index, ll inc, vector<ll> & tree) {
+    ll n = (ll)tree.size();
+    assert(index >= 0);
+    assert(index < n);
+    index += 1;
+    while (index < n) {
+        tree[index] += inc;
+        index += index & -index;
+    }
+} // zero-indexed!!!
+void build_ft(vector<ll> & a, vector<ll> & tree) {
+    ll n = (ll)tree.size();
+    assert(tree.size() == a.size());
+    for (ll i = 0; i < n; i++) {
+        tree[i] += a[i];
+        ll good = i | (i + 1);
+        if (good < n) tree[good] += tree[i];
+    }
+} // zero-indexed!!!
+/*
+void copy_this () {
+    ll n; cin >> n;
+    ll n, k; cin >> n >> k;
+    ll n, q; cin >> n >> q;
+    ll a[n]; for (ll i=0; i < n; i+=1) cin >> a[i];
+    vector<ll> a(n); for (ll i=0; i < n; i+=1) cin >> a[i];
+}
+*/
+
+void solve() {
+    ll n;
+    cin >> n;
+    vll a(n);
+    vll b(n);
+    for (auto &x : a) cin >> x;
+    map<ll, set<ll>> indices_by_val;
+    for (ll i = 0; i < n; i += 1) {
+        cin >> b[i];
+        indices_by_val[b[i]].insert(i);
+    }
+    ll m; cin >> m;
+    vll br(m);
+    for (auto &x : br) cin >> x;
+    for (ll i = 0; i < n; i += 1) {
+        if (a[i] < b[i]) {
+            cout << "NO" << endl;
+            return;
         }
     }
-    int q;cin>>q;
-    while(q--) {
-        int c;
-        cin >> c;
-        stack<int> stck;
-        vector<int> up(m, -1), left(m), right(m);
-        int ans = -1e9;
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < m; j+=1) {
-                if (a[i][j] != c) {
-                    up[j] = i;
-                }
-            }
-            while (!stck.empty()) stck.pop();
-            for (int j = 0; j < m; j += 1) {
-                while (!stck.empty() && up[stck.top()] <= up[j]) stck.pop();
-                if (stck.empty()) left[j] = -1;
-                else left[j] = stck.top();
-                stck.push(j);
-            }
-            while (!stck.empty()) stck.pop();
-            for (int j = m - 1; j >= 0; j -= 1) {
-                while (!stck.empty() && up[stck.top()] <= up[j]) stck.pop();
-                if (stck.empty()) right[j] = m;
-                else right[j] = stck.top();
-                stck.push(j);
-            }
-            for(int j = 0; j < m; j++) ans = max(ans, (i - up[j]) * (right[j] - left[j] - 1));
-        }
-        cout << ans << endl;
+    vll nxt_greater(n, n);
+    stack<ll> stck_of_records;
+    for (ll i = n-1; i >= 0; i -= 1) {
+        while (!stck_of_records.empty() && b[stck_of_records.top()] <= b[i]) stck_of_records.pop();
+        if (!stck_of_records.empty()) nxt_greater[i] = stck_of_records.top();
+        stck_of_records.push(i);
     }
+    for (const auto &x : br) {
+        // do NOT count a[i] == b[i] as something that needs to be done
+        while (!indices_by_val[x].empty() &&
+                a[*indices_by_val[x].begin()] == b[*indices_by_val[x].begin()])
+        {
+            indices_by_val[x].erase(indices_by_val[x].begin());
+        }
+        if (indices_by_val[x].empty()) continue;
+//        watch(x);
+        ll first_undone = *indices_by_val[x].begin();
+//        watch(first_undone);
+        auto r_limit = nxt_greater[first_undone];
+//        watch(r_limit);
+        while (!indices_by_val[x].empty() && *indices_by_val[x].begin() < r_limit) {
+            auto it = indices_by_val[x].begin();
+            a[*it] = b[*it];
+            indices_by_val[x].erase(it);
+        }
+    }
+    for (ll i = 0; i < n; i += 1) {
+        if (a[i] != b[i]) {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+int32_t main(int32_t argc, char* argv[]) {
+//    ifstream cin("distance.in");
+//    ofstream cout("distance.out");
+    cout << setprecision(17);
+    bool use_fast_io = true;
+    for (int32_t i = 1; i < argc; ++i) {
+        if (string(argv[i]) == "-local-no-fast-io") {
+            use_fast_io = false;
+//            cout << "No fastIO" << endl;
+            break;
+        }
+    }
+    if (use_fast_io) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        cerr.tie(nullptr);
+        clog.tie(nullptr);
+    }
+    ll tt = 1;
+    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
+    return 0;
 }
