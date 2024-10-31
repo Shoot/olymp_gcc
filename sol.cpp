0b227e0ee8dff674eb1f0f3b884bdcb9730741a7
Shoot <11522779+Shoot@users.noreply.github.com>
2024-10-31 11:59:40 +0300

    bin search races

--- sol.cpp
+++ sol.cpp
@@ -19,8 +19,8 @@ template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_ta
 ostream& endl(ostream& os) {
     return os << '\n';
 }
-//define all(xxx) xxx.begin(), xxx.end()
-//define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
+#define all(xxx) xxx.begin(), xxx.end()
+#define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
 template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
 template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
 template <typename T, typename U>
@@ -139,48 +139,6 @@ ll sub(ll a, ll b) {
 ll sub(ll a, ll b, ll MODD) {
     return (a-(b%MODD)+MODD)%MODD;
 }
-ll fj0dsq983gf8(ll index, vector<ll> & damn)  {
-    index += 1;
-    ll sum = 0;
-    while (index > 0) {
-        sum += damn[index-1];
-        index -= index & -index;
-    }
-    return sum;
-} // zero-indexed!!!
-ll get_sum_ft(ll left, ll right, vector<ll> & damn) {
-    ll n = (ll)damn.size();
-    if (left > right) return 0;
-    assert(left <= right);
-    if (right >= n) {
-        clog << "FENWICK ALERT: R >= damn.size() (IT'S ZERO INDEXED !!!)" << endl;
-        right = n-1;
-    }
-    ll dist_from_last_bfs = fj0dsq983gf8(right, damn);
-    if (left-1 >= 0) {
-        dist_from_last_bfs -= fj0dsq983gf8(left - 1, damn);
-    }
-    return dist_from_last_bfs;
-} // zero-indexed!!!
-void inc_ft(ll index, ll inc, vector<ll> & damn) {
-    ll n = (ll)damn.size();
-    assert(index >= 0);
-    assert(index < n);
-    index += 1;
-    while (index < n) {
-        damn[index] += inc;
-        index += index & -index;
-    }
-} // zero-indexed!!!
-void build_ft(vector<ll> & a, vector<ll> & damn) {
-    ll n = (ll)damn.size();
-    assert(damn.size() == a.size());
-    for (ll i = 0; i < n; i++) {
-        damn[i] += a[i];
-        ll good = i | (i + 1);
-        if (good < n) damn[good] += damn[i];
-    }
-} // zero-indexed!!!
 /*
 void copy_this () {
     ll n; cin >> n;
@@ -190,103 +148,69 @@ void copy_this () {
     vector<ll> a(n); for (ll i=0; i < n; i+=1) cin >> a[i];
 }
 */
-vll a;
-vector<ordered_set<pll>> damn;
-pll get_equal_range(ll v, ll tl, ll tr, ll l, ll r, ll val) {
-    if (tl == l && tr == r) {
-//            cout << l+1 << " " << r+1 << endl;
-        ll ub = damn[v].order_of_key(pll(val, 1e18));
-        ll lb = damn[v].order_of_key(pll(val, -1e18));
-        return pll(lb, ub);
-    }
-    pll ans; ans.first = 0; ans.second = 0;
-    ll tm = (tl+tr) >> 1;
-    if (l <= tm) {
-        pll left = get_equal_range(2*v+1, tl, tm, l, min(tm, r), val);
-        ans.first += left.first;
-        ans.second += left.second;
-    }
-    if (r > tm) {
-        pll right = get_equal_range(2*v+2, tm+1, tr, max(tm+1, l), r, val);
-        ans.first += right.first;
-        ans.second += right.second;
-    }
-    return ans;
-}
-void build(ll v, ll tl, ll tr) {
-    for (ll i = tl; i <= tr; i += 1) {
-        damn[v].insert(pll(a[i], i));
-    }
-    if (tl == tr) return;
-    ll tm = (tl + tr) >> 1;
-    build(2*v+1, tl, tm);
-    build(2*v+2, tm + 1, tr);
-}
-void set_point(ll v, ll tl, ll tr, ll pos, ll val) {
-    ll tm = (tl+tr) >> 1;
-    damn[v].erase(pll(a[pos], pos));
-    damn[v].insert(pll(val, pos));
-    if (tl == tr) return;
-    if (pos <= tm) {
-        set_point(2*v+1, tl, tm, pos, val);
-    } else {
-        set_point(2*v+2, tm+1, tr, pos, val);
-    }
-}
+
 void solve() {
-    damn.resize(4*50000+10);
-    ll n;
-    cin >> n;
-    a.resize(n);
-    for (auto &x : a) {
-        x = distrib(rng);
-        cin >> x;
+    ll n, races, k, s;
+    cin >> n >> races >> k >> s;
+    vll prize_by_place(n);
+    for (ll place = 0; place < k; place += 1) {
+        ll prize; cin >> prize;
+        prize_by_place[place] = prize;
     }
-    build(0, 0, n-1);
-    ll q;
-    cin >> q;
-    for (ll i = 0; i < q; i += 1) {
-        char type = 'Q';
-        cin >> type;
-        if (type == 'Q') {
-            ll x1, y1, x2, y2;
-            cin >> x1 >> y1 >> x2 >> y2;
-            ll less_or_eq = (y1+y2)/2;
-            ll l = x1, r = x2;
-            assert(l <= r);
-            assert(l >= 1);
-            assert(r <= n);
-            pll res = get_equal_range(0, 0, n-1, l-1, r-1, less_or_eq);
-            if ((y1+y2)%2 == 0) {
-                ll draw = res.second-res.first;
-                ll first = res.first;
-                ll second = r-l+1-first-draw;
-                if (first == second) {
-                    cout << 0 << endl;
-                } else if (first < second) {
-                    cout << 2 << endl;
-                } else {
-                    cout << 1 << endl;
-                }
-                continue;
-            }
-            ll first = res.second;
-            ll second = r-l+1-first;
-            if (first == second) {
-                cout << 0 << endl;
-            } else if (first < second) {
-                cout << 2 << endl;
-            } else {
-                cout << 1 << endl;
-            }
-        } else {
-            ll pos, val;
-            cin >> pos >> val;
-            assert(pos >= 1);
-            assert(pos <= n);
-            set_point(0, 0, n-1, pos-1, val);
-            a[pos-1] = val;
+    vll initial(n);
+    for (auto &x : initial) cin >> x;
+    vvll prizes_by_person(n, vll(races));
+    vvll prizes_by_person_pref(n, vll(races));
+    for (ll i = 0; i < races; i += 1) {
+        for (ll place = 0; place < n; place += 1) {
+            ll who; cin >> who;
+            who -= 1;
+            prizes_by_person[who][i] = prize_by_place[place];
         }
+    }
+    for (auto &x : prizes_by_person) sort(all(x), greater<>());
+//    for (ll guy = 0; guy < n; guy += 1) {
+//        cout << guy << " won ";
+//        print(prizes_by_person[guy]);
+//    }
+    for (ll guy = 0; guy < n; guy += 1) {
+        prizes_by_person_pref[guy][0] = prizes_by_person[guy][0];
+        for (ll race = 1; race < races; race += 1) {
+            prizes_by_person_pref[guy][race] = prizes_by_person_pref[guy][race-1]+prizes_by_person[guy][race];
+        }
+    }
+//    for (ll guy = 0; guy < n; guy += 1) {
+//        cout << guy << " won_pref ";
+//        print(prizes_by_person_pref[guy]);
+//    }
+    ll q; cin >> q;
+    while (q--) {
+        ll u, v;
+        cin >> u >> v;
+        u -= 1;
+        v -= 1;
+        ll l=0, r=1e9;
+        ll good = -1;
+        while (l <= r) {
+            ll mid = (l+r)>>1;
+            ll new_sz = races+mid;
+            ll counted_sz = new_sz-s;
+            ll sum_u = initial[u]+prizes_by_person_pref[u][min(races, counted_sz)-1];
+            if (counted_sz > races) {
+                sum_u += prize_by_place[n-1]*(counted_sz-races);
+            }
+            ll sum_v = initial[v]+prize_by_place[0]*mid+prizes_by_person_pref[v][races-s-1];
+//            cout << u << ' ' << v << ": " << sum_u << ' ' << sum_v << endl;
+//            cout << u << ' ' << v << ": " << initial[u] << ' ' << initial[v] << endl;
+            if (sum_v > sum_u) {
+                r = mid-1;
+                good = mid;
+            } else {
+                l = mid+1;
+            }
+        }
+        assert(good != -1);
+        cout << good << endl;
     }
 }
 
