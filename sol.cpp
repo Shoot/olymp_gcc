e49d503632a12dc41ba48d91c813259e4dc724e1
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-03 01:31:41 +0300

    __int128 хеши, понять что ответ порядка N и оптимайзить память а не поиск ответа

--- sol.cpp
+++ sol.cpp
@@ -3,24 +3,21 @@ using namespace std;
 using vbo = vector<bool>;
 using ll = long long;
 using ull = unsigned long long;
-using pll = pair<ll, ll>;
 using ld = long double;
-using qll = queue<ll>;
-using vll = vector<ll>;
-using vvll = vector<vector<ll>>;
+using qll = queue<__int128>;
+using vll = vector<__int128>;
+using vvll = vector<vector<__int128>>;
 using qld = queue<ld>;
 using vld = vector<ld>;
-using qpll = queue<pll>;
-using vpll = vector<pll>;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
 template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-//ostream& endl(ostream& os) {
-//    return os << '\n';
-//}
-#define all(xxx) xxx.begin(), xxx.end()
-#define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
+ostream& endl(ostream& os) {
+    return os << '\n';
+}
+//define all(xxx) xxx.begin(), xxx.end()
+//define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
 template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
 template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
 template <typename T, typename U>
@@ -30,9 +27,9 @@ ostream& operator<<(ostream& os, const pair<T, U>& A) {
 }
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T>& A) {
-    for (size_t modulo = 0; modulo < A.size(); modulo++) {
-        if (modulo) os << " ";
-        os << A[modulo];
+    for (size_t i = 0; i < A.size(); i++) {
+        if (i) os << " ";
+        os << A[i];
     }
     return os;
 }
@@ -42,7 +39,7 @@ void scan(double &a) { cin >> a; }
 void scan(long double &a) { cin >> a; }
 void scan(string &a) { cin >> a; }
 template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
-template <class T> void scan(vector<T> &a) {for(auto &modulo : a) scan(modulo);}
+template <class T> void scan(vector<T> &a) {for(auto &i : a) scan(i);}
 template <class T> void scan(T &a) { cin >> a; }
 void IN() {}
 template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
@@ -65,25 +62,12 @@ void print(Head&& head, Tail&&... tail) {
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
 #endif
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-uniform_int_distribution<ll> distrib(0ll, 25ll);
-//constexpr ll MOD = 1e9+7;
-constexpr ll MOD = 1e9+7;
-void in(vector<ll> & a) {
-    for (auto & zero_leaf : a) cin >> zero_leaf;
-}
-void in(vector<ll> & a, ll l, ll r) {
-    for (ll modulo=l; modulo < r; modulo+=1) {
-        cin >> a[modulo];
-    }
-}
-void inn(vector<ll> & a, ll l, ll rr) {
-    for (ll modulo=l; modulo <= rr; modulo+=1) {
-        cin >> a[modulo];
-    }
-}
-ll powm(ll a, ll b) {
-    assert(b >= 0);
-    ll d = 1;
+uniform_int_distribution<ll> distrib(0ll, 10ll);
+constexpr __int128 MOD = 884341072164363733ll;
+//constexpr __int128 MOD = 1e9+7;
+__int128 powm(__int128 a, __int128 b) {
+    //assert(b >= 0);
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*a) % MOD;
         b >>= 1;
@@ -91,9 +75,9 @@ ll powm(ll a, ll b) {
     }
     return d;
 }
-ll powm(ll a, ll b, ll MODD) {
-    assert(b >= 0);
-    ll d = 1;
+__int128 powm(__int128 a, __int128 b, __int128 MODD) {
+    //assert(b >= 0);
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*a) % MODD;
         b >>= 1;
@@ -101,9 +85,9 @@ ll powm(ll a, ll b, ll MODD) {
     }
     return d;
 }
-ll poww(ll a, ll b) {
-    assert(b >= 0);
-    ll d = 1;
+__int128 poww(__int128 a, __int128 b) {
+    //assert(b >= 0);
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*a);
         b >>= 1;
@@ -111,8 +95,8 @@ ll poww(ll a, ll b) {
     }
     return d;
 }
-ld poww(ld a, ll b) {
-    assert(b >= 0);
+ld poww(ld a, __int128 b) {
+    //assert(b >= 0);
     ld d = 1;
     while (b) {
         if (b&1) d = (d*a);
@@ -121,72 +105,83 @@ ld poww(ld a, ll b) {
     }
     return d;
 }
-ll mul(ll a, ll b) {
+__int128 mul(__int128 a, __int128 b) {
     return (a*b)%MOD;
 }
-ll mul(ll a, ll b, ll MODD) {
+__int128 mul(__int128 a, __int128 b, __int128 MODD) {
     return (a*b)%MODD;
 }
-ll sum(ll a, ll b) {
+__int128 sum(__int128 a, __int128 b) {
     return (a+b)%MOD;
 }
-ll sum(ll a, ll b, ll MODD) {
+__int128 sum(__int128 a, __int128 b, __int128 MODD) {
     return (a+b)%MODD;
 }
-ll sub(ll a, ll b) {
+__int128 sub(__int128 a, __int128 b) {
     return (a-(b%MOD)+MOD)%MOD;
 }
-ll sub(ll a, ll b, ll MODD) {
+__int128 sub(__int128 a, __int128 b, __int128 MODD) {
     return (a-(b%MODD)+MODD)%MODD;
 }
 /*
 void copy_this () {
-    ll n; cin >> n;
-    ll n, BAN; cin >> n >> BAN;
-    ll n, q; cin >> n >> q;
-    ll a[n]; for (ll modulo=0; modulo < n; modulo+=1) cin >> a[modulo];
-    vector<ll> a(n); for (ll modulo=0; modulo < n; modulo+=1) cin >> a[modulo];
+    __int128 n; cin >> n;
+    __int128 n, k; cin >> n >> k;
+    __int128 n, q; cin >> n >> q;
+    __int128 a[n]; for (__int128 i=0; i < n; i+=1) cin >> a[i];
+    vector<__int128> a(n); for (__int128 i=0; i < n; i+=1) cin >> a[i];
 }
 */
-ll max_dlina_puti_so_startom_i_koncom_v[5001][5001];
-ll max_put_so_startom_v[5001];
+vll cntbycharlocal(27);
+vll cntbychar(27);
 void solve() {
-    ll n;
-    cin >> n;
-    vll a(n);
-    for (ll i = 0; i < n; i += 1) {
-        cin >> a[i];
-        max_dlina_puti_so_startom_i_koncom_v[i][i] = 1;
-    }
-    sort(all(a));
-    ll ans = -1;
-    for (ll middle = n-1; middle >= 0; middle -= 1) {
-        ll prv = middle-1;
-        for (ll j = 0; j < n; j += 1) max_put_so_startom_v[j] = 0;
-        for (ll nxt = middle; nxt < n; nxt += 1) {
-            while (prv >= 0 && a[middle]-a[prv] <= a[nxt]-a[middle]) prv -= 1;
-            if (prv == -1) break;
-            max_put_so_startom_v[prv] = max(max_put_so_startom_v[prv],
-                                max_dlina_puti_so_startom_i_koncom_v[middle][nxt]+1);
-        }
-        for (ll j = middle - 1; j >= 0; j -= 1) {
-            max_put_so_startom_v[j] = max(max_put_so_startom_v[j], max_put_so_startom_v[j+1]);
-            max_dlina_puti_so_startom_i_koncom_v[j][middle] = max_put_so_startom_v[j];
-        }
+    __int128 BASE = 31;
+    __int128 IBASE = powm(31, MOD-2);
+    ll n; cin >> n;
+    map<__int128, vector<char>> ask;
+    ll tot = 0;
+    vector<string> v(n);
+    for (auto &x : v) cin >> x;
+    sort(v.begin(), v.end(), [] (string a, string b){return a.size() < b.size();});
+    for (const auto &x : v) {
+        fill(cntbycharlocal.begin(), cntbycharlocal.end(), 0);
+        for (const auto &c : x) cntbycharlocal[c-'a'+1] = 1;
+        for (ll i = 1; i <= 26; i += 1) cntbychar[i] += cntbycharlocal[i];
     }
     for (ll i = 0; i < n; i += 1) {
-        ans = max(ans, max_dlina_puti_so_startom_i_koncom_v[0][i]);
+        ll sz = ll(v[i].size());
+        __int128 hash = 0;
+        for (ll j = sz-1; j >= 0; j -= 1) {
+            hash = mul(hash, BASE);
+            hash = sum(hash, v[i][j]-'a'+1);
+        }
+        __int128 HASH = hash;
+        set<ll> chars;
+        for (ll j = 1; j < sz; j += 1) {
+            chars.insert(v[i][j-1]-'a'+1);
+            hash = sub(hash, v[i][j-1]-'a'+1);
+            hash = mul(hash, IBASE);
+            if (!ask[hash].empty()) for (const auto &c : ask[hash]) {
+                if (chars.contains(c)) tot += 1;
+            }
+        }
+        hash = HASH;
+        if (sz > 1) {
+            ask[mul(sub(hash, v[i][0]-'a'+1), IBASE)].push_back(v[i][0]-'a'+1);
+        } else {
+            tot += cntbychar[v[i][0]-'a'+1]-1;
+        }
     }
-    cout << ans << endl;
+    cout << tot << endl;
 }
 
 int32_t main(int32_t argc, char* argv[]) {
-//    ifstream cin("distance.in");;
-//    ofstream cout("distance.out");;
+//    ifstream cin("distance.in");
+//    ofstream cout("distance.out");
     cout << fixed << setprecision(17);
     bool use_fast_io = true;
-    for (int32_t modulo = 1; modulo < argc; ++modulo) {
-        if (string(argv[modulo]) == "-local-no-fast-io") {
+    for (int32_t i = 1; i < argc; ++i) {
+        if (string(argv[i]) == "-local-no-fast-io") {
             use_fast_io = false;
 //            cout << "No fastIO" << endl;
             break;
@@ -199,8 +194,8 @@ int32_t main(int32_t argc, char* argv[]) {
         cerr.tie(nullptr);
         clog.tie(nullptr);
     }
-    ll tt = 1;
-    cin >> tt;
+    __int128 tt = 1;
+//    cin >> tt;
 
     while (tt--) {
         solve();
