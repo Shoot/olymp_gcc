0c55e93bbed0814de0acc9aa9eaed37c2943df59
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-13 11:09:58 +0300

    getline and stringstream

--- sol.cpp
+++ sol.cpp
@@ -137,110 +137,21 @@ ll sub(ll best, ll b, ll MODD) {
     return (best-(b%MODD)+MODD)%MODD;
 }
 void solve() {
-    ll n; cin >> n;
-    vll a(n); for (auto &x : a) cin >> x;
-    a.insert(a.begin(), 0ll);
-    struct shit {
-        ll val;
-        ll pos;
-        auto operator<(const shit& other) {
-            return val < other.val;
-        };
-    };
-    vector<shit> positions(n+1);
-    for (ll i = 0; i <= n; i += 1) {
-        positions[i].val = a[i];
-        positions[i].pos = i;
+    string s;
+    getline(cin, s);
+    auto stream1 = stringstream(s);
+    vll first;
+    vll second;
+    while (!stream1.eof()) {
+        ll x; stream1 >> x;
+        first.push_back(x);
     }
-    sort(all(positions));
-    vll idx_by_order(n+1);
-    for (ll i = 0; i <= n; i += 1) {
-        idx_by_order[positions[i].pos] = i;
+    getline(cin, s);
+    auto stream2 = stringstream(s);
+    while (!stream2.eof()) {
+        ll x; stream2 >> x;
+        second.push_back(x);
     }
-    vll simple_sorted_for_ub;
-    for (ll i = 0; i <= n; i += 1) simple_sorted_for_ub.push_back(positions[i].val);
-    struct node {
-        ll val=1e18;
-        ll push=0;
-    };
-    vector<node> ST_plus_val(4*n+10);
-    vector<node> ST_minus_val(4*n+10);
-    vector<node> BASIC(4*n+10);
-    auto merge = [&] (ll v, vector<node>& tree) {
-        tree[v].val = min(tree[2*v+1].val, tree[2*v+2].val);
-    };
-    auto push = [&] (ll v, vector<node>& tree) {
-        tree[2*v+1].val += tree[v].push;
-        tree[2*v+1].push += tree[v].push;
-        tree[2*v+2].val += tree[v].push;
-        tree[2*v+2].push += tree[v].push;
-        tree[v].push = 0;
-    };
-    auto set_point = [&] (auto f, ll v, ll tl, ll tr, ll pos, ll val, vector<node>& tree) -> void {
-        if (tl == tr) {
-            tree[v].val = val;
-            return;
-        }
-        push(v, tree);
-        ll tm = (tl+tr) >> 1;
-        if (pos <= tm) {
-            f(f, 2*v+1, tl, tm, pos, val, tree);
-        } else {
-            f(f, 2*v+2, tm+1, tr, pos, val, tree);
-        }
-        merge(v, tree);
-    };
-    auto add_on_segment = [&] (auto f, ll v, ll tl, ll tr, ll l, ll r, ll inc, vector<node>& tree)  -> void {
-        if (tl == l && tr == r) {
-            tree[v].val += inc;
-            tree[v].push = inc;
-            return;
-        }
-        push(v, tree);
-        ll tm = (tl+tr) >> 1;
-        if (l <= tm) {
-            f(f, 2*v+1, tl, tm, l, min(r, tm), inc, tree);
-        }
-        if (r >= tm+1) {
-            f(f, 2*v+2, tm+1, tr, max(tm+1, l), r, inc, tree);
-        }
-        merge(v, tree);
-    };
-    auto get_minimum = [&] (auto f, ll v, ll tl, ll tr, ll l, ll r, vector<node>& tree) -> ll {
-        if (l > r) return 1e18;
-        if (tl == l && tr == r) {
-            return tree[v].val;
-        }
-        push(v, tree);
-        ll tm = (tl+tr) >> 1;
-        ll x = 1e18;
-        if (l <= tm) {
-            x = min(x, f(f, 2*v+1, tl, tm, l, min(r, tm), tree));
-        }
-        if (r >= tm+1) {
-            x = min(x, f(f, 2*v+2, tm+1, tr, max(tm+1, l), r, tree));
-        }
-        return x;
-    };
-    set_point(set_point, 0, 0, n, idx_by_order[0], 0-positions[idx_by_order[0]].val, ST_minus_val);
-    set_point(set_point, 0, 0, n, idx_by_order[0], 0+positions[idx_by_order[0]].val, ST_plus_val);
-    set_point(set_point, 0, 0, n, idx_by_order[0], 0, BASIC);
-    for (ll i = 1; i <= n; i += 1) {
-        ll THIS = positions[idx_by_order[i]].val;
-        ll first_minus = upper_bound(all(simple_sorted_for_ub), THIS)-simple_sorted_for_ub.begin();
-        ll old_goes_now = 1e18;
-        old_goes_now = min(old_goes_now, get_minimum(get_minimum,0,0,n,0,first_minus-1,ST_minus_val)+THIS);
-        old_goes_now = min(old_goes_now, get_minimum(get_minimum,0,0,n,first_minus,n,ST_plus_val)-THIS);
-        ll VAL = abs(positions[idx_by_order[i]].val-positions[idx_by_order[i-1]].val);
-        add_on_segment(add_on_segment, 0, 0, n, 0, n, VAL, ST_minus_val);
-        add_on_segment(add_on_segment, 0, 0, n, 0, n, VAL, ST_plus_val);
-        add_on_segment(add_on_segment, 0, 0, n, 0, n, VAL, BASIC);
-        ll SHIT = min(get_minimum(get_minimum,0,0,n,idx_by_order[i-1],idx_by_order[i-1],BASIC), old_goes_now);
-        set_point(set_point, 0, 0, n, idx_by_order[i-1], SHIT-positions[idx_by_order[i-1]].val, ST_minus_val);
-        set_point(set_point, 0, 0, n, idx_by_order[i-1], SHIT+positions[idx_by_order[i-1]].val, ST_plus_val);
-        set_point(set_point, 0, 0, n, idx_by_order[i-1], SHIT, BASIC);
-    }
-    cout << get_minimum(get_minimum,0,0,n,0,n,BASIC) << endl;
 }
 
 
