ac1df537d3041b809bbbe329cb66eeb4078e59b7
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-14 19:56:38 +0300

    wrong case *.*.

--- sol.cpp
+++ sol.cpp
@@ -21,7 +21,7 @@ template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_ta
 //    return os << '\n';
 //}
 #define all(xxx) xxx.begin(), xxx.end()
-#define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl;
+#define watch(xxx) cout << "value of " << #xxx << " is " << xxx << endl
 template <class T, class S> inline bool chmax(T &best, const S &b) { return (best < b ? best = b, 1 : 0); }
 template <class T, class S> inline bool chmin(T &best, const S &b) { return (best > b ? best = b, 1 : 0); }
 template <typename T, typename U>
@@ -138,27 +138,115 @@ ll sub(ll best, ll b, ll MODD) {
 }
 
 void solve() {
-    ll nvm; cin >> nvm;
-    ll n; cin >> n;
-    ll curr = 0;
-    multiset<pair<ll,ll>> segments;
+    ll n, m;
+    cin >> n >> m;
+    vector<string> a(n);
+    vll leftmost_snizu(n);
+    vll leftmost_sverhu(n);
+    vll rightmost_snizu(n);
+    vll rightmost_sverhu(n);
+    for (ll i = 0; i < n; i += 1) cin >> a[i];
+    vvll points(n);
     for (ll i = 0; i < n; i += 1) {
-        ll l, r; cin >> l >> r;
-        // удаляем те у которых l принадлежит [l,r]
-        while (1) {
-            auto it = segments.lower_bound(pll(l,-1e18)); // что угодно что начинается позже или в l
-            if (it == segments.end() || it->first > r) break;
-            segments.erase(it);
+        for (ll j = 0; j < m; j += 1) {
+            if (a[i][j] == '*') points[i].push_back(j);
         }
-        // остался максимум 1 который начинается раньше l
-        auto it = segments.lower_bound(pll(l,-1e18));
-        if (it != segments.begin()) {
-            it = prev(it);
-            if (it->second >= l) segments.erase(it);
-        }
-        segments.insert(pll(l,r));
+//        if (points[i].empty() && i > 0 && !points[i-1].empty()) {
+//            points[i].push_back(points[i-1].front());
+//        }
     }
-    cout << segments.size() << endl;
+    ll curr_leftmost = 1e18;
+    ll curr_rightmost = -1e18;
+    for (ll i = 0; i < n; i += 1) {
+        for (ll j = 0; j < m; j += 1) if (a[i][j] == '*') {
+            curr_leftmost = min(curr_leftmost, j);
+            curr_rightmost = max(curr_rightmost, j);
+        }
+        leftmost_sverhu[i] = curr_leftmost;
+        rightmost_sverhu[i] = curr_rightmost;
+    }
+    curr_leftmost = 1e18;
+    curr_rightmost = -1e18;
+    for (ll i = n-1; i >= 0; i -= 1) {
+        for (ll j = 0; j < m; j += 1) if (a[i][j] == '*') {
+            curr_leftmost = min(curr_leftmost, j);
+            curr_rightmost = max(curr_rightmost, j);
+        }
+        leftmost_snizu[i] = curr_leftmost;
+        rightmost_snizu[i] = curr_rightmost;
+    }
+    vpll shit;
+    ll best_used = 1e18;
+    for (ll i = 0; i < n; i += 1) if (!points[i].empty()) {
+        ll l = points[i].front();
+        ll r = points[i].back();
+        if (leftmost_sverhu[i] >= l && rightmost_sverhu[i] <= r) {
+            // сверху втупую включаем все
+            bool vpravo = rightmost_snizu[i] > r;
+            bool vlevo = leftmost_snizu[i] < l;
+            if (vpravo && vlevo) {
+                continue;
+            }
+            watch(i);
+            watch(vpravo);
+            watch(vlevo);
+            watch(best_used);
+            ll VPRAVO = 0;
+            vpll my;
+            for (ll j = 0; j < i; j += 1) {
+                my.push_back(pll(leftmost_sverhu[j], rightmost_sverhu[j]));
+                VPRAVO += rightmost_sverhu[j]-leftmost_sverhu[j]+1;
+            }
+            my.push_back(pll(l,r));
+            VPRAVO += r-l+1;
+            ll curr_r_1 = r;
+            for (ll j = i+1; j < n; j += 1) {
+                ll curr_l_1 = leftmost_snizu[j];
+                if (!points[j].empty()) curr_r_1 = max(curr_r_1, points[j].back());
+                curr_r_1 = min(curr_r_1, rightmost_snizu[j]);
+                my.push_back(pll(curr_l_1, curr_r_1));
+                watch(curr_r_1);
+                VPRAVO += curr_r_1-curr_l_1+1;
+            }
+            if (VPRAVO < best_used) shit = my, best_used=VPRAVO;
+//            for (const auto &[left,right] : my) {
+//                cout << left << " " << right << endl;
+//            }
+            ll VLEVO = 0;
+            my.clear();
+            for (ll j = 0; j < i; j += 1) {
+                my.push_back(pll(leftmost_sverhu[j], rightmost_sverhu[j]));
+                VLEVO += rightmost_sverhu[j]-leftmost_sverhu[j]+1;
+            }
+            my.push_back(pll(l,r));
+            VLEVO += r-l+1;
+            ll curr_l_2 = l;
+            for (ll j = i+1; j < n; j += 1) {
+                ll curr_r_2 = rightmost_snizu[j];
+                if (!points[j].empty()) curr_l_2 = min(curr_l_2, points[j].front());
+                curr_l_2 = max(curr_l_2, leftmost_snizu[j]);
+                my.push_back(pll(curr_l_2, curr_r_2));
+                VPRAVO += curr_r_2-curr_l_2+1;
+            }
+            if (VLEVO < best_used) shit = my, best_used=VLEVO;
+        }
+    }
+    vector<string> ans(n);
+    for (ll i = 0; i < n; i += 1) {
+        ans[i].assign(m, '.');
+    }
+    if (!shit.empty()) {
+        for (ll i = 0; i < n; i += 1) {
+            ans[i].assign(m, '.');
+            cout << shit[i].first << " " << shit[i].second << endl;
+            for (ll j = shit[i].first; j <= shit[i].second; j += 1) {
+                ans[i][j] = '*';
+            }
+        }
+    }
+    for (const auto &x : ans) {
+        cout << x << endl;
+    }
 }
 
 int32_t main(int32_t argc, char* argv[]) {
