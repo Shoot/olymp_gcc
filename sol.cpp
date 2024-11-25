6e66d301bafb6c3afff66138d35650856871ef2c
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-25 16:53:37 +0300

    наибольшая общая подстрока

--- sol.cpp
+++ sol.cpp
@@ -20,8 +20,8 @@ template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_ta
 ostream& endl(ostream& os) {
     return os << '\n';
 }
-#define all(xxx) xxx.begin(), xxx.end()
-#define watch(xxx) cout << "value of " << #xxx << " is " << (xxx) << endl
+//define all(xxx) xxx.begin(), xxx.end()
+//define watch(xxx) cout << "value of " << #xxx << " is " << (xxx) << endl
 template <class T, class S> inline bool chmax(T &best, const S &b) { return (best < b ? best = b, 1 : 0); }
 template <class T, class S> inline bool chmin(T &best, const S &b) { return (best > b ? best = b, 1 : 0); }
 template <typename T, typename U>
@@ -63,24 +63,11 @@ void print(Head&& head, Tail&&... tail) {
 #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 uniform_int_distribution<ll> distrib(0ll, 5ll);
-//constexpr ll MOD = 819356875157278019ll;
-constexpr ll MOD = 1e9+7;
-void in(vector<ll> & best) {
-    for (auto & zero_leaf : best) cin >> zero_leaf;
-}
-void in(vector<ll> & best, ll l, ll r) {
-    for (ll i=l; i < r; i+=1) {
-        cin >> best[i];
-    }
-}
-void inn(vector<ll> & best, ll l, ll rr) {
-    for (ll i=l; i <= rr; i+=1) {
-        cin >> best[i];
-    }
-}
-ll powm(ll best, ll b) {
+//constexpr __int128 MOD = 819356875157278019ll;
+constexpr __int128 MOD = 957363431467152001;
+__int128 powm(__int128 best, __int128 b) {
     assert(b >= 0);
-    ll d = 1;
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*best) % MOD;
         b >>= 1;
@@ -88,9 +75,9 @@ ll powm(ll best, ll b) {
     }
     return d;
 }
-ll powm(ll best, ll b, ll MODD) {
+__int128 powm(__int128 best, __int128 b, __int128 MODD) {
     assert(b >= 0);
-    ll d = 1;
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*best) % MODD;
         b >>= 1;
@@ -98,9 +85,9 @@ ll powm(ll best, ll b, ll MODD) {
     }
     return d;
 }
-ll poww(ll best, ll b) {
+__int128 poww(__int128 best, __int128 b) {
     assert(b >= 0);
-    ll d = 1;
+    __int128 d = 1;
     while (b) {
         if (b&1) d = (d*best);
         b >>= 1;
@@ -108,7 +95,7 @@ ll poww(ll best, ll b) {
     }
     return d;
 }
-ld poww(ld best, ll b) {
+ld poww(ld best, __int128 b) {
     assert(b >= 0);
     ld d = 1;
     while (b) {
@@ -118,78 +105,100 @@ ld poww(ld best, ll b) {
     }
     return d;
 }
-ll mul(ll best, ll b) {
+__int128 mul(__int128 best, __int128 b) {
     return (best*b)%MOD;
 }
-ll mul(ll best, ll b, ll MODD) {
+__int128 mul(__int128 best, __int128 b, __int128 MODD) {
     return (best*b)%MODD;
 }
-ll sum(ll best, ll b) {
+__int128 sum(__int128 best, __int128 b) {
     return (best+b)%MOD;
 }
-ll sum(ll best, ll b, ll MODD) {
+__int128 sum(__int128 best, __int128 b, __int128 MODD) {
     return (best+b)%MODD;
 }
-ll sub(ll best, ll b) {
+__int128 sub(__int128 best, __int128 b) {
     return (best-(b%MOD)+MOD)%MOD;
 }
-ll sub(ll best, ll b, ll MODD) {
+__int128 sub(__int128 best, __int128 b, __int128 MODD) {
     return (best-(b%MODD)+MODD)%MODD;
 }
+namespace std {
+    template <>
+    struct hash<__int128> {
+        std::size_t operator()(__int128 value) const {
+            // Simple hash function: mix the bits of __int128 into a size_t
+            uint64_t high = static_cast<uint64_t>(value >> 64);
+            uint64_t low = static_cast<uint64_t>(value);
+            return std::hash<uint64_t>()(high) ^ (std::hash<uint64_t>()(low) << 1);
+        }
+    };
+} // namespace std
+__int128 MAXN = 2e5+10;
+__int128 base = MAXN;
+__int128 revbase7 = powm(base, MOD-2, MOD);
+vector<__int128> base_power7(MAXN, 1);
+vector<__int128> base_rev_power7(MAXN, 1);
 void solve() {
+    for (ll i = 1; i < MAXN; i += 1) {
+        base_power7[i] = (base_power7[i-1]*base)%MOD;
+        base_rev_power7[i] = (base_rev_power7[i-1]*revbase7)%MOD;
+    }
     ll n; cin >> n;
-    ll msb = -1;
-    for (ll i = 60; i >= 0; i -= 1) {
-        ll nvml = (1ll << i);
-        ll nvmr = min(n, (1ll << (i+1))-1);
-        if (nvml > nvmr) continue;
-        cout << "xor " << nvml << " " << nvmr << endl;
-        cout.flush();
-        ll ans; cin >> ans;
-        if (ans) {
-            msb = i;
-            break;
+    vector<string> shit(n);
+    vvll pref(n);
+    vll sz(n);
+    for (ll i = 0; i < n; i += 1) {
+        cin >> shit[i];
+        sz[i] = shit[i].size();
+        pref[i].resize(sz[i]+1);
+    }
+    for (ll x = 0; x < n; x += 1) {
+        for (ll i = 0; i < sz[x]; i += 1) {
+            pref[x][i+1] = sum(pref[x][i], mul(__int128(shit[x][i]-'a'+1), base_power7[i]), MOD);
         }
     }
-    assert(msb != -1);
-    ll biggest_non_zero = -1;
-    ll l = 1ll << msb;
-    ll r = min(n, (1ll << (msb+1))-1);
+    ll l = 1, r = *min_element(sz.begin(), sz.end());
+    string ans;
     while (l <= r) {
-        ll mid = (l + r) >> 1;
-        cout << "xor " << mid << " " << min(n, (1ll << (msb+1))-1) << endl;
-        cout.flush();
-        ll ans; cin >> ans;
-        if (ans) {
-            biggest_non_zero = mid;
+        ll mid = (l+r) >> 1;
+        unordered_map<__int128, ll> st;
+        for (ll x = 0; x < n; x += 1) {
+            unordered_set<__int128> st_local;
+            for (ll i = 0; i < sz[x]+1-mid; i += 1) {
+                __int128 val = (((pref[x][i+mid]-pref[x][i]+MOD)%MOD)*base_rev_power7[i])%MOD;
+                st_local.insert(val);
+            }
+            for (const auto &z : st_local) {
+                st[z] += 1;
+            }
+        }
+        bool intersected7 = false;
+        __int128 got = -1;
+        for (const auto &[i, j] : st) {
+            assert(j <= n);
+            if (j == n) {
+                got = i;
+                intersected7 = true;
+                break;
+            }
+        }
+        if (intersected7) {
             l = mid+1;
+            ll x = 0;
+            unordered_set<__int128> st_local;
+            for (ll i = 0; i < sz[x]+1-mid; i += 1) {
+                __int128 val = (((pref[x][i+mid]-pref[x][i]+MOD)%MOD)*base_rev_power7[i])%MOD;
+                if (val == got) {
+                    ans = shit[x].substr(i, mid);
+                }
+                st_local.insert(val);
+            }
         } else {
             r = mid-1;
         }
     }
-    assert(biggest_non_zero != -1);
-    ll A = biggest_non_zero;
-    ll first_non_zero = -1;
-    ll L = 1; ll R = A-1;
-    while (L <= R) {
-        ll MID = (L + R) >> 1;
-        cout << "xor 1 " << MID << endl;
-        cout.flush();
-        ll ans; cin >> ans;
-        if (ans != 0) {
-            first_non_zero = MID;
-            R = MID-1;
-        } else {
-            L = MID+1;
-        }
-    }
-    assert(first_non_zero != -1);
-    ll B = first_non_zero;
-    cout << "xor 1 " << n << endl;
-    cout.flush();
-    ll ans; cin >> ans;
-    cout << "ans " << A << " " << B << " " << (ans^A^B) << endl;
-    cout.flush();
+    cout << ans << endl;
 }
 
 int32_t main(int32_t argc, char* argv[]) {
@@ -212,7 +221,7 @@ int32_t main(int32_t argc, char* argv[]) {
         clog.tie(nullptr);
     }
     ll tt = 1;
-    cin >> tt;
+//    cin >> tt;
 
     while (tt--) {
         solve();
