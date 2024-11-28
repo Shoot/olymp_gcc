ac5893729289ffa4ad0295260064185cf1f15acf
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-28 08:55:25 +0300

    рефрен (отрезок с наибольшим значением минимум*длина @ lcp)

--- sol.cpp
+++ sol.cpp
@@ -1,84 +1,277 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <set>
-#include <functional>
-#include <random>
-#include <chrono>
-#include <bitset>
-#include <assert.h>
+#include <bits/stdc++.h>
 using namespace std;
+using vbo = vector<bool>;
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using ld = long double;
+using qll = queue<ll>;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using vvpll = vector<vector<pll>>;
+using qld = queue<ld>;
+using vld = vector<ld>;
+using qpll = queue<pll>;
+using vpll = vector<pll>;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ostream& endl(ostream& os) {
+    return os << '\n';
+}
+//define all(xxx) xxx.begin(), xxx.end()
+//define watch(xxx) cout << "value of " << #xxx << " is " << (xxx) << endl
+template <class T, class S> inline bool chmax(T &best, const S &b) { return (best < b ? best = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &best, const S &b) { return (best > b ? best = b, 1 : 0); }
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {
+    os << A.fi << " " << A.se;
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+    for (size_t i = 0; i < A.size(); i++) {
+        if (i) os << " ";
+        os << A[i];
+    }
+    return os;
+}
+void scan(ll &best) { cin >> best; }
+void scan(char &best) { cin >> best; }
+void scan(double &best) { cin >> best; }
+void scan(long double &best) { cin >> best; }
+void scan(string &best) { cin >> best; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &best) {for(auto &i : best) scan(i);}
+template <class T> void scan(T &best) { cin >> best; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+void print() {
+    cout << "\n";
+}
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+    cout << head;
+    if (sizeof...(Tail)) cout << " ";
+    print(forward<Tail>(tail)...);
+}
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math,trapv")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<ll> distrib(0ll, 5ll);
+//constexpr ll MOD = 819356875157278019ll;
+constexpr ll MOD = 1e9+7;
+void in(vector<ll> & best) {
+    for (auto & zero_leaf : best) cin >> zero_leaf;
+}
+void in(vector<ll> & best, ll l, ll r) {
+    for (ll i=l; i < r; i+=1) {
+        cin >> best[i];
+    }
+}
+void inn(vector<ll> & best, ll l, ll rr) {
+    for (ll i=l; i <= rr; i+=1) {
+        cin >> best[i];
+    }
+}
+ll powm(ll best, ll b) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*best) % MOD;
+        b >>= 1;
+        best = (best*best) % MOD;
+    }
+    return d;
+}
+ll powm(ll best, ll b, ll MODD) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*best) % MODD;
+        b >>= 1;
+        best = (best*best) % MODD;
+    }
+    return d;
+}
+ll poww(ll best, ll b) {
+    assert(b >= 0);
+    ll d = 1;
+    while (b) {
+        if (b&1) d = (d*best);
+        b >>= 1;
+        best = (best*best);
+    }
+    return d;
+}
+ld poww(ld best, ll b) {
+    assert(b >= 0);
+    ld d = 1;
+    while (b) {
+        if (b&1) d = (d*best);
+        b >>= 1;
+        best = (best*best);
+    }
+    return d;
+}
+ll mul(ll best, ll b) {
+    return (best*b)%MOD;
+}
+ll mul(ll best, ll b, ll MODD) {
+    return (best*b)%MODD;
+}
+ll sum(ll best, ll b) {
+    return (best+b)%MOD;
+}
+ll sum(ll best, ll b, ll MODD) {
+    return (best+b)%MODD;
+}
+ll sub(ll best, ll b) {
+    return (best-(b%MOD)+MOD)%MOD;
+}
+ll sub(ll best, ll b, ll MODD) {
+    return (best-(b%MODD)+MODD)%MODD;
+}
 void solve() {
-    int n; cin >> n;
-    vector<multiset<int>> sm(n+1);
-    for (int i = 1; i <= n; i += 1) {
-        for (int j = 1; j <= n; j += 1) {
-            int x; cin >> x;
-            if (x != 0) {
-                assert(x == 1);
-                assert(i != j);
-                sm[i].insert(j);
+    ll n; cin >> n;
+    ll q; cin >> q;
+    vll s(n);
+    for (auto &x : s) cin >> x;
+    s.push_back(0);
+    n = ll(s.size());
+    vll rank(n);
+    vll sa(n);
+    vll lcp(n - 1);
+    ll alphabet = 256;
+    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++)
+        cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++)
+        cnt[i] += cnt[i - 1];
+    for (int i = 0; i < n; i++)
+        p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i - 1]])
+            classes++;
+        c[p[i]] = classes - 1;
+    }
+    vector<int> pn(n), cn(n);
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0)
+                pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++)
+            cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++)
+            cnt[i] += cnt[i - 1];
+        for (int i = n - 1; i >= 0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
+            if (cur != prev)
+                ++classes;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    for (ll i = 0; i < n; i += 1) {
+        rank[i] = c[i];
+        sa[c[i]] = i;
+    }
+    ll k = 0;
+    for (ll i = 0; i < n; i += 1, k ? k -= 1 : 0) {
+        if (rank[i] == n-1) {
+            k = 0;
+            continue;
+        }
+        ll j = sa[rank[i]+1];
+        while (i+k < n && j+k < n && s[i+k] == s[j+k]) k++;
+        lcp[rank[i]] = k;
+    }
+    stack<ll> st;
+    ll maxi = n-1;
+    ll best_l = 0;
+    ll best_r = n-2;
+    for (ll i = 0; i < ll(lcp.size()); i += 1) {
+        while (!st.empty() && lcp[st.top()] > lcp[i]) {
+            ll x = st.top();
+            st.pop();
+            ll length;
+            if (!st.empty()) {
+                length = i-st.top();
+            } else {
+                length = i+1;
+            }
+            ll val = lcp[x];
+            if (val*length > maxi) {
+                maxi = val*length;
+                ll where = sa[x];
+                best_l = where;
+                best_r = where+val-1;
             }
         }
+        st.push(i);
     }
-    bitset<1'000'000> seen;
-    auto dfs = [&] (auto f, int v) -> void {
-        seen[v] = true;
-        for (const auto &x : sm[v]) {
-            if (!seen[x]) {
-                seen[x] = true;
-                f(f, x);
-            }
+    while (!st.empty()) {
+        ll x = st.top();
+        ll val = lcp[x];
+        st.pop();
+        ll length;
+        if (!st.empty()) {
+            length = lcp.size()-st.top();
+        } else {
+            length = lcp.size()+1;
         }
-    };
-    dfs(dfs, 1);
-    for (int i = 1; i <= n; i += 1) {
-        if (!seen[i]) {
-            cout << 0 << endl;
-            return;
-        }
-    }
-    vector<int> nech_vs;
-    for (int i = 1; i <= n; i += 1) {
-        if (sm[i].size()%2) {
-            nech_vs.push_back(i);
-            clog << i << " is nech" << endl;
+        if (val*length > maxi) {
+            maxi = val*length;
+            ll where = sa[x];
+            best_l = where;
+            best_r = where+val-1;
         }
     }
-    if (nech_vs.size() > 2) {
-        cout << 0 << endl;
-        return;
-    }
-    vector<int> ans;
-    auto find_euler_path = [&] (auto f, int v) -> void {
-        while (!sm[v].empty()) {
-            auto it = sm[v].begin();
-            int x = *it;
-            auto it1 = sm[v].find(x);
-            auto it2 = sm[x].find(v);
-            assert(it1 != sm[v].end());
-            assert(it2 != sm[x].end());
-            sm[v].erase(it1);
-            sm[x].erase(it2);
-            f(f, x);
-        }
-        ans.push_back(v);
-    };
-    if (nech_vs.size() == 2) {
-        find_euler_path(find_euler_path, min(nech_vs[0], nech_vs[1]));
-        reverse(ans.begin(), ans.end());
-    } else {
-        assert(nech_vs.size() == 0);
-        find_euler_path(find_euler_path, 1);
-        reverse(ans.begin(), ans.end());
-    }
-    for (const auto &x : ans) {
-        cout << x << ' ';
+    cout << maxi << endl;
+    cout << best_r-best_l+1 << endl;
+    for (ll i = best_l; i <= best_r; i += 1) {
+        cout << s[i] << ' ';
     }cout << endl;
 }
 
-int32_t main() {
-    solve();
+int32_t main(int32_t argc, char* argv[]) {
+//    ifstream cin("distance.in");
+//    ofstream cout("distance.out");
+    cout << fixed << setprecision(17);
+    bool use_fast_io = true;
+    for (int32_t i = 1; i < argc; ++i) {
+        if (string(argv[i]) == "-local-no-fast-io") {
+            use_fast_io = false;
+//            cout << "No fastIO" << endl;
+            break;
+        }
+    }
+    if (use_fast_io) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        cerr.tie(nullptr);
+        clog.tie(nullptr);
+    }
+    ll tt = 1;
+//    cin >> tt;
+
+    while (tt--) {
+        solve();
+    }
     return 0;
 }
