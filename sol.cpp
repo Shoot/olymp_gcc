f5ebd0cb8782c36f8f2b5a88668931880e9116fd
Shoot <11522779+Shoot@users.noreply.github.com>
2024-11-29 12:39:26 +0300

    Диаметр взвеш графа где длина пути это максимум среди весов ребер на пути

--- sol.cpp
+++ sol.cpp
@@ -1,90 +1,79 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <functional>
+#include <random>
+#include <chrono>
+#include <bitset>
+#include <assert.h>
 using namespace std;
-using ll = long long;
-using pll = pair<ll, ll>;
-using vll = vector<ll>;
-using vpll = vector<pll>;
-using vvll = vector<vector<ll>>;
-using vvpll = vector<vector<pll>>;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<int> distrib(1, 10);
 void solve() {
-    ll INF = 1e18;
-    ll n, m, k;
-    cin >> n >> m >> k;
-    vvpll sm(n+1);
-    struct edge {
-        ll u, v, w, idx;
-    };
-    vector<edge> edges(m);
-    ll nvm = 0;
-    for (auto &edge : edges) {
-        nvm += 1;
-        edge.idx = nvm;
-        cin >> edge.u >> edge.v >> edge.w;
-        edge.w = -edge.w;
-        sm[edge.u].push_back(pll(edge.v, edge.w));
-    }
-    vll need(k);
-    for (auto &x : need) cin >> x;
-    vvll D(n+1, vll(n+1, INF));
-    vvll EDGE(n+1, vll(n+1, INF));
-    vvll nxt(n+1, vll(n+1, INF));
-    for (ll i = 1; i <= n; i += 1) {
-        D[i][i] = 0;
-        nxt[i][i] = i;
-    }
-    for (const auto &[u, v, w, idx] : edges) {
-        if (D[u][v] > w) {
-            D[u][v] = w;
-            EDGE[u][v] = idx;
-            nxt[u][v] = v;
+    int n; cin >> n;
+    vector<vector<int>> d(n, vector<int>(n));
+    int maxi = 0;
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < n; j += 1) {
+            cin >> d[i][j];
+            maxi = max(maxi, d[i][j]);
         }
     }
-    vll ans;
-    for (ll i = 1; i <= n; i += 1) {
-        for (ll u = 1; u <= n; u += 1) {
-            for (ll v = 1; v <= n; v += 1) {
-                if (D[u][i] + D[i][v] < D[u][v]) {
-                    if (D[u][i] < INF && D[i][v] < INF) {
-                        // чтобы не делать тупых обновлений из-за отриц ребер
-                        // то есть может получится < INF а пути нету!!
-                        D[u][v] = D[u][i] + D[i][v];
-                        nxt[u][v] = nxt[u][i];
-                        if (D[u][v] < -INF) D[u][v] = -INF;
-                    }
+    int l = 0, r = maxi;
+    int good = -1;
+    auto can = [&] (int x) -> bool {
+        vector<vector<int>> sm(n);
+        vector<vector<int>> sm_rev(n);
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (d[i][j] <= x) sm[i].push_back(j), sm_rev[j].push_back(i);
+            }
+        }
+        vector<bool> seen(n, false);
+        auto dfs = [&] (auto f, int v) -> void {
+            seen[v] = true;
+            for (const auto &x : sm[v]) {
+                if (!seen[x]) {
+                    seen[x] = true;
+                    f(f, x);
                 }
             }
+        };
+        dfs(dfs, 0);
+        for (int i = 0; i < n; i += 1) {
+            if (!seen[i]) return false;
         }
-    }
-    bool FARM_IT = false;
-    auto go_to = [&] (ll from, ll to) -> void {
-        while (from != to) {
-            ans.push_back(EDGE[from][nxt[from][to]]);
-            from = nxt[from][to];
-        }
-    };
-    for (ll i = 1; i < k; i += 1) {
-        ll from = need[i-1];
-        ll to = need[i];
-        for (ll s = 1; s <= n; s += 1) {
-            FARM_IT |= D[from][s] < INF && D[s][to] < INF && D[s][s] < 0;
-            if (FARM_IT) {
-                cout << "infinitely kind" << endl;
-                return;
+        fill(seen.begin(), seen.end(), false);
+        auto dfs_rev = [&] (auto f, int v) -> void {
+            seen[v] = true;
+            for (const auto &x : sm_rev[v]) {
+                if (!seen[x]) {
+                    seen[x] = true;
+                    f(f, x);
+                }
             }
+        };
+        dfs_rev(dfs_rev, 0);
+        for (int i = 0; i < n; i += 1) {
+            if (!seen[i]) return false;
         }
-        go_to(from, to);
+        return true;
+    };
+    while (l <= r) {
+        int mid = (l + r) >> 1;
+        if (can(mid)) {
+            r = mid - 1;
+            good = mid;
+        } else {
+            l = mid + 1;
+        }
     }
-    if (FARM_IT) {
-        cout << "infinitely kind" << endl;
-        return;
-    }
-    cout << ans.size() << endl;
-    for (const auto &x : ans) {
-        cout << x << ' ';
-    }cout << endl;
+    cout << good << endl;
 }
-int main()
-{
+
+int32_t main() {
     solve();
     return 0;
 }
