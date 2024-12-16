0dad42d3088d128a7b1550964484284ba8dbbd8d
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-17 02:19:02 +0300

    в онлайне колво непересекающихся черных отрезков и длина

--- sol.cpp
+++ sol.cpp
@@ -1,116 +1,126 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
+#define endl "\n"
+using ll = long long;
+const ll INF = 1e18;
+mt19937 mt(time(0));
 
-// Функция кодирования данных с использованием кода Хэмминга
-vector<int> hammingEncode(const vector<int>& data) {
-    int m = data.size(); // Количество бит данных
-    int r = 0; // Количество контрольных бит
-
-    // Вычисляем количество контрольных бит
-    while (pow(2, r) < (m + r + 1)) {
-        r++;
-    }
-
-    int totalBits = m + r; // Общее количество бит (данные + контрольные биты)
-    vector<int> encoded(totalBits, 0);
-
-    // Распределяем данные по позициям (пропускаем позиции для контрольных бит)
-    int j = 0;
-    for (int i = 1; i <= totalBits; ++i) {
-        if ((i & (i - 1)) == 0) {
-            // Позиция степени двойки (контрольный бит)
-            continue;
+void solve() {
+    ll q;
+    cin >> q;
+    multiset<pair<long long, long long>> b_segments; // non-intersecting !!!
+    ll b_length = 0;
+    auto nvm_black = [&](ll &l, ll &r) -> void {
+        while (1) { // fully inside || sprava
+            auto it = b_segments.lower_bound({l, -INF});
+            if (it == b_segments.end()) {
+                break;
+            }
+            if (it->second > r) { // sprava
+                if (it->first <= r + 1) {
+                    l = min(l, it->first);
+                    r = max(r, it->second);
+                    ll prev_l = it->second - it->first + 1;
+                    b_length -= prev_l;
+                    b_segments.erase(it);
+                }
+                break;
+            }
+            // fully inside
+            b_length -= it->second - it->first + 1;
+            b_segments.erase(it);
         }
-        encoded[i - 1] = data[j++];
-    }
-
-    // Устанавливаем значения контрольных бит
-    for (int i = 0; i < r; ++i) {
-        int position = pow(2, i);
-        int parity = 0;
-        for (int j = 1; j <= totalBits; ++j) {
-            if (j & position) {
-                parity ^= encoded[j - 1];
+        auto it = b_segments.lower_bound({l, -INF});
+        if (it != b_segments.begin()) { // sleva
+            it = prev(it);
+            if (it->second >= l - 1) {
+                l = min(l, it->first);
+                r = max(r, it->second);
+                ll prev_l = it->second - it->first + 1;
+                b_length -= prev_l;
+                b_segments.erase(it);
             }
         }
-        encoded[position - 1] = parity;
-    }
-
-    return encoded;
-}
-
-// Функция декодирования и проверки ошибок
-vector<int> hammingDecode(vector<int>& received) {
-    int totalBits = received.size();
-    int r = 0;
-
-    // Вычисляем количество контрольных бит
-    while (pow(2, r) < (totalBits + 1)) {
-        r++;
-    }
-
-    // Проверяем контрольные биты на ошибки
-    int errorPosition = 0;
-    for (int i = 0; i < r; ++i) {
-        int position = pow(2, i);
-        int parity = 0;
-        for (int j = 1; j <= totalBits; ++j) {
-            if (j & position) {
-                parity ^= received[j - 1];
+    };
+    while (q--) {
+        char c;
+        cin >> c;
+        ll l, d;
+        cin >> l >> d;
+        ll r = l + d - 1;
+        if (c == 'W') {
+            while (1) { // fully inside || sprava
+                auto it = b_segments.lower_bound({l, -INF});
+                if (it == b_segments.end()) {
+                    break;
+                }
+                if (it->second > r) { // sprava
+                    if (it->first <= r) {
+                        pair<ll, ll> nw = {r + 1, it->second};
+                        ll prev_l = it->second - it->first + 1;
+                        ll nw_l = nw.second - nw.first + 1;
+                        assert(nw_l < prev_l);
+                        assert(nw_l > 0);
+                        b_segments.erase(it);
+                        b_segments.insert(nw);
+                        b_length -= prev_l;
+                        b_length += nw_l;
+                    }
+                    break;
+                }
+                // fully inside
+                b_length -= it->second - it->first + 1;
+                b_segments.erase(it);
             }
+            auto it = b_segments.lower_bound({l, -INF});
+            if (it != b_segments.begin()) { // sleva
+                it = prev(it);
+                if (it->second >= l) {
+                    if (it->second > r) {
+                        // BBBwwBBB
+                        //    !!
+                        pair<ll, ll> nw1 = {it->first, l - 1};
+                        pair<ll, ll> nw2 = {r+1, it->second};
+                        ll prev_l = it->second - it->first + 1;
+                        ll nw1_l = nw1.second - nw1.first + 1;
+                        ll nw2_l = nw2.second - nw2.first + 1;
+                        assert(nw1_l < prev_l);
+                        assert(nw1_l > 0);
+                        assert(nw2_l > 0);
+                        b_segments.erase(it);
+                        b_segments.insert(nw1);
+                        b_segments.insert(nw2);
+                        b_length -= prev_l;
+                        b_length += nw1_l;
+                        b_length += nw2_l;
+                    } else {
+                        pair<ll, ll> nw = {it->first, l - 1};
+                        ll prev_l = it->second - it->first + 1;
+                        ll nw_l = nw.second - nw.first + 1;
+                        assert(nw_l < prev_l);
+                        assert(nw_l > 0);
+                        b_segments.erase(it);
+                        b_segments.insert(nw);
+                        b_length -= prev_l;
+                        b_length += nw_l;
+                    }
+                }
+            }
+        } else {
+            nvm_black(l, r);
+            b_segments.insert({l, r});
+            b_length += r - l + 1;
         }
-        if (parity != 0) {
-            errorPosition += position;
-        }
+        cout << b_segments.size() << " " << b_length << endl;
     }
-
-    // Исправляем ошибку, если обнаружена
-    if (errorPosition > 0) {
-        cout << "Ошибка обнаружена в позиции " << errorPosition << ". Исправляем...\n";
-        received[errorPosition - 1] ^= 1;
-    } else {
-        cout << "Ошибок не обнаружено.\n";
-    }
-
-    // Извлекаем данные, исключая контрольные биты
-    vector<int> data;
-    for (int i = 1; i <= totalBits; ++i) {
-        if ((i & (i - 1)) != 0) {
-            data.push_back(received[i - 1]);
-        }
-    }
-
-    return data;
 }
 
-// Главная функция
 int main() {
-    // Пример данных для кодирования
-    vector<int> data = {1, 0, 1, 1};
-    cout << "Исходные данные: ";
-    for (int bit : data) cout << bit;
-    cout << endl;
-
-    // Кодирование
-    vector<int> encoded = hammingEncode(data);
-    cout << "Закодированные данные: ";
-    for (int bit : encoded) cout << bit;
-    cout << endl;
-
-    // Добавляем ошибку для проверки декодирования
-    encoded[6] ^= 1; // Инвертируем один бит (вносим ошибку)
-    cout << "Принятые данные (с ошибкой): ";
-    for (int bit : encoded) cout << bit;
-    cout << endl;
-
-    // Декодирование
-    vector<int> decoded = hammingDecode(encoded);
-    cout << "Декодированные данные: ";
-    for (int bit : decoded) cout << bit;
-    cout << endl;
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll tt = 1;
+    while (tt--)
+        solve();
     return 0;
 }
