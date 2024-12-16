aac8550db2581b9a17b2f12fd356f414b6240b59
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-17 02:19:36 +0300

    treap на массивеgit add .(AC@ejudge)

--- sol.cpp
+++ sol.cpp
@@ -5,120 +5,113 @@ using namespace std;
 using ll = long long;
 const ll INF = 1e18;
 mt19937 mt(time(0));
-
+struct Node {
+    ll val;
+    ll y;
+    Node* l;
+    Node* r;
+    ll mn;
+    ll sz;
+    Node (ll val) : val(val), y(mt()), mn(val), sz(1), l(0), r(0){};
+};
 void solve() {
-    ll q;
-    cin >> q;
-    multiset<pair<long long, long long>> b_segments; // non-intersecting !!!
-    ll b_length = 0;
-    auto nvm_black = [&](ll &l, ll &r) -> void {
-        while (1) { // fully inside || sprava
-            auto it = b_segments.lower_bound({l, -INF});
-            if (it == b_segments.end()) {
-                break;
-            }
-            if (it->second > r) { // sprava
-                if (it->first <= r + 1) {
-                    l = min(l, it->first);
-                    r = max(r, it->second);
-                    ll prev_l = it->second - it->first + 1;
-                    b_length -= prev_l;
-                    b_segments.erase(it);
-                }
-                break;
-            }
-            // fully inside
-            b_length -= it->second - it->first + 1;
-            b_segments.erase(it);
+    auto getsz = [&] (Node* v) -> ll {
+        if (!v) return 0;
+        return v->sz;
+    };
+    auto getmn = [&] (Node* v) -> ll {
+        if (!v) return INF;
+        return v->mn;
+    };
+    auto recalc = [&] (Node* v) -> void {
+        if (!v) return;
+        v->sz = 1+getsz(v->l)+getsz(v->r);
+        v->mn = min({v->val, getmn(v->l), getmn(v->r)});
+    };
+    auto merge = [&] (auto f, Node* l, Node* r) -> Node* {
+        assert(l || r);
+        if (!l) {
+            return r;
         }
-        auto it = b_segments.lower_bound({l, -INF});
-        if (it != b_segments.begin()) { // sleva
-            it = prev(it);
-            if (it->second >= l - 1) {
-                l = min(l, it->first);
-                r = max(r, it->second);
-                ll prev_l = it->second - it->first + 1;
-                b_length -= prev_l;
-                b_segments.erase(it);
-            }
+        if (!r) {
+            return l;
+        }
+        assert(l->y != r->y);
+        if (l->y > r->y) {
+            l->r = f(f, l->r, r);
+            recalc(l);
+            return l;
+        } else {
+            r->l = f(f, l, r->l);
+            recalc(r);
+            return r;
         }
     };
-    while (q--) {
-        char c;
-        cin >> c;
-        ll l, d;
-        cin >> l >> d;
-        ll r = l + d - 1;
-        if (c == 'W') {
-            while (1) { // fully inside || sprava
-                auto it = b_segments.lower_bound({l, -INF});
-                if (it == b_segments.end()) {
-                    break;
-                }
-                if (it->second > r) { // sprava
-                    if (it->first <= r) {
-                        pair<ll, ll> nw = {r + 1, it->second};
-                        ll prev_l = it->second - it->first + 1;
-                        ll nw_l = nw.second - nw.first + 1;
-                        assert(nw_l < prev_l);
-                        assert(nw_l > 0);
-                        b_segments.erase(it);
-                        b_segments.insert(nw);
-                        b_length -= prev_l;
-                        b_length += nw_l;
-                    }
-                    break;
-                }
-                // fully inside
-                b_length -= it->second - it->first + 1;
-                b_segments.erase(it);
-            }
-            auto it = b_segments.lower_bound({l, -INF});
-            if (it != b_segments.begin()) { // sleva
-                it = prev(it);
-                if (it->second >= l) {
-                    if (it->second > r) {
-                        // BBBwwBBB
-                        //    !!
-                        pair<ll, ll> nw1 = {it->first, l - 1};
-                        pair<ll, ll> nw2 = {r+1, it->second};
-                        ll prev_l = it->second - it->first + 1;
-                        ll nw1_l = nw1.second - nw1.first + 1;
-                        ll nw2_l = nw2.second - nw2.first + 1;
-                        assert(nw1_l < prev_l);
-                        assert(nw1_l > 0);
-                        assert(nw2_l > 0);
-                        b_segments.erase(it);
-                        b_segments.insert(nw1);
-                        b_segments.insert(nw2);
-                        b_length -= prev_l;
-                        b_length += nw1_l;
-                        b_length += nw2_l;
-                    } else {
-                        pair<ll, ll> nw = {it->first, l - 1};
-                        ll prev_l = it->second - it->first + 1;
-                        ll nw_l = nw.second - nw.first + 1;
-                        assert(nw_l < prev_l);
-                        assert(nw_l > 0);
-                        b_segments.erase(it);
-                        b_segments.insert(nw);
-                        b_length -= prev_l;
-                        b_length += nw_l;
-                    }
-                }
-            }
-        } else {
-            nvm_black(l, r);
-            b_segments.insert({l, r});
-            b_length += r - l + 1;
+    /*auto split = [&] (auto f, Node* v, ll key) -> pair<Node*, Node*> {
+        if (!v) {
+            return {};
         }
-        cout << b_segments.size() << " " << b_length << endl;
+        assert(v->x != key);
+        if (v->x < key) { // pravoe rezhem
+            pair<Node*, Node*> xxx = f(f, v->r, key);
+            v->r = xxx.first;
+            recalc(*v);
+            return {v, xxx.second};
+        } else { // levoe rezhem
+            pair<Node*, Node*> xxx = f(f, v->l, key);
+            v->l = xxx.second;
+            recalc(*v);
+            return {xxx.first, v};
+        }
+    };*/
+    auto splitsz = [&] (auto f, Node* v, ll sz) -> pair<Node*, Node*> {
+        if (!v) {
+            return {};
+        }
+        if (getsz(v->l) >= sz) { // levoe rezhem
+            pair<Node*, Node*> xxx = f(f, v->l, sz);
+            v->l = xxx.second;
+            recalc(v);
+            return {xxx.first, v};
+        } else { // pravoe rezhem
+            pair<Node*, Node*> xxx = f(f, v->r, sz-getsz(v->l)-1/*Ð¼Ð¸Ð½ÑƒÑ ÐºÐ¾Ñ€ÐµÐ½ÑŒ Ð¸ Ð»ÐµÐ²Ð¾Ðµ Ð¿Ð¾Ð´Ð´ÐµÑ€ÐµÐ²Ð¾*/);
+            v->r = xxx.first;
+            recalc(v);
+            return {v, xxx.second};
+        }
+    };
+    auto print_subtree = [&] (auto f, Node* v, bool r) -> void {
+        if (!v) return;
+        f(f, v->l, false);
+        f(f, v->r, false);
+    };
+    ll q; cin >> q;
+    Node* t = nullptr;
+    for (ll i = 0; i < q; i += 1) {
+        char tp; cin >> tp;
+        if (tp == '+') {
+            ll idx, val; cin >> idx >> val;
+            auto [t1, t2] = splitsz(splitsz, t, idx);
+            assert(getsz(t1) == idx);
+            t1 = merge(merge, t1, new Node(val));
+            t = merge(merge, t1, t2);
+        } else {
+            ll l, r; cin >> l >> r;
+            auto [t0, t1] = splitsz(splitsz, t, l-1);
+            assert(getsz(t0) == l-1);
+            auto [t1t, t2] = splitsz(splitsz, t1, r-l+1);
+            t1 = t1t;
+            assert(getsz(t1) == r-l+1);
+            cout << t1->mn << "\n";
+            t = merge(merge, t0, t1);
+            t = merge(merge, t, t2);
+        }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+    //ios::sync_with_stdio(false);
+    //cin.tie(0);
     ll tt = 1;
     while (tt--)
         solve();
