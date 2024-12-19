2eeed22ef6dc10cec7140799b0571427d0828da1
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-19 11:37:31 +0300

    Mo на ребрах

--- sol.cpp
+++ sol.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
-
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
 using namespace std;
-#define endl "\n"
-using ll = long long;
-const ll INF = 1e18;
+//define endl "\n"
+using ll = int;
+const ll INF = 1e9;
 mt19937 mt(time(0));
 void solve() {
-    ll n, q; cin >> n >> q;
+    ll n, m; cin >> n >> m;
+    vector<pair<ll, ll>> edges(m);
+    for (auto &[u, v] : edges) {
+        cin >> u >> v;
+    }
     vector<ll> p(n+1), sz(n+1, 1);
     iota(p.begin(), p.end(), 0ll);
     auto find = [&] (auto f, ll u) -> ll {
@@ -16,7 +20,6 @@ void solve() {
         return p[u];
     };
     vector<pair<ll&, ll>> modns;
-    stack<ll> move_to;
     auto unite = [&] (ll u, ll v) -> void {
         u = find(find, u);
         v = find(find, v);
@@ -31,22 +34,52 @@ void solve() {
         modns.push_back({p[u], p[u]});
         p[u] = v;
     };
+    struct Q {
+        ll l;
+        ll r;
+        ll idx;
+    };
+    ll q; cin >> q;
+    vector<ll> ans(q);
+    vector<Q> ask(q);
     for (ll i = 0; i < q; i += 1) {
-        string type; cin >> type;
-        if (type == "persist") {
-            move_to.push({ll(modns.size())});
-        } else if (type == "union") {
-            ll u, v; cin >> u >> v;
+        ll l, r; cin >> l >> r;
+        ask[i].l = l - 1;
+        ask[i].r = r - 1;
+        ask[i].idx = i;
+    }
+    const ll C = 200;
+    sort(ask.begin(), ask.end(), [&] (Q a, Q b) {
+        if (a.l/C == b.l/C) return a.r < b.r;
+        return a.l/C < b.l/C;
+    });
+    ll last_made = -1;
+    for (ll i = 0; i < q; i += 1) {
+        for (ll j = max(last_made, ask[i].l/C*C+C); j <= ask[i].r;  j += 1) { // without rollback
+            auto [u, v] = edges[j];
             unite(u, v);
-            cout << n-modns.size()/2 << endl;
-        } else {
-            while (ll(modns.size()) > move_to.top()) {
+        }
+        last_made = max(last_made, ll(ask[i].r));
+        ll r = ll(modns.size());
+        for (ll idx = ask[i].l; idx <= min(ask[i].r, ask[i].l/C*C+C-1); idx += 1) {
+            auto [u, v] = edges[idx];
+            unite(u, v);
+        }
+        ans[ask[i].idx] = n-modns.size()/2;
+        while (ll(modns.size()) > r) {
+            modns.back().first = modns.back().second;
+            modns.pop_back();
+        }
+        if (i+1 < q && ask[i+1].l/C != ask[i].l/C) {
+            while (modns.size() > 0) {
                 modns.back().first = modns.back().second;
                 modns.pop_back();
             }
-            cout << n-modns.size()/2 << endl;
-            move_to.pop();
+            last_made = -1;
         }
+    }
+    for (const auto &x : ans) {
+        cout << x << "\n";
     }
 }
 
