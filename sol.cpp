98509291bfbefaa88eed9ce2b5b1150337a7b4bd
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-21 14:35:05 +0300

    bigint

--- sol.cpp
+++ sol.cpp
@@ -1,82 +1,263 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr int digits(int base) noexcept {
+    return base <= 1 ? 0 : 1 + digits(base / 10);
+}
+
+constexpr int base = 10;
+constexpr int base_digits = digits(base);
+
+struct bigint {
+    // value == 0 is represented by empty z
+    vector<int> z;  // digits
+
+    // sign == 1 <==> value >= 0
+    // sign == -1 <==> value < 0
+    int sign;
+
+    bigint(long long v = 0) { *this = v; }
+
+    bigint &operator=(long long v) {
+        sign = v < 0 ? -1 : 1;
+        v *= sign;
+        z.clear();
+        for (; v > 0; v = v / base)
+            z.push_back((int)(v % base));
+        return *this;
+    }
+
+    bigint &operator+=(const bigint &other) {
+        if (sign == other.sign) {
+            for (int i = 0, carry = 0; i < other.z.size() || carry; ++i) {
+                if (i == z.size())
+                    z.push_back(0);
+                z[i] += carry + (i < other.z.size() ? other.z[i] : 0);
+                carry = z[i] >= base;
+                if (carry)
+                    z[i] -= base;
+            }
+        } else if (other != 0 /* prevent infinite loop */) {
+            *this -= -other;
+        }
+        return *this;
+    }
+
+    friend bigint operator+(bigint a, const bigint &b) {
+        a += b;
+        return a;
+    }
+
+    bigint &operator-=(const bigint &other) {
+        if (sign == other.sign) {
+            if ((sign == 1 && *this >= other) || (sign == -1 && *this <= other)) {
+                for (int i = 0, carry = 0; i < other.z.size() || carry; ++i) {
+                    z[i] -= carry + (i < other.z.size() ? other.z[i] : 0);
+                    carry = z[i] < 0;
+                    if (carry)
+                        z[i] += base;
+                }
+                trim();
+            } else {
+                *this = other - *this;
+                this->sign = -this->sign;
+            }
+        } else {
+            *this += -other;
+        }
+        return *this;
+    }
+
+    friend bigint operator-(bigint a, const bigint &b) {
+        a -= b;
+        return a;
+    }
+
+    bigint &operator*=(int v) {
+        if (v < 0)
+            sign = -sign, v = -v;
+        for (int i = 0, carry = 0; i < z.size() || carry; ++i) {
+            if (i == z.size())
+                z.push_back(0);
+            long long cur = (long long)z[i] * v + carry;
+            carry = (int)(cur / base);
+            z[i] = (int)(cur % base);
+        }
+        trim();
+        return *this;
+    }
+
+    bigint operator*(int v) const { return bigint(*this) *= v; }
+
+    friend pair<bigint, bigint> divmod(const bigint &a1, const bigint &b1) {
+        int norm = base / (b1.z.back() + 1);
+        bigint a = a1.abs() * norm;
+        bigint b = b1.abs() * norm;
+        bigint q, r;
+        q.z.resize(a.z.size());
+
+        for (int i = (int)a.z.size() - 1; i >= 0; i--) {
+            r *= base;
+            r += a.z[i];
+            int s1 = b.z.size() < r.z.size() ? r.z[b.z.size()] : 0;
+            int s2 = b.z.size() - 1 < r.z.size() ? r.z[b.z.size() - 1] : 0;
+            int d = (int)(((long long)s1 * base + s2) / b.z.back());
+            r -= b * d;
+            while (r < 0)
+                r += b, --d;
+            q.z[i] = d;
+        }
+
+        q.sign = a1.sign * b1.sign;
+        r.sign = a1.sign;
+        q.trim();
+        r.trim();
+        return {q, r / norm};
+    }
+
+    bigint operator/(const bigint &v) const { return divmod(*this, v).first; }
+
+    bigint operator%(const bigint &v) const { return divmod(*this, v).second; }
+
+    bigint &operator/=(int v) {
+        if (v < 0)
+            sign = -sign, v = -v;
+        for (int i = (int)z.size() - 1, rem = 0; i >= 0; --i) {
+            long long cur = z[i] + rem * (long long)base;
+            z[i] = (int)(cur / v);
+            rem = (int)(cur % v);
+        }
+        trim();
+        return *this;
+    }
+
+    bigint operator/(int v) const { return bigint(*this) /= v; }
+
+    int operator%(int v) const {
+        if (v < 0)
+            v = -v;
+        int m = 0;
+        for (int i = (int)z.size() - 1; i >= 0; --i)
+            m = (int)((z[i] + m * (long long)base) % v);
+        return m * sign;
+    }
+
+    bool operator<(const bigint &v) const {
+        if (sign != v.sign)
+            return sign < v.sign;
+        if (z.size() != v.z.size())
+            return z.size() * sign < v.z.size() * v.sign;
+        for (int i = (int)z.size() - 1; i >= 0; i--)
+            if (z[i] != v.z[i])
+                return z[i] * sign < v.z[i] * sign;
+        return false;
+    }
+
+    bool operator>(const bigint &v) const { return v < *this; }
+
+    bool operator<=(const bigint &v) const { return !(v < *this); }
+
+    bool operator>=(const bigint &v) const { return !(*this < v); }
+
+    bool operator==(const bigint &v) const { return sign == v.sign && z == v.z; }
+
+
+    void trim() {
+        while (!z.empty() && z.back() == 0)
+            z.pop_back();
+        if (z.empty())
+            sign = 1;
+    }
+
+    friend bigint operator-(bigint v) {
+        if (!v.z.empty())
+            v.sign = -v.sign;
+        return v;
+    }
+
+    bigint abs() const { return sign == 1 ? *this : -*this; }
+
+    void read(const string &s) {
+        sign = 1;
+        z.clear();
+        int pos = 0;
+        while (pos < s.size() && (s[pos] == '-' || s[pos] == '+')) {
+            if (s[pos] == '-')
+                sign = -sign;
+            ++pos;
+        }
+        for (int i = (int)s.size() - 1; i >= pos; i -= base_digits) {
+            int x = 0;
+            for (int j = max(pos, i - base_digits + 1); j <= i; j++)
+                x = x * 10 + s[j] - '0';
+            z.push_back(x);
+        }
+        trim();
+    }
+
+    friend istream &operator>>(istream &stream, bigint &v) {
+        string s;
+        stream >> s;
+        v.read(s);
+        return stream;
+    }
+
+    friend ostream &operator<<(ostream &stream, const bigint &v) {
+        if (v.sign == -1)
+            stream << '-';
+        stream << (v.z.empty() ? 0 : v.z.back());
+        for (int i = (int)v.z.size() - 2; i >= 0; --i)
+            stream << setw(base_digits) << setfill('0') << v.z[i];
+        return stream;
+    }
+
+    bigint operator*(const bigint &v) const {
+        return mul_simple(v);
+    }
+
+    bigint mul_simple(const bigint &v) const {
+        bigint res;
+        res.sign = sign * v.sign;
+        res.z.resize(z.size() + v.z.size());
+        for (int i = 0; i < z.size(); ++i)
+            if (z[i])
+                for (int j = 0, carry = 0; j < v.z.size() || carry; ++j) {
+                    long long cur = res.z[i + j] + (long long)z[i] * (j < v.z.size() ? v.z[j] : 0) + carry;
+                    carry = (int)(cur / base);
+                    res.z[i + j] = (int)(cur % base);
+                }
+        res.trim();
+        return res;
+    }
+};
+
 int main () {
-//    ios::sync_with_stdio(false);
-//    cin.tie(0);
-    int n; cin >> n;
-    map<int, string> mp;
-    map<string, int> invmp;
-    for (int i = 1; i <= n; i += 1) {
-        string filename; cin >> filename;
-        mp[i] = filename;
-        invmp[filename] = i;
-    }
-    vector<vector<int>> sm(n+1);
-    for (int i = 1; i <= n; i += 1) {
-        string filename; cin >> filename;
-        assert(filename == mp[i]);
-        int sz; cin >> sz;
-        cin.ignore();
-        for (int j = 0; j < sz; j += 1) {
-            string imports;
-            getline(cin, imports);
-            int start_of_next_item = 1;
-            for (int k = 0; k <= imports.size(); k += 1) {
-                if (k == imports.size() || imports[k] == ',') {
-                    sm[i].push_back(invmp[imports.substr(start_of_next_item, k-start_of_next_item)]);
-                } else if (imports[k] == ' ') {
-                    start_of_next_item = k + 1;
-                }
-            }
-        }
-    }
-    int min_cycle = 1e9;
-    int min_cycle_vertice = -1;
-    bitset<501> seen;
-    vector<int> par(501);
-    auto bfs = [&] (int u) -> void {
-        queue<pair<int,int>> q;
-        q.push({0, u});
-        seen.reset();
-        seen[u] = true;
-        while (!q.empty()) {
-            auto [d, tp] = q.front();
-            q.pop();
-            for (const auto &x : sm[tp]) {
-                if (x == u) {
-                    par[x] = tp;
-                    if (d + 1 < min_cycle) {
-                        min_cycle = d + 1;
-                        min_cycle_vertice = u;
-                    }
-                    return;
-                }
-                if (!seen[x]) {
-                    par[x] = tp;
-                    seen[x] = true;
-                    q.push({d + 1, x});
-                }
-            }
-        }
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    bigint n, m; cin >> n >> m;
+    bigint p; cin >> p;
+    auto sum = [&] (bigint a, bigint b) -> bigint {
+        return (a+b)%p;
     };
-    for (int i = 1; i <= n; i += 1) {
-        bfs(i);
-    }
-    if (min_cycle_vertice == -1) {
-        cout << "SHIP IT" << "\n";
-        return 0;
-    }
-    bfs(min_cycle_vertice);
-    int curr = min_cycle_vertice;
-    vector<string> res;
-    while (par[curr] != min_cycle_vertice) {
-        curr = par[curr];
-        res.push_back(mp[curr]);
-    }
-    reverse(res.begin(), res.end());
-    cout << mp[min_cycle_vertice] << " ";
-    for (const auto &x : res) {
-        cout << x << " ";
-    }
-    cout << "\n";
+    auto sub = [&] (bigint a, bigint b) -> bigint {
+        return (a%p-b%p+p)%p;
+    };
+    auto mul = [&] (bigint a, bigint b) -> bigint {
+        return (a*b)%p;
+    };
+    auto powm = [&] (bigint a, bigint power) -> bigint {
+        bigint res = 1;
+        bigint p = a;
+        while (power > 0) {
+            if (power % 2 == 1) {
+                res = mul(res, p);
+            }
+            p = mul(p, p);
+            power = power / 2;
+        }
+        return res;
+    };
+    bigint bad = powm(2, (n-1)*(m-1));
+    bigint total = powm(2, n*m);
+    bigint good = sub(total, bad);
+    cout << good << "\n";
 }
