ce60ee9c21157acf9536a7ff9c270b054077fc22
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-24 00:07:20 +0300

    MLE https://dmoj.ca/problem/wc16c3s2

--- sol.cpp
+++ sol.cpp
@@ -1,72 +1,70 @@
-#include <algorithm>
-#include <vector>
-#include <iostream>
-#include <numeric>
+#include <bits/stdc++.h>
 using namespace std;
-const long long INF = 1e18;
-struct line {
-    long long k, b;
-    long long operator()(long long x) {
-        return k * x + b;
-    }
-};
-long long cross(line a, line b) {
-    return double(a.b - b.b) / (b.k - a.k);
-}
-vector<pair<long long, line>> lines(1e6);
-struct CHT {
-    long long idx = 0;
-    long long nxt = 0;
-    void insert(line l) {
-        while (nxt && cross(l, lines[nxt-1].second) <= lines[nxt-1].first) nxt -= 1;
-        lines[nxt++] = {lines.empty() ? -INF : cross(l, lines[nxt-1].second), l};
-        idx = min(idx, (long long)(nxt - 1));
-    }
-    long long get(long long x) {
-        for (;;++idx) {
-            if (idx + 1 == nxt || lines[idx + 1].first > x) return lines[idx].second(x);
-        }
-    }
-};
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    long long n; cin >> n;
-    long long cats_n; cin >> cats_n;
-    long long feeders; cin >> feeders;
-    struct cat {
-        long long where;
-        long long t;
+    const long long INF = 1e18;
+    long long n;
+    cin >> n;
+    long long m;
+    cin >> m;
+    vector<long long> price(n+1);
+    set<long long> prices;
+    for (long long i = 1; i <= n; i += 1) {
+        cin >> price[i];
+        prices.insert(price[i]);
+    }
+    vector<vector<pair<int, int>>> adj(n+1);
+    set<int> key_levels;
+    for (long long i = 0; i < m; i += 1) {
+        long long u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({w, v});
+        adj[v].push_back({w, u});
+        key_levels.insert(w);
+    }
+    struct State {
+        int v;
+        int level;
+        auto operator<=>(const State & o) const = default;
     };
-    vector<long long> d(n);
-    for (long long i = 1; i < n; i += 1) {
-        d[i] = d[i-1];
-        long long x; cin >> x;
-        d[i] += x;
-    }
-    vector<cat> a(cats_n);
-    vector<long long> val(cats_n);
-    for (long long i = 0; i < cats_n; i += 1) {
-        cin >> a[i].where >> a[i].t;
-        a[i].where -= 1;
-        val[i] = a[i].t-d[a[i].where];
-    }
-    sort(val.begin(), val.end());
-    vector<long long> dp(cats_n+1, INF);
-    vector<long long> ndp(cats_n+1, INF);
-    for (long long k = 1; k <= feeders; k += 1) {
-        CHT c;
-        for (long long i = 1; i <= cats_n; i += 1) {
-            long long cur = val[i-1] * i;
-            if (i != 1) cur = min(cur, c.get(val[i-1]) + val[i-1] * i);
-            ndp[i] = cur;
-            c.insert({-i, dp[i]});
+    map<long long, set<State>> st;
+    vector<map<int, long long>> d(n+1);
+    vector<map<int, bool>> seen(n+1);
+    d[1][1] = 0;
+    st[0].insert({1, 1});
+    while (!st.empty()) {
+        auto [u, level] = *st.begin()->second.begin();
+        if (u == n) {
+            cout << d[u][level] << endl;
+            return 0;
         }
-        swap(dp, ndp);
+        st.begin()->second.erase(st.begin()->second.begin());
+        if (st.begin()->second.empty()) {
+            st.erase(st.begin());
+        }
+        if (seen[u].count(level)) {
+            continue;
+        }
+        seen[u][level] = true;
+        for (const auto &[w, v] : adj[u]) {
+            if (w <= level) {
+                if (!d[v].count(level) || d[v][level] > d[u][level]) {
+                    d[v][level] = d[u][level];
+                    st[d[v][level]].insert({v, level});
+                }
+            }
+        }
+        auto it = key_levels.upper_bound(level);
+        while (it != key_levels.end()) {
+            int nwl = *it;
+            if (!d[u].count(nwl) || d[u][nwl] > d[u][level] + price[u]*(nwl-level)) {
+                d[u][nwl] = d[u][level] + price[u]*(nwl-level);
+                st[d[u][level] + price[u]*(nwl-level)].insert({u, nwl});
+            } else {
+                break;
+            }
+            it++;
+        }
     }
-
-    long long ans = dp.back() - accumulate(val.begin(), val.end(), 0ll);
-    cout << ans << endl;
+    cout << -1 << endl;
     return 0;
 }
