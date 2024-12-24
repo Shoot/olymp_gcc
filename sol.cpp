3bfa326c59790ee9397db28efa50ad73a5aede9a
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-25 02:29:10 +0300

    DLP simple sqrt

--- sol.cpp
+++ sol.cpp
@@ -7,6 +7,31 @@ long long gcd(long long a,long long b) {
     return gcd(b,a%b);
 }
 long long x, z, k;
+void solve2() {
+    long long a = x;
+    long long m = z;
+    long long b = k;
+    long long K = 1;
+    long long C = sqrt(m);
+    long long a_pow_sqrt = 1;
+    for (long long i = 0; i < C; i += 1) {
+        a_pow_sqrt = (a_pow_sqrt * a) % m;
+    }
+    unordered_map<long long, long long> mp;
+    long long curr = 1;
+    for (long long i = 0; i <= C; i += 1) {
+        mp[curr] = i;
+        curr = (curr * a_pow_sqrt) % m;
+    }
+    curr = b;
+    for (long long j = 0; j < C; j += 1) {
+        cout << "j = " << j << endl;
+        if (mp.count(curr)) {
+            cout << mp[curr]*C-j << endl;
+        }
+        curr = (curr * a) % m;
+    }
+}
 void solve() {
     x %= z, k %= z;
     long long O = 1, add = 0, g;
@@ -54,7 +79,7 @@ int main() {
         if (x == 0 && z == 0 && k == 0) {
             break;
         }
-        solve();
+        solve2();
     }
     return 0;
 }
