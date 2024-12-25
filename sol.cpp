a623e7c0531d1a6837e1a4fc3f5786e15d338992
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-25 03:02:48 +0300

    should improve

--- sol.cpp
+++ sol.cpp
@@ -7,42 +7,50 @@ long long gcd(long long a,long long b) {
     return gcd(b,a%b);
 }
 long long x, z, k;
-void solve2() {
+string solve2() {
     long long a = x;
     long long m = z;
     long long b = k;
+    a %= m;
+    b %= m;
     long long K = 1;
     long long C = sqrt(m);
     long long a_pow_sqrt = 1;
     for (long long i = 0; i < C; i += 1) {
         a_pow_sqrt = (a_pow_sqrt * a) % m;
     }
-    unordered_map<long long, long long> mp;
+    unordered_map<long long, set<long long>> mp;
     long long curr = 1;
     for (long long i = 0; i <= C; i += 1) {
-        mp[curr] = i;
+        mp[curr].insert(C*i);
         curr = (curr * a_pow_sqrt) % m;
     }
     curr = b;
+    const long long INF = 1e18;
+    long long mini = INF;
     for (long long j = 0; j < C; j += 1) {
-        cout << "j = " << j << endl;
         if (mp.count(curr)) {
-            cout << mp[curr]*C-j << endl;
+            auto it = mp[curr].lower_bound(j);
+            if (it != mp[curr].end()) {
+                mini = min(mini, *it-j);
+            }
         }
         curr = (curr * a) % m;
     }
+    if (mini == INF) {
+        return "No Solution\n";
+    }
+    return to_string(mini)+"\n";
 }
-void solve() {
+string solve() {
     x %= z, k %= z;
     long long O = 1, add = 0, g;
     while ((g = gcd(x, z)) > 1) {
         if (k == O) {
-            cout << add << "\n";
-            return;
+            return to_string(add) + "\n";
         }
         if (k % g) {
-            cout << "No Solution\n";
-            return;
+            return "No Solution\n";
         }
         O /= g;
         z /= g;
@@ -66,12 +74,10 @@ void solve() {
         cur = (cur * x_pow_sqrt) % z;
         if (k_mult_by_x_pow.count(cur)) {
             long long ans = sqrt_z * p - k_mult_by_x_pow[cur] + add;
-            cout << ans << "\n";
-            return;
+            return to_string(ans) + "\n";
         }
     }
-    cout << "No Solution\n";
-    return;
+    return "No Solution\n";
 }
 int main() {
     while (true) {
@@ -79,7 +85,9 @@ int main() {
         if (x == 0 && z == 0 && k == 0) {
             break;
         }
-        solve2();
+        cout << solve();
+        cout <<solve2();
+        assert(solve() == solve2());
     }
     return 0;
 }
