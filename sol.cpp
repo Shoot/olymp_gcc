b2827577dac19f87363b10602e61cf3b80ff9e92
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-27 17:26:35 +0300

    temp

--- sol.cpp
+++ sol.cpp
@@ -1,85 +1,90 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main () {
     int n;
     cin >> n;
-    map<int, vector<pair<int, int>>> inc;
-    map<int, vector<pair<int, int>>> dec;
-    set<int> key_x;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
+    }
+    struct suffix {
+        int idx;
+        int high_priority_class, low_priority_class;
+    };
+    function<int(suffix,suffix)> comp = [] (suffix a, suffix b){
+        if (a.high_priority_class == b.high_priority_class) {
+            return a.low_priority_class < b.low_priority_class;
+        }
+        return a.high_priority_class < b.high_priority_class;
+    };
+    a.push_back(-1);
+    n += 1;
+    vector<suffix> curr(n);
     for (int i = 0; i < n; i += 1) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        x1 += 200'000;
-        y1 += 200'000;
-        x2 += 200'000;
-        y2 += 200'000;
-        key_x.insert(x1);
-        key_x.insert(x2);
-        inc[x1].push_back({y1, y2});
-        dec[x2].push_back({y1, y2});
+        curr[i].idx = i;
+        curr[i].high_priority_class = a[i];
+        curr[i].low_priority_class = a[i];
     }
-    int maxi = -1;
-    const int N = 400'005;
-    pair<int, int> maxi_point = {-1, -1};
-    struct Node {
-        int lazy = 0;
-        int maxi = 0;
-        int maxi_idx = 0;
-    };
-    vector<Node> tree(4*N+10);
-    auto build = [&] (auto f, int v, int tl, int tr) -> void {
-        tree[v].maxi_idx = tl;
-        if (tl == tr) {
-            return;
+    sort(curr.begin(), curr.end(), comp);
+    int classes_cnt = 1;
+    vector<int> c(n);
+    for (int i = 0; i < n; i += 1) {
+        if (i > 0 && (curr[i].high_priority_class!=curr[i-1].high_priority_class ||
+                      curr[i].low_priority_class!=curr[i-1].low_priority_class)) {
+            classes_cnt += 1;
         }
-        int tm = (tl + tr) >> 1;
-        f(f, 2*v+1, tl, tm);
-        f(f, 2*v+2, tm+1, tr);
-    };
-    build(build, 0, 0, N);
-    auto push = [&] (int v) -> void {
-        tree[2*v+1].maxi += tree[v].lazy;
-        tree[2*v+1].lazy += tree[v].lazy;
-        tree[2*v+2].maxi += tree[v].lazy;
-        tree[2*v+2].lazy += tree[v].lazy;
-        tree[v].lazy = 0;
-    };
-    auto add_segment = [&] (auto f, int v, int tl, int tr, int l, int r, int inc) -> void {
-        if (tl == l && tr == r) {
-            tree[v].maxi += inc;
-            tree[v].lazy += inc;
-            return;
+        c[curr[i].idx] = classes_cnt;
+    }
+    for (int k = 0; classes_cnt < n; k += 1) {
+        int two_pow_k = 1ll << k;
+        for (int i = 0; i < n; i += 1) {
+            curr[i].idx = i;
+            curr[i].high_priority_class = c[i];
+            curr[i].low_priority_class = c[(i+two_pow_k)%n];
         }
-        int tm = (tl + tr) >> 1;
-        push(v);
-        if (l <= tm) {
-            f(f, 2*v+1, tl, tm, l, min(r, tm), inc);
-        }
-        if (r >= tm + 1) {
-            f(f, 2*v+2, tm+1, tr, max(l, tm+1), r, inc);
-        }
-        tree[v].maxi = tree[2*v+1].maxi;
-        tree[v].maxi_idx = tree[2*v+1].maxi_idx;
-        if (tree[v].maxi < tree[2*v+2].maxi) {
-            tree[v].maxi = tree[2*v+2].maxi;
-            tree[v].maxi_idx = tree[2*v+2].maxi_idx;
-        }
-    };
-    for (const auto &x : key_x) {
-        for (const auto &[l, r] : inc[x]) {
-            add_segment(add_segment, 0, 0, N, l, r, 1);
-        }
-        if (maxi < tree[0].maxi) {
-            maxi = tree[0].maxi;
-            maxi_point.first = x - 200'000;
-            maxi_point.second = tree[0].maxi_idx - 200'000;
-        }
-        for (const auto &[l, r] : dec[x]) {
-            add_segment(add_segment, 0, 0, N, l, r, -1);
+        sort(curr.begin(), curr.end(), comp);
+        classes_cnt = 1;
+        for (int i = 0; i < n; i += 1) {
+            if (i > 0 && (curr[i].high_priority_class!=curr[i-1].high_priority_class ||
+                          curr[i].low_priority_class!=curr[i-1].low_priority_class)) {
+                classes_cnt += 1;
+            }
+            c[curr[i].idx] = classes_cnt;
         }
     }
-    cout << maxi << endl;
-    cout << maxi_point.first << " " << maxi_point.second << endl;
+    vector<int> rank(n);
+    vector<int> sa(n);
+    vector<int> lcp(n-1);
+    for (int i = 0; i < n; i+=1) {
+        rank[i] = c[i]-1;
+        sa[c[i]-1] = i;
+    }
+    int k = 0;
+    for(int i=0; i<n; i++, k?k--:0)
+    {
+        if(rank[i]==n-1) {
+            k=0;
+            continue;
+        }
+        int j=sa[rank[i]+1];
+        while(i+k<n && j+k<n && a[i+k]==a[j+k]) k++;
+        lcp[rank[i]]=k;
+    }
+    int X = accumulate(lcp.begin(), lcp.end(), 0);
+    n -= 1;
+    // одна входнит в другую суффиксом, остальное нули - этот случай дает равенство
+    int cnt0 = 0;
+    int zero_subseg = 0;
+    for (int i = 0; i < n; i += 1) {
+        if (a[i] != 0) {
+            zero_subseg += cnt0 * (cnt0 + 1) / 2;
+            cnt0 = 0;
+        } else {
+            cnt0 += 1;
+        }
+    }
+    zero_subseg += cnt0 * (cnt0 - 1) / 2;
+    cout << "zero_subseg = " << zero_subseg << endl;
+    cout << (n*(n+1)/2*n*(n+1)/2 - X - zero_subseg)/2 << "\n";
     return 0;
 }
