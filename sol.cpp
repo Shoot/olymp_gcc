73928c5fbf6f22a689f287f87169d465862d3e43
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-27 22:22:06 +0300

    unique integers among substrings

--- sol.cpp
+++ sol.cpp
@@ -1,90 +1,100 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main () {
-    int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    long long n;
     cin >> n;
-    vector<int> a(n);
+    vector<long long> a(n);
     for (auto &x : a) {
         cin >> x;
+        x += 1;
     }
-    struct suffix {
-        int idx;
-        int high_priority_class, low_priority_class;
-    };
-    function<int(suffix,suffix)> comp = [] (suffix a, suffix b){
-        if (a.high_priority_class == b.high_priority_class) {
-            return a.low_priority_class < b.low_priority_class;
-        }
-        return a.high_priority_class < b.high_priority_class;
-    };
-    a.push_back(-1);
     n += 1;
-    vector<suffix> curr(n);
-    for (int i = 0; i < n; i += 1) {
-        curr[i].idx = i;
-        curr[i].high_priority_class = a[i];
-        curr[i].low_priority_class = a[i];
+    a.push_back(0);
+    vector<long long> rank(n);
+    vector<long long> sa(n);
+    vector<long long> lcp(n - 1);
+    long long alphabet = 12;
+    vector<long long> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (long long i = 0; i < n; i += 1) {
+        cnt[a[i]] += 1;
     }
-    sort(curr.begin(), curr.end(), comp);
-    int classes_cnt = 1;
-    vector<int> c(n);
-    for (int i = 0; i < n; i += 1) {
-        if (i > 0 && (curr[i].high_priority_class!=curr[i-1].high_priority_class ||
-                      curr[i].low_priority_class!=curr[i-1].low_priority_class)) {
-            classes_cnt += 1;
-        }
-        c[curr[i].idx] = classes_cnt;
+    for (long long i = 1; i < alphabet; i += 1) {
+        cnt[i] += cnt[i - 1];
     }
-    for (int k = 0; classes_cnt < n; k += 1) {
-        int two_pow_k = 1ll << k;
-        for (int i = 0; i < n; i += 1) {
-            curr[i].idx = i;
-            curr[i].high_priority_class = c[i];
-            curr[i].low_priority_class = c[(i+two_pow_k)%n];
+    for (long long i = 0; i < n; i += 1) {
+        p[--cnt[a[i]]] = i;
+    }
+    c[p[0]] = 0;
+    long long classes = 1;
+    for (long long i = 1; i < n; i += 1) {
+        if (a[p[i]] != a[p[i - 1]]) {
+            classes += 1;
         }
-        sort(curr.begin(), curr.end(), comp);
-        classes_cnt = 1;
-        for (int i = 0; i < n; i += 1) {
-            if (i > 0 && (curr[i].high_priority_class!=curr[i-1].high_priority_class ||
-                          curr[i].low_priority_class!=curr[i-1].low_priority_class)) {
-                classes_cnt += 1;
+        c[p[i]] = classes - 1;
+    }
+    vector<long long> pn(n), cn(n);
+    for (long long h = 0; (1 << h) < n; h += 1) {
+        for (long long i = 0; i < n; i += 1) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0) {
+                pn[i] += n;
             }
-            c[curr[i].idx] = classes_cnt;
         }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (long long i = 0; i < n; i += 1) {
+            cnt[c[pn[i]]] += 1;
+        }
+        for (long long i = 1; i < classes; i += 1) {
+            cnt[i] += cnt[i - 1];
+        }
+        for (long long i = n - 1; i >= 0; i--) {
+            p[--cnt[c[pn[i]]]] = pn[i];
+        }
+        cn[p[0]] = 0;
+        classes = 1;
+        for (long long i = 1; i < n; i += 1) {
+            pair<long long, long long> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<long long, long long> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
+            if (cur != prev) {
+                classes += 1;
+            }
+            cn[p[i]] = classes - 1;
+        }
+        swap(c, cn);
     }
-    vector<int> rank(n);
-    vector<int> sa(n);
-    vector<int> lcp(n-1);
-    for (int i = 0; i < n; i+=1) {
-        rank[i] = c[i]-1;
-        sa[c[i]-1] = i;
+    for (long long i = 0; i < n; i += 1) {
+        rank[i] = c[i];
+        sa[c[i]] = i;
     }
-    int k = 0;
-    for(int i=0; i<n; i++, k?k--:0)
-    {
-        if(rank[i]==n-1) {
-            k=0;
+    for (long long i = 0, k = 0; i < n; i += 1, k ? k -= 1 : 0) {
+        if (rank[i] == n-1) {
+            k = 0;
             continue;
         }
-        int j=sa[rank[i]+1];
-        while(i+k<n && j+k<n && a[i+k]==a[j+k]) k++;
-        lcp[rank[i]]=k;
+        long long j = sa[rank[i]+1];
+        while (i+k < n && j+k < n && a[i+k] == a[j+k]) k++;
+        lcp[rank[i]] = k;
     }
-    int X = accumulate(lcp.begin(), lcp.end(), 0);
     n -= 1;
-    // одна входнит в другую суффиксом, остальное нули - этот случай дает равенство
-    int cnt0 = 0;
-    int zero_subseg = 0;
-    for (int i = 0; i < n; i += 1) {
-        if (a[i] != 0) {
-            zero_subseg += cnt0 * (cnt0 + 1) / 2;
-            cnt0 = 0;
-        } else {
-            cnt0 += 1;
+    long long decr = 0;
+    long long incr = 0;
+    for (auto &x : a) {
+        x -= 1;
+    }
+    for (long long i = 0; i < n; i += 1) {
+        if (a[i] == 0) {
+            decr += n - i;
+            incr = 1;
         }
     }
-    zero_subseg += cnt0 * (cnt0 - 1) / 2;
-    cout << "zero_subseg = " << zero_subseg << endl;
-    cout << (n*(n+1)/2*n*(n+1)/2 - X - zero_subseg)/2 << "\n";
+    for (long long i = 1; i < n; i += 1) {
+        if (a[sa[i]] != 0) {
+            decr += lcp[i];
+        }
+    }
+    long long UNIQUE_SUBSTRINGS = n*(n+1)/2-decr+incr;
     return 0;
 }
