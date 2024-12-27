13492ae6898bf7e5f9a506a00e9940ab925b7e4f
Shoot <11522779+Shoot@users.noreply.github.com>
2024-12-27 16:05:32 +0300

    2D sweep line

--- sol.cpp
+++ sol.cpp
@@ -2,165 +2,84 @@
 using namespace std;
 
 int main () {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
     int n;
     cin >> n;
-    cin.ignore();
-    map<string, vector<int>> what_could_it_be;
-    map<string, int> no_change;
-    no_change["     |  |"] = 1;
-    no_change[" _  _||_ "] = 2;
-    no_change[" _  _| _|"] = 3;
-    no_change["   |_|  |"] = 4;
-    no_change[" _ |_  _|"] = 5;
-    no_change[" _ |_ |_|"] = 6;
-    no_change[" _   |  |"] = 7;
-    no_change[" _ |_||_|"] = 8;
-    no_change[" _ |_| _|"] = 9;
-    no_change[" _ | ||_|"] = 0;
-    vector<int> code(10);
-    for (const auto &[s, val] : no_change) {
-        for (int i = 0; i < 9; i += 1) {
-            if (s[i] != ' ') {
-                code[val] += 1 << i;
-            }
-        }
-    }
-    auto scan = [&] (string &s1, string &s2, string &s3) -> void {
-        vector<int> definite(9, -1);
-        vector<vector<int>> possible(9);
-        for (int f = 0; f < 27; f += 3) {
-            string s;
-            s += s1.substr(f, 3);
-            s += s2.substr(f, 3);
-            s += s3.substr(f, 3);
-            int cd = 0;
-            for (int i = 0; i < 9; i += 1) {
-                if (s[i] != ' ') {
-                    cd += 1 << i;
-                }
-            }
-            for (int i = 0; i < 10; i += 1) {
-                if (code[i] == cd) {
-                    definite[f/3] = i;
-                }
-            }
-            for (int i = 0; i < 10; i += 1) {
-                int same = code[i]&cd;
-                int diff = code[i]-cd;
-                if (diff > 0 && (code[i]&diff)==diff) {
-                    possible[f/3].push_back(i);
-                }
-            }
-        }
-//        for (const auto &x : definite) {
-//            clog << x << " ";
-//        }
-//        clog << "\n";
-//        for (int i = 0; i < 9; i += 1) {
-//            if (!possible[i].empty()) {
-//                cout << i << ":";
-//                for (const auto &x : possible[i]) {
-//                    cout << x << "!";
-//                }
-//                cout << "\n";
-//            }
-//        }
-        vector<int> bad;
-        for (int i = 0; i < 9; i += 1) {
-            if (definite[i] == -1) {
-                bad.push_back(i);
-            }
-        }
-        if (bad.size() > 1) {
-            cout << "failure\n";
-            return;
-        }
-        if (bad.size() == 1) {
-            int bd = bad[0];
-            int su = 0;
-            for (int i = 0; i < 9; i += 1) {
-                if (i != bd) {
-                    assert(definite[i] != -1);
-                    su += definite[i]*(9-i);
-                    su %= 11;
-                }
-            }
-            vector<int> good_bd;
-            for (const auto &x : possible[bd]) {
-                if ((su+x*(9-bd))%11==0) {
-                    good_bd.push_back(x);
-                }
-            }
-            if (good_bd.empty()) {
-                cout << "failure\n";
-                return;
-            }
-            if (good_bd.size() > 1) {
-                cout << "ambiguous\n";
-                return;
-            }
-            for (int i = 0; i < 9; i += 1) {
-                if (i != bd) {
-                    cout << definite[i];
-                } else {
-                    cout << good_bd[0];
-                }
-            }
-            cout << "\n";
-            return;
-        }
-        vector<int> nice;
-        int su = 0;
-        for (int i = 0; i < 9; i += 1) {
-            su += definite[i]*(9-i);
-            su %= 11;
-        }
-        if (su == 0) {
-            nice = definite;
-        }
-        for (int change = 0; change < 9; change += 1) {
-            int tsu = 0;
-            for (int i = 0; i < 9; i += 1) {
-                if (i != change) {
-                    tsu += definite[i]*(9-i);
-                    tsu %= 11;
-                }
-            }
-            for (const auto &x : possible[change]) {
-                int ntsu = tsu + x*(9-change);
-                ntsu %= 11;
-                if (ntsu == 0) {
-                    if (!nice.empty()) {
-                        cout << "ambiguous\n";
-                        return;
-                    }
-                    for (int i = 0; i < 9; i += 1) {
-                        if (i != change) {
-                            nice.push_back(definite[i]);
-                        } else {
-                            nice.push_back(x);
-                        }
-                    }
-                }
-            }
-        }
-        if (nice.empty()) {
-            cout << "failure\n";
-            return;
-        }
-        for (const auto &x : nice) {
-            cout << x;
-        }
-        cout << "\n";
-    };
+    map<int, vector<pair<int, int>>> inc;
+    map<int, vector<pair<int, int>>> dec;
+    set<int> key_x;
     for (int i = 0; i < n; i += 1) {
-        string s1, s2, s3;
-        getline(cin, s1);
-        getline(cin, s2);
-        getline(cin, s3);
-        scan(s1, s2, s3);
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1 += 200'000;
+        y1 += 200'000;
+        x2 += 200'000;
+        y2 += 200'000;
+        key_x.insert(x1);
+        key_x.insert(x2);
+        inc[x1].push_back({y1, y2});
+        dec[x2].push_back({y1, y2});
     }
+    int maxi = -1;
+    const int N = 400'005;
+    pair<int, int> maxi_point = {-1, -1};
+    struct Node {
+        int lazy = 0;
+        int maxi = 0;
+        int maxi_idx = 0;
+    };
+    vector<Node> tree(4*N+10);
+    auto build = [&] (auto f, int v, int tl, int tr) -> void {
+        tree[v].maxi_idx = tl;
+        if (tl == tr) {
+            return;
+        }
+        int tm = (tl + tr) >> 1;
+        f(f, 2*v+1, tl, tm);
+        f(f, 2*v+2, tm+1, tr);
+    };
+    build(build, 0, 0, N);
+    auto push = [&] (int v) -> void {
+        tree[2*v+1].maxi += tree[v].lazy;
+        tree[2*v+1].lazy += tree[v].lazy;
+        tree[2*v+2].maxi += tree[v].lazy;
+        tree[2*v+2].lazy += tree[v].lazy;
+        tree[v].lazy = 0;
+    };
+    auto add_segment = [&] (auto f, int v, int tl, int tr, int l, int r, int inc) -> void {
+        if (tl == l && tr == r) {
+            tree[v].maxi += inc;
+            tree[v].lazy += inc;
+            return;
+        }
+        int tm = (tl + tr) >> 1;
+        push(v);
+        if (l <= tm) {
+            f(f, 2*v+1, tl, tm, l, min(r, tm), inc);
+        }
+        if (r >= tm + 1) {
+            f(f, 2*v+2, tm+1, tr, max(l, tm+1), r, inc);
+        }
+        tree[v].maxi = tree[2*v+1].maxi;
+        tree[v].maxi_idx = tree[2*v+1].maxi_idx;
+        if (tree[v].maxi < tree[2*v+2].maxi) {
+            tree[v].maxi = tree[2*v+2].maxi;
+            tree[v].maxi_idx = tree[2*v+2].maxi_idx;
+        }
+    };
+    for (const auto &x : key_x) {
+        for (const auto &[l, r] : inc[x]) {
+            add_segment(add_segment, 0, 0, N, l, r, 1);
+        }
+        if (maxi < tree[0].maxi) {
+            maxi = tree[0].maxi;
+            maxi_point.first = x - 200'000;
+            maxi_point.second = tree[0].maxi_idx - 200'000;
+        }
+        for (const auto &[l, r] : dec[x]) {
+            add_segment(add_segment, 0, 0, N, l, r, -1);
+        }
+    }
+    cout << maxi << endl;
+    cout << maxi_point.first << " " << maxi_point.second << endl;
     return 0;
 }
