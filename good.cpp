93356c46ec2d1b922ad6257eb7615672c306cd3e
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-08 01:04:28 +0300

    LOOOL 1st try Edmonds-Karp

--- good.cpp
+++ good.cpp
@@ -1,88 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cmath>
-#include <sstream>
-#include <cstring>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Order {
-    int departure_time; // Departure time in minutes from 00:00
-    pair<int, int> start; // Starting coordinates
-    pair<int, int> end;   // Destination coordinates
-    int travel_time;      // Travel time from start to end
-};
-
-// Convert time from hh:mm to total minutes
-int timeToMinutes(const string& time) {
-    int h, m;
-    sscanf(time.c_str(), "%d:%d", &h, &m);
-    return h * 60 + m;
-}
-
-// Check if order1 can be followed by order2
-bool canChain(const Order& o1, const Order& o2) {
-    int end_time1 = o1.departure_time + o1.travel_time;
-    int travel_to_next = abs(o1.end.first - o2.start.first) + abs(o1.end.second - o2.start.second);
-    return end_time1 + travel_to_next + 1 <= o2.departure_time;
-}
-
-// Depth-First Search for finding an augmenting path in bipartite graph
-bool bpm(int u, vector<vector<int>>& graph, vector<bool>& visited, vector<int>& match) {
-    for (int v : graph[u]) {
-        if (!visited[v]) {
-            visited[v] = true;
-            if (match[v] == -1 || bpm(match[v], graph, visited, match)) {
-                match[v] = u;
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-int minTaxis(vector<Order>& orders) {
-    int n = orders.size();
-    vector<vector<int>> graph(n);
-
-    // Build the graph based on chaining constraints
-    for (int i = 0; i < n; ++i) {
-        for (int j = i + 1; j < n; ++j) {
-            if (canChain(orders[i], orders[j])) {
-                graph[i].push_back(j);
-            }
-        }
-    }
-
-    // Maximum Bipartite Matching using DFS
-    vector<int> match(n, -1);
-    int result = 0;
-    for (int i = 0; i < n; ++i) {
-        vector<bool> visited(n, false);
-        if (bpm(i, graph, visited, match)) {
-            ++result;
-        }
-    }
-
-    // Minimum number of taxis required
-    return n - result;
-}
-
 int main() {
-    int M;
-    cin >> M;
-    vector<Order> orders;
-
-    for (int i = 0; i < M; ++i) {
-        string time;
-        int a, b, c, d;
-        cin >> time >> a >> b >> c >> d;
-        int departure_time = timeToMinutes(time);
-        int travel_time = abs(a - c) + abs(b - d);
-        orders.push_back({departure_time, {a, b}, {c, d}, travel_time});
+    int T;
+    cin >> T;
+    struct Bowl {
+        double h;
+        double r;
+        double R;
+    };
+    auto oper = [&] (const Bowl &up, const Bowl &down) -> double {
+        if (up.R <= down.r) {
+            return 0.;
+        }
+        if (up.r >= down.R) {
+            return down.h;
+        }
+        double ans = 0.;
+        if (up.r > down.r) {
+            ans = max(ans, double((up.r-down.r) * down.h) / (down.R-down.r));
+        }
+        if (up.R < down.R) {
+            ans = max(ans, double((up.R-down.r) * down.h) / (down.R-down.r) - up.h);
+        } else {
+            ans = max(ans, double(down.h) - double((down.R-up.r) * up.h / (up.R-up.r)));
+        }
+        return ans;
+    };
+    for (int tt = 0; tt < T; tt += 1) {
+        int n;
+        cin >> n;
+        vector<Bowl> bowls(n);
+        for (auto &[h, r, R] : bowls) {
+            cin >> h >> r >> R;
+        }
+        vector<int> order(n);
+        iota(order.begin(), order.end(), 0);
+        double ans = 1e18;
+        do {
+            vector<double> where(n);
+            for (int i = 0; i < n; i += 1) {
+                for (int o = 0; o < i; o += 1) {
+                    where[i] = max(where[i], oper(bowls[order[i]], bowls[order[o]]) + where[o]);
+                }
+            }
+            double this_perm_ans = 0;
+            for (int i = 0; i < n; i += 1) {
+                this_perm_ans = max(this_perm_ans, where[i] + bowls[order[i]].h);
+            }
+            ans = min(ans, this_perm_ans);
+        } while (next_permutation(order.begin(), order.end()));
+        cout << trunc(ans) << "\n";
     }
-
-    cout << minTaxis(orders) << endl;
-    return 0;
 }
--- sol.cpp
+++ sol.cpp
@@ -1,143 +1,77 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,no-stack-protector,fast-math")
-
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+vector<int> par;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    char S[40001];
-    while (true) {
-        int M;
-        cin >> M;
-        if (!M) {
-            return 0;
-        }
-        cin >> S;
-        string s;
-        int it = 0;
-        while (S[it] != '\0') {
-            s.push_back(S[it]);
-            it += 1;
-        }
-//        string s;
-//        cin >> s;
-        s.push_back('\0');
-        int n = int(s.size());
-        if (M == 1) {
-            cout << n-1 << " " << 0 << "\n";
-            continue;
-        }
-        vector<int> rank(n);
-        vector<int> sa(n);
-        vector<int> lcp(n - 1);
-        int alphabet = 256;
-        vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
-        for (int i = 0; i < n; i++)
-            cnt[s[i]]++;
-        for (int i = 1; i < alphabet; i++)
-            cnt[i] += cnt[i - 1];
-        for (int i = 0; i < n; i++)
-            p[--cnt[s[i]]] = i;
-        c[p[0]] = 0;
-        int classes = 1;
-        for (int i = 1; i < n; i++) {
-            if (s[p[i]] != s[p[i - 1]])
-                classes++;
-            c[p[i]] = classes - 1;
-        }
-        vector<int> pn(n), cn(n);
-        for (int h = 0; (1 << h) < n; ++h) {
-            for (int i = 0; i < n; i++) {
-                pn[i] = p[i] - (1 << h);
-                if (pn[i] < 0)
-                    pn[i] += n;
-            }
-            fill(cnt.begin(), cnt.begin() + classes, 0);
-            for (int i = 0; i < n; i++)
-                cnt[c[pn[i]]]++;
-            for (int i = 1; i < classes; i++)
-                cnt[i] += cnt[i - 1];
-            for (int i = n - 1; i >= 0; i--)
-                p[--cnt[c[pn[i]]]] = pn[i];
-            cn[p[0]] = 0;
-            classes = 1;
-            for (int i = 1; i < n; i++) {
-                pair<int, int> cur = make_pair(c[p[i]], c[(p[i] + (1 << h)) % n]);
-                pair<int, int> prev = make_pair(c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]);
-                if (cur != prev)
-                    ++classes;
-                cn[p[i]] = classes - 1;
-            }
-            c.swap(cn);
-        }
-        for (int i = 0; i < n; i += 1) {
-            rank[i] = c[i];
-            sa[c[i]] = i;
-        }
-        int k = 0;
-        for (int i = 0; i < n; i += 1, k ? k -= 1 : 0) {
-            if (rank[i] == n-1) {
-                k = 0;
-                continue;
-            }
-            int j = sa[rank[i]+1];
-            while (i+k < n && j+k < n && s[i+k] == s[j+k]) k++;
-            lcp[rank[i]] = k;
-        }
-        n -= 1;
-        vector<int> left(n), right(n);
-        stack<int> stck;
-        for (int j = 0; j < n; j += 1) {
-            while (!stck.empty() && lcp[stck.top()] >= lcp[j]) stck.pop();
-            if (stck.empty()) left[j] = -1;
-            else left[j] = stck.top();
-            stck.push(j);
-        }
-        while (!stck.empty()) stck.pop();
-        for (int j = n - 1; j >= 0; j -= 1) {
-            while (!stck.empty() && lcp[stck.top()] >= lcp[j]) stck.pop();
-            if (stck.empty()) right[j] = n;
-            else right[j] = stck.top();
-            stck.push(j);
-        }
-        int maxi = -66;
-        int last = -66;
-        vector<vector<int>> st(20, vector<int>(n));
-        st[0] = sa;
-        for (int b = 1; b < 20; b += 1) {
-            for (int i = 0; i < n; i += 1) {
-                st[b][i] = st[b-1][i];
-                int nxt = i+(1 << (b-1));
-                if (nxt < n) st[b][i] = max(st[b][i], st[b-1][nxt]);
-            }
-        }
-        for (int i = 0; i < n; i += 1) {
-            int width = right[i]-left[i]-1;
-            int height = lcp[i];
-            if (width >= M - 1 && height >= maxi) {
-                if (height > maxi) {
-                    last = -1;
-                }
-                maxi = height;
-                int r = right[i];
-                int l = left[i] + 1;
-                int sz = r-l+1;
-                int bt = 0;
-                int btval = 1;
-                while (btval*2 <= sz) {
-                    btval *= 2;
-                    bt += 1;
-                }
-                last = max(last, max(st[bt][l], st[bt][r-(1ll << bt)+1]));
-            }
-        }
-        if (maxi == 0) {
-            cout << "none\n";
-        } else {
-            cout << maxi << " " << last << "\n";
-        }
+signed main() {
+    int n, m;
+    cin >> n >> m;
+    int cap[1005][1005];
+    memset(cap, 0, sizeof(cap));
+    int src = n + m;
+    int snk = n + m + 1;
+    int no_expences_or_misses = 0;
+    for (int i = 0; i < n; i += 1) {
+        int price;
+        cin >> price;
+        cap[i][snk] = price;
     }
+    for (int i = 0; i < m; i += 1) {
+        int u, v, profit;
+        cin >> u >> v >> profit;
+        u -= 1;
+        v -= 1;
+        cap[src][i + n] = profit;
+        cap[i + n][u] = 1e18;
+        cap[i + n][v] = 1e18;
+        no_expences_or_misses += profit;
+    }
+    auto min_cut = [&] () -> int {
+        vector<vector<int>> adj(n + m + 2);
+        for (int i = 0; i < n + m + 2; i += 1) {
+            for (int j = 0; j < n + m + 2; j += 1) {
+                if (cap[i][j] != 0) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        auto find_shortest = [&] () -> int {
+            par.assign(n + m + 2, -1);
+            queue<pair<int, int>> q;
+            q.push({1e18, src});
+            while (!q.empty()) {
+                auto [min_on_path, u] = q.front();
+                q.pop();
+                if (u == snk) {
+                    return min_on_path;
+                }
+                for (const auto &v : adj[u]) {
+                    if (cap[u][v] == 0 || par[v] != -1) {
+                        continue;
+                    }
+                    par[v] = u;
+                    q.push({min(min_on_path, cap[u][v]), v});
+                }
+            }
+            return 0;
+        };
+        int tot = 0;
+        while (true) {
+            int add = find_shortest();
+            if (add == 0) {
+                break;
+            }
+            int go = snk;
+            while (go != src) {
+                int from = par[go];
+                cap[from][go] -= add;
+                cap[go][from] += add;
+                go = from;
+            }
+            tot += add;
+        }
+        return tot;
+    };
+    cout << no_expences_or_misses - min_cut() << "\n";
 }
