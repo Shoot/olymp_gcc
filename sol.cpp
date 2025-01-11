8a22f77c2659b6f60cbf277346949014a1cfc778
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-11 04:33:08 +0300

    damn modified dijkstra on paths (bars path problem)

--- sol.cpp
+++ sol.cpp
@@ -2,80 +2,105 @@
 using namespace std;
 
 int main() {
-    while (true) {
-        int m, n, A, B;
-        cin >> m >> n >> A >> B;
-        if (m == 0 && n == 0 && A == 0 && B == 0) {
-            return 0;
+    string nvm;
+    int n, m;
+    cin >> nvm;
+    double S;
+    int map_number = 0;
+    while (cin >> n >> m) {
+        map_number += 1;
+        cout << "MAP " << map_number << "\n";
+        map<string, int> mp;
+        map<int, string> imp;
+        vector<pair<double, double>> pts(n);
+        vector<int> s(n);
+        for (int i = 0; i < n; i += 1) {
+            cin >> pts[i].first >> pts[i].second;
+            cin >> s[i];
+            string id;
+            cin >> id;
+            mp[id] = i;
+            imp[i] = id;
+            getline(cin, nvm);
         }
-        int a = A, b = B, C = ~(1<<31), M = (1<<16)-1;
-        auto gen = [&] () -> int {
-            a = 36969 * (a & M) + (a >> 16);
-            b = 18000 * (b & M) + (b >> 16);
-            return (C & ((a << 16) + b)) % 1000000;
-        };
-        struct Triple {
-            int x;
-            int y;
-            int z;
-        };
-        vector<Triple> triples(m + n);
+        vector<vector<int>> adj(n);
         for (int i = 0; i < m; i += 1) {
-            cin >> triples[i].x >> triples[i].y >> triples[i].z;
+            string x, y;
+            cin >> x >> y;
+            int X = mp[x];
+            int Y = mp[y];
+            adj[X].push_back(Y);
+            adj[Y].push_back(X);
         }
-        for (int i = m; i < m + n; i += 1) {
-            triples[i].x = gen();
-            triples[i].y = gen();
-            triples[i].z = gen();
+        vector<vector<double>> dist(n, vector<double>(n, 1e18));
+        for (int i = 0; i < n; i += 1) {
+            dist[i][i] = 0;
+            for (const auto &x : adj[i]) {
+                dist[i][x] = 15 * hypot(abs(pts[i].first-pts[x].first), abs(pts[i].second-pts[x].second));
+            }
         }
-        auto comp = [] (Triple a, Triple b) {
-            if (a.x != b.x) return a.x < b.x;
-            if (a.y != b.y) return a.y > b.y;
-            return a.z > b.z;
-        };
-        sort(triples.begin(), triples.end(), comp);
-        struct YZ {
-            int y;
-            int z;
-            bool operator<(const YZ& another) const {
-                if (y != another.y) return y < another.y;
-                return z > another.z;
-            }
-        };
-        int sz = int(triples.size());
-        vector<set<YZ>> last(sz+1);
-        auto can_continue_from = [&] (int from, int y, int z) -> bool {
-            auto it = last[from].lower_bound({y, z});
-            if (it == last[from].begin()) return false;
-            it--;
-            return it->z < z;
-        };
-        auto continue_from = [&] (int from, int y, int z) -> void {
-            auto it = last[from + 1].upper_bound({y, z});
-            while (it != last[from+1].end() && it->z >= z) {
-                last[from + 1].erase(it);
-                it = last[from + 1].upper_bound({y, z});
-            }
-            last[from + 1].insert({y, z});
-        };
-        for (const auto &[x, y, z] : triples) {
-            int l = 1, r = sz - 1;
-            int good = 0;
-            while (l <= r) {
-                int mid = (l + r) >> 1;
-                if (can_continue_from(mid, y, z)) {
-                    good = mid;
-                    l = mid + 1;
-                } else {
-                    r = mid - 1;
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                for (int k = 0; k < n; k += 1) {
+                    dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k]);
                 }
             }
-            continue_from(good, y, z);
         }
-        for (int ans = sz; ans >= 1; ans -= 1) {
-            if (!last[ans].empty()) {
-                cout << ans << "\n";
+        cin >> nvm;
+        assert(nvm == "ARRIVALS");
+        while (cin >> nvm) {
+            string time_from, from;
+            string time_to, to;
+            if (nvm == "MAP") {
+                cout << "B\n";
                 break;
+            }cout << "C\n";
+            time_from = nvm;
+            cin >> from >> time_to >> to >> S;
+            int start = mp[from];
+            int finish = mp[to];
+            int time = 0;
+            time += (time_to[0]-time_from[0])*10*60;
+            time += (time_to[1]-time_from[1])*60;
+            time += (time_to[3]-time_from[3])*10;
+            time += (time_to[4]-time_from[4]);
+            if (time < 0) {
+                time += 24*60;
+            }
+            struct Node {
+                int u;
+                double time;
+                double s;
+                unsigned long long seen;
+                vector<string> ans;
+            };
+            auto comp = [&] (const Node& one, const Node& another) -> bool {
+                double this_path = abs(S - (one.s - dist[one.u][finish]));
+                double another_path = abs(S - (another.s - dist[another.u][finish]));
+                return this_path < another_path;
+            };
+            multiset<Node, decltype(comp)> st(comp);
+            st.insert({start, 0, 0, 1ull << start, {"!"+imp[start]}});
+            st.insert({start, 15, double(s[start]), 1ull << start, {imp[start]}});
+            auto show = [&] (Node &x) {
+                cout << "PATH FOUND: " << x.s << " ";
+                for (const auto &y : x.ans) {
+                    cout << y << " ";
+                }
+                cout << "\n";
+            };
+            bool done = false;
+            while (!st.empty()) {
+                if (done) {
+                    break;
+                }
+                Node p = *st.begin();
+                st.erase(st.begin());
+                for (const auto &v : adj[p.u]) {
+                }
+            }
+            if (!done) {
+                cout << "Impossible!\n";
             }
         }
     }
