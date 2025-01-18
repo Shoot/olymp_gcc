85422c1c40c36c708c0f2fb9b8ba62fc12d0b819
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-18 12:59:03 +0300

    unsafe bigint

--- sol.cpp
+++ sol.cpp
@@ -1,90 +1,143 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr int base = 10;
+struct Big {
+    vector<int> z;
+    int sign;
+    Big &operator=(long long v) {
+        sign = v < 0 ? -1 : 1;
+        v *= sign;
+        z.clear();
+        for (; v > 0; v = v / base)
+            z.push_back((int)(v % base));
+        return *this;
+    }
+    Big &operator-=(const Big &other) {
+        if ((sign == 1 && *this >= other) || (sign == -1 && *this <= other)) {
+            for (int i = 0, carry = 0; i < other.z.size() || carry; ++i) {
+                z[i] -= carry + (i < other.z.size() ? other.z[i] : 0);
+                carry = z[i] < 0;
+                if (carry)
+                    z[i] += base;
+            }
+            trim();
+        } else {
+            *this = other - *this;
+            this->sign = -this->sign;
+        }
+        return *this;
+    }
+    Big &operator+=(const Big &other) {
+        for (int i = 0, carry = 0; i < other.z.size() || carry; ++i) {
+            if (i == z.size())
+                z.push_back(0);
+            z[i] += carry + (i < other.z.size() ? other.z[i] : 0);
+            carry = z[i] >= base;
+            if (carry)
+                z[i] -= base;
+        }
+        return *this;
+    }
 
-int main() {
-    int n, q;
-    cin >> n >> q;
-    struct Edge {
-        int u;
-        int v;
-    };
-    vector<Edge> edges;
-    edges.push_back({});
-    vector<int> deactivate_vert(q+1);
-    vector<int> deactivate_road(q+1);
-    for (int i = 1; i <= q; i += 1) {
-        char type;
-        cin >> type;
-        if (type == 'D') {
-            int u;
-            cin >> u;
-            deactivate_vert[i] = u;
-        }
-        if (type == 'A') {
-            int u, v;
-            cin >> u >> v;
-            edges.push_back({u, v});
-        }
-        if (type == 'R') {
-            int r;
-            cin >> r;
-            deactivate_road[i] = r;
-        }
+    friend Big operator+(Big a, const Big &b) {
+        a += b;
+        return a;
     }
-    vector<bool> sv(n+1, 1);
-    vector<bool> se(edges.size(), 1);
-    for (int i = 1; i <= q; i += 1) {
-        if (deactivate_vert[i] != 0) {
-            sv[deactivate_vert[i]] = 0;
-        }
+    friend Big operator-(Big a, const Big &b) {
+        a -= b;
+        return a;
     }
-    for (int i = 1; i <= q; i += 1) {
-        if (deactivate_road[i] != 0) {
-            se[deactivate_road[i]] = 0;
+
+    Big &operator/=(int v) {
+        if (v < 0)
+            sign = -sign, v = -v;
+        for (int i = (int)z.size() - 1, rem = 0; i >= 0; --i) {
+            long long cur = z[i] + rem * (long long)base;
+            z[i] = (int)(cur / v);
+            rem = (int)(cur % v);
         }
+        trim();
+        return *this;
     }
-    vector<int> ans(n+1, -1);
-    vector<multiset<int>> adj(n+1);
-    auto activate_and_write_ans = [&] (auto f, int u, int x) -> void {
-        if (ans[u] != -1) {
-            return;
-        }
-        ans[u] = x;
-        for (const auto &v : adj[u]) {
-            f(f, v, x);
-        }
-    };
-    for (int i = 1; i < edges.size(); i += 1) {
-        if (se[i]) {
-            auto [u, v] = edges[i];
-            adj[u].insert(v);
-            adj[v].insert(u);
-        }
+
+    Big operator/(int v) const { return Big(*this) /= v; }
+
+    bool operator<(const Big &v) const {
+        if (sign != v.sign)
+            return sign < v.sign;
+        if (z.size() != v.z.size())
+            return z.size() * sign < v.z.size() * v.sign;
+        for (int i = (int)z.size() - 1; i >= 0; i--)
+            if (z[i] != v.z[i])
+                return z[i] * sign < v.z[i] * sign;
+        return false;
     }
-    for (int i = 1; i <= n; i += 1) {
-        if (sv[i]) {
-            activate_and_write_ans(activate_and_write_ans, i, q);
-        }
+
+    bool operator>(const Big &v) const { return v < *this; }
+
+    bool operator<=(const Big &v) const { return !(v < *this); }
+
+    bool operator>=(const Big &v) const { return !(*this < v); }
+
+    void trim() {
+        while (!z.empty() && z.back() == 0)
+            z.pop_back();
+        if (z.empty())
+            sign = 1;
     }
-    for (int i = q; i >= 1; i -= 1) {
-        if (deactivate_vert[i]) {
-            activate_and_write_ans(activate_and_write_ans, deactivate_vert[i], i-1);
-        }
-        if (deactivate_road[i]) {
-            auto [u, v] = edges[deactivate_road[i]];
-            adj[u].insert(v);
-            adj[v].insert(u);
-            if (ans[u] == -1 && ans[v] != -1) {
-                // подключили к живому
-                activate_and_write_ans(activate_and_write_ans, u, i-1);
-            }
-            if (ans[v] == -1 && ans[u] != -1) {
-                // подключили к живому
-                activate_and_write_ans(activate_and_write_ans, v, i-1);
-            }
-        }
+
+    friend Big operator-(Big v) {
+        if (!v.z.empty())
+            v.sign = -v.sign;
+        return v;
     }
-    for (int i = 1; i <= n; i += 1) {
-        cout << ans[i] << "\n";
+
+    void read(const string &s) {
+        sign = 1;
+        z.clear();
+        int pos = 0;
+        while (pos < s.size() && (s[pos] == '-' || s[pos] == '+')) {
+            if (s[pos] == '-')
+                sign = -sign;
+            ++pos;
+        }
+        for (int i = (int)s.size() - 1; i >= pos; i -= 1) {
+            int x = 0;
+            for (int j = max(pos, i - 1 + 1); j <= i; j++)
+                x = x * 10 + s[j] - '0';
+            z.push_back(x);
+        }
+        trim();
     }
+
+    friend istream &operator>>(istream &stream, Big &v) {
+        string s;
+        stream >> s;
+        v.read(s);
+        return stream;
+    }
+    friend ostream &operator<<(ostream &stream, const Big &v) {
+        if (v.sign == -1)
+            stream << '-';
+        stream << (v.z.empty() ? 0 : v.z.back());
+        for (int i = (int)v.z.size() - 2; i >= 0; --i)
+            stream << setw(1) << setfill('0') << v.z[i];
+        return stream;
+    }
+};
+Big k;
+map<Big, Big> save;
+Big cnt(Big n) {
+    if (save.count(n)) {
+        return save[n];
+    }
+    if (n > k) {
+        return save[n]=cnt(n / 2)+cnt(n-n/2);
+    }
+    return save[n]=1;
+}
+signed main() {
+    Big n;
+    cin >> n >> k;
+    cout << cnt(n) << "\n";
 }
