2a6cfded0c2986124e97f37cf311121f22861529
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-19 23:32:08 +0300

    extremely cool fix max and then delete from heap. https://dmoj.ca/problem/ioi09p2

--- good.cpp
+++ good.cpp
@@ -1,60 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long MOD = 1e9+7;
-inline long long powm(long long n, int k) {
-    long long res = 1;
-    while (k > 0) {
-        if (k % 2) {
-            res *= n;
-            res %= MOD;
-        }
-        n *= n;
-        n %= MOD;
-        k /= 2;
-    }
-    return res;
-}
+
 signed main() {
-    long long words, strings, length_of_each;
-    cin >> words >> strings >> length_of_each;
-    struct Word {
-        long long len;
-        long long type;
-    };
-    vector<Word> wrds(words);
-    for (long long i = 0; i < words; i += 1) {
-        cin >> wrds[i].len >> wrds[i].type;
+    int n, W;
+    cin >> n >> W;
+    for (int i = 0; i < n; i += 1) {
+        int a, b;
+        cin >> a >> b;
     }
-    vector<long long> last(words+1);
-    vector<long long> ways_by_len(length_of_each+1);
-    ways_by_len[0] = 1;
-    for (long long i = 0; i <= length_of_each; i += 1) {
-        for (const auto &[len, type] : wrds) {
-            if (i + len <= length_of_each) {
-                ways_by_len[i + len] = (ways_by_len[i + len] + ways_by_len[i]) % MOD;
-                if (i + len == length_of_each) {
-                    last[type] = (last[type] + ways_by_len[i]) % MOD;
-                }
-            }
-        }
-    }
-    vector<long long> cnt(26);
-    for (long long i = 0; i < strings; i += 1) {
-        char c;
-        cin >> c;
-        cnt[c-'A'] += 1;
-    }
-    long long tot = 1;
-    for (int i : cnt) {
-        if (!i) continue;
-        long long cnt = 0;
-        for (int j: last) {
-            if (!j) continue;
-            cnt += powm(j, i);
-            cnt %= MOD;
-        }
-        tot *= cnt;
-        tot %= MOD;
-    }
-    cout << tot << "\n";
+    // Sum(a_1, a_2, ..,  a_k) * Max(b_1, b_2, .., b_k) \leq W
+    // maximal subset
 }
--- sol.cpp
+++ sol.cpp
@@ -1,58 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast,unroll-loops")
-#pragma GCC target("avx2,fma,bmi")
-vector<vector<bool>> Good(600, vector<bool>(600));
-vector<vector<pair<int, int>>> match(600, vector<pair<int, int>>(600, {-1, -1}));
-bool seen[600][600];
-int di[4] = {0, 0, -1, 1};
-int dj[4] = {1, -1, 0, 0};
-int n;
-bool try_kuhn(int i, int j) {
-    if (seen[i][j]) return false;
-    seen[i][j] = true;
-    for (int k = 0; k < 4; k += 1) {
-        int ni = i + di[k];
-        int nj = j + dj[k];
-        if (ni < 0 || ni >= n || nj < 0 || nj >= n || !Good[ni][nj]) {
-            continue;
+#define int long long
+signed main() {
+    int n, W;
+    cin >> n >> W;
+    struct Worker {
+        int Qual;
+        int Bound;
+        int idx;
+    };
+    vector<Worker> workers(n);
+    int idx = 0;
+    for (auto &[Q, B, i] : workers) {
+        cin >> B >> Q;
+        i = ++idx;
+    }
+    sort(workers.begin(), workers.end(), [](const Worker &a, const Worker &b) {
+        return double(a.Bound)/a.Qual < double(b.Bound)/b.Qual;
+    });
+    priority_queue<int> qs;
+    int qs_sum = 0;
+    struct Ans {
+        int Size;
+        int Cost;
+        bool operator<(const Ans& other) {
+            if (Size != other.Size) {
+                return Size < other.Size;
+            }
+            return Cost > other.Cost;
         }
-        if (match[ni][nj] == make_pair(-1, -1) || try_kuhn(match[ni][nj].first, match[ni][nj].second)) {
-            match[ni][nj] = {i, j};
-            return true;
+    };
+    Ans ans = {-1, 0};
+    int Max_idx = -1;
+    for (int i = 0; i < n; i += 1) {
+        double Max = double(workers[i].Bound)/workers[i].Qual;
+        qs.push(workers[i].Qual);
+        qs_sum += workers[i].Qual;
+        int limit_qs_sum = W / Max;
+        while (qs_sum > limit_qs_sum) {
+            qs_sum -= qs.top();
+            qs.pop();
+        }
+        if (ans < Ans{(int)qs.size(), (int)(Max * qs_sum)}) {
+            ans = Ans{(int)qs.size(), (int)(Max * qs_sum)};
+            Max_idx = i;
         }
     }
-    return false;
-};
-signed main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    for (int tt = 1; tt <= T; tt += 1) {
-        cin >> n;
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < n; j += 1) {
-                match[i][j] = {-1, -1};
-                char x;
-                cin >> x;
-                if (x == '#') {
-                    Good[i][j] = true;
-                } else {
-                    Good[i][j] = false;
-                }
-            }
+    cout << ans.Size << "\n";
+    priority_queue<pair<int,int>> qs_guys;
+    qs_sum = 0;
+    for (int i = 0; i <= Max_idx; i += 1) {
+        qs_guys.push({workers[i].Qual, workers[i].idx});
+        qs_sum += workers[i].Qual;
+    }
+    double Max = double(workers[Max_idx].Bound)/workers[Max_idx].Qual;
+    int limit_qs_sum = W / Max;
+    set<int> not_using;
+    while (qs_sum > limit_qs_sum) {
+        qs_sum -= qs_guys.top().first;
+        not_using.insert(qs_guys.top().second);
+        qs_guys.pop();
+    }
+    for (int i = 0; i <= Max_idx; i += 1) {
+        if (!not_using.count(workers[i].idx)) {
+            cout << workers[i].idx << "\n";
         }
-        int matching = 0;
-        for (int i = 0; i < n; i += 1) {
-            for (int j = 0; j < n; j += 1) {
-                if ((i + j) % 2 != 0 || !Good[i][j]) continue;
-                memset(seen, 0, sizeof(seen));
-                if (try_kuhn(i, j)) {
-                    matching += 1;
-                }
-            }
-        }
-        cout << "Case " << tt << ": " << matching << "\n";
     }
 }
