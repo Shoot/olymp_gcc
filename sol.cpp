ada98bb0f3cf1426102c4b0fbd4893bc9933e752
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-24 15:30:26 +0300

    basic

--- sol.cpp
+++ sol.cpp
@@ -1,28 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("sse,avx2")
+vector<vector<bool>> dir;
+vector<vector<bool>> complex_path;
+vector<vector<int>> all;
+void godo(int i, int j) {
+    for (int b4j = i; b4j < j; b4j += 1) {
+        complex_path[i][j] = complex_path[i][j] ^ (all[i][b4j]&dir[b4j][j]);
+    }
+}
 signed main() {
     int n;
-    while (cin >> n) {
-        struct Corner {
-            int x;
-            int y;
-            bool up_left;
-            int idx;
-            bool operator<(const Corner& other) const {
-                return tie(x, y) < tie(other.x, other.y);
-            }
-        };
-        vector<Corner> corners;
-        for (int i = 1; i <= n; i += 1) {
-            int x, y;
-            cin >> x >> y;
-            corners.push_back({x, y, true, i});
+    cin >> n;
+    all.assign(n, vector<int>(n));
+    dir.assign(n, vector<bool>(n));
+    complex_path.assign(n, vector<bool>(n));
+    for (int i = 0; i < n; i += 1) {
+        for (int j = i + 1; j < n; j += 1) {
+            char c;
+            cin >> c;
+            all[i][j] = c == '1';
         }
-        for (int i = 1; i <= n; i += 1) {
-            int x, y;
-            cin >> x >> y;
-            corners.push_back({x, y, false, i});
-        }
-        sort(corners.begin(), corners.end());
     }
+    int su = 0;
+    for (int i = n-1; i >= 0; i -= 1) {
+        for (int j = i + 1; j < n; j += 1) {
+            godo(i, j);
+            dir[i][j] = all[i][j] ^ complex_path[i][j];
+            su += dir[i][j];
+        }
+    }
+    cout << su << "\n";
 }
