e9058d3b794c18438c6b2e7ac389df03430f81cf
Shoot <11522779+Shoot@users.noreply.github.com>
2025-01-27 19:04:01 +0300

    центроидка сумма длин всех путей (сумма на пути учитвается)

--- sol.cpp
+++ sol.cpp
@@ -1,80 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+#define int long long
+signed main() {
     int n;
     cin >> n;
-    vector<int> a(n);
-    for (auto &x : a) {
-        cin >> x;
+    vector<int> val(n+1);
+    for (int i = 1; i <= n; i += 1) {
+        cin >> val[i];
     }
-    vector<int> gte_right(n);
-    vector<int> less_right(n);
-    stack<int> st;
-    a.push_back(1e9);
-    st.push(n);
-    for (int i = n-1; i >= 0; i -= 1) {
-        while (a[i] > a[st.top()]) {
-            st.pop();
-        }
-        gte_right[i] = st.top();
-        st.push(i);
+    vector<vector<int>> adj(n+1);
+    for (int i = 0; i < n-1; i += 1) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    while (!st.empty()) {
-        st.pop();
-    }
-    st.push(n);
-    a.back() = -1e9;
-    for (int i = n-1; i >= 0; i -= 1) {
-        while (a[i] <= a[st.top()]) {
-            st.pop();
-        }
-        less_right[i] = st.top();
-        st.push(i);
-    }
-    vector<int> res;
-    bool ans = true;
-    auto is_good = [&] (auto f, int l, int r) -> pair<int,int> {
-        pair<int,int> mn_mx = {a[l], a[l]};;
-        if (l == r) {
-            res.push_back(a[l]);
-            return mn_mx;
-        }
-        if (a[l+1] < a[l]) {
-            auto start_of_second_subtree = gte_right[l];
-            auto [mn_1, mx_1] = f(f, l+1, min(r, start_of_second_subtree-1));
-            ans &= mx_1 < a[l];
-            mn_mx.first = min(mn_mx.first, mn_1);
-            mn_mx.second = max(mn_mx.second, mx_1);
-            if (start_of_second_subtree <= r) {
-                auto [mn_2, mx_2] = f(f, start_of_second_subtree, r);
-                ans &= mn_2 >= a[l];
-                mn_mx.first = min(mn_mx.first, mn_2);
-                mn_mx.second = max(mn_mx.second, mx_2);
-            }
-        } else {
-            auto start_of_second_subtree = less_right[l];
-            auto [mn_1, mx_1] = f(f, l+1, min(r, start_of_second_subtree-1));
-            ans &= mn_1 >= a[l];
-            mn_mx.first = min(mn_mx.first, mn_1);
-            mn_mx.second = max(mn_mx.second, mx_1);
-            if (start_of_second_subtree <= r) {
-                auto [mn_2, mx_2] = f(f, start_of_second_subtree, r);
-                ans &= mx_2 < a[l];
-                mn_mx.first = min(mn_mx.first, mn_2);
-                mn_mx.second = max(mn_mx.second, mx_2);
+    vector<int> sz(n+1);
+    bitset<1'000'000> done;
+    auto calc_sizes = [&] (auto f, int u, int p) -> void {
+        sz[u] = 1;
+        for (const auto &v : adj[u]) {
+            if (v != p && !done[v]) {
+                f(f, v, u);
+                sz[u] += sz[v];
             }
         }
-        res.push_back(a[l]);
-        return mn_mx;
     };
-    is_good(is_good, 0, n-1);
-    if (ans) {
-        cout << "YES\n";
-        for (const auto &x : res) {
-            cout << x << " ";
+    int su = 0;
+    auto find_centroid_in_subtree = [&] (auto f, int u, int root, int p) -> int {
+        int subtree_size = sz[root];
+        for (const auto &x : adj[u]) {
+            if (x != p && !done[x] && sz[x] > subtree_size / 2) {
+                return f(f, x, root, u);
+            }
         }
-        cout << "\n";
-    } else {
-        cout << "NO\n";
-    }
+        return u;
+    };
+    vector<int> w(n+1);
+    vector<int> d(n+1);
+    vector<pair<int,int>> curr_w_d;
+    curr_w_d.reserve(1'000'000);
+    auto calc_w_d = [&] (auto f, int u, int p) -> void {
+        curr_w_d.push_back({w[u], d[u]});
+        for (const auto &x : adj[u]) {
+            if (x != p && !done[x]) {
+                d[x] = d[u] + 1;
+                w[x] = w[u] + val[x];
+                f(f, x, u);
+            }
+        }
+    };
+    auto solve = [&] (auto f, int root) -> void {
+        calc_sizes(calc_sizes, root, -1);
+        int centroid = find_centroid_in_subtree(find_centroid_in_subtree, root, root, -1);
+        int cnt = 1;
+        int suD = 0;
+        int suW = val[centroid];
+        int suWD = 0 * val[centroid];
+        for (const auto &x : adj[centroid]) {
+            if (!done[x]) {
+                curr_w_d.clear();
+                d[x] = 1;
+                w[x] = val[x];
+                calc_w_d(calc_w_d, x, centroid);
+                for (const auto &[W, D] : curr_w_d) {
+                    su += cnt * W * D;
+                    su += suD * W;
+                    su += suW * D;
+                    su += suWD;
+                }
+                for (const auto &[W, D] : curr_w_d) {
+                    cnt += 1;
+                    suW += W + val[centroid];
+                    suD += D;
+                    suWD += (W + val[centroid])*D;
+                }
+            }
+        }
+        done[centroid] = true;
+        for (const auto &x : adj[centroid]) {
+            if (!done[x]) {
+                f(f, x);
+            }
+        }
+    };
+    solve(solve, 1);
+    cout << su << "\n";
 }
