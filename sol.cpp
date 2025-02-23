646f800139757994fe66e9a8c44cf9b8d11c62a0
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-23 13:39:54 +0300

    // dp[сколько символов из копий s уже полностью позади][колво покрытых символов из строки t] = number_of_ways     // dp[колво покрытых символов из строки t] = number_of_ways

--- sol.cpp
+++ sol.cpp
@@ -2,41 +2,32 @@
 using namespace std;
 #define int long long
 signed main() {
-    int n, k;
-    cin >> n >> k;
-    vector<int> vals(n);
-    vector<int> sums(n);
-    for (int i = 0; i < n; i += 1) {
-        int x;
-        cin >> x;
-        int su = 0;
-        for (const auto &y : to_string(x)) {
-            su += y - '0';
-        }
-        vals[i] = x;
-        sums[i] = su;
-    }
-    vector<int> g(n);
-    iota(g.begin(), g.end(), 0LL);
-    auto can = [&] (double x) -> bool {
-        sort(g.begin(), g.end(), [&] (int a, int b) {
-            return double(vals[a]) - x*sums[a] > double(vals[b]) - x*sums[b];
-        });
-        int V = 0, S = 0;
-        for (int i = 0; i < k; i += 1) {
-            V += vals[g[i]];
-            S += sums[g[i]];
-        }
-        return double(V)/S >= x;
-    };
-    double l = 0, r = accumulate(vals.begin(), vals.end(), 0.);
-    while (r - l > 1e-8) {
-        double mid = (l + r) / 2;
-        if (can(mid)) {
-            l = mid;
-        } else {
-            r = mid;
+    constexpr int MOD = 1e9+7;
+    string s, t;
+    int k;
+    cin >> s >> t >> k;
+    // сохраняем позиции первой, второй, .. букв строки t которые находятся внутри s.
+    // dp[сколько символов из копий s уже полностью позади][колво покрытых символов из строки t] = number_of_ways
+    // dp[колво покрытых символов из строки t] = number_of_ways
+    vector<vector<int>> where(t.size());
+    for (int i = 0; i < s.size(); i += 1) {
+        for (int j = 0; j < t.size(); j += 1) {
+            if (s[i] == t[j]) {
+                where[j].push_back(i);
+            }
         }
     }
-    cout << fixed << setprecision(6) << l << "\n";
+    vector<int> dp(t.size()+1);
+    dp[0] = 1;
+    vector<int> nwdp(t.size()+1);
+    int ans = 0;
+    for (int pozad = 0; pozad < s.size()*k; pozad += 1) {
+        fill(nwdp.begin(), nwdp.end(), 0ll);
+        for (int pokr = t.size()-1; pokr >= 0; pokr -= 1) if (dp[pokr]) {
+            if (s[pozad%s.size()] == t[pokr]) {
+                (dp[pokr+1] += dp[pokr]) %= MOD;
+            }
+        }
+    }
+    cout << dp.back() << "\n";
 }
