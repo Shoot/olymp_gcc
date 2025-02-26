a1502399b64494633498a9a13d897f86c7b7a96a
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-26 12:56:49 +0300

    Напишите программу, которая находит разбиение по номеру либо номер по разбиению. Рассматриваются все разбиения натурального числа 𝑁  на сумму К  неотрицательных слагаемых (1≤𝑁≤32 , 2≤𝐾≤32 ). Суммы, отличающиеся только порядком слагаемых, считаем различными

--- sol.cpp
+++ sol.cpp
@@ -4,29 +4,74 @@
 #include <algorithm>
 #include <map>
 #include <cassert>
+#include <set>
 using namespace std;
 signed main() {
-    int n, k, p1, v1, p2, v2;
-    cin >> n >> k >> p1 >> v1 >> p2 >> v2;
-    if (p1%3 == p2%3) {
-        int rest = k - v1;
-        int cnt = 0;
-        for (int i = 1; i <= k; i += 1) {
-            for (int j = 1; j <= k; j += 1) {
-                if (i + j == rest) {
-                    cnt += 1;
+    map<pair<long long,long long>, long long> mem;
+    auto number_by_len_and_su = [&] (auto f, long long len, long long su) -> long long {
+        if (mem.count({len, su})) {
+            return mem[{len, su}];
+        }
+        if (len == 0) {
+            if (su == 0) {
+                return mem[{len, su}] = 1;
+            }
+            return mem[{len, su}] = 0;
+        }
+        long long ret = 0;
+        for (long long rem = 0; rem <= su; rem += 1) {
+            ret += f(f, len-1, su-rem);
+        }
+        return mem[{len, su}] = ret;
+    };
+    auto get = [&] (long long len, long long su, long long order) -> vector<long long> {
+        vector<long long> ret;
+        order -= 1;
+        for (long long i = 0; i < len; i += 1) {
+            long long curr = su;
+            su -= curr;
+            while (curr) {
+                long long cnt = number_by_len_and_su(number_by_len_and_su, len-i-1, su);
+                if (order >= cnt) {
+                    curr -= 1;
+                    su += 1;
+                    order -= cnt;
+                } else {
+                    break;
                 }
             }
+            ret.push_back(curr);
         }
-        if (v1 != v2) {
-            cnt = 0;
-        }
-        cout << cnt << "\n";
-        return 0;
-    }
-    if (v1 + v2 < k) {
-        cout << 1 << "\n";
+        return ret;
+    };
+    long long tp;
+    cin >> tp;
+    if (tp == 0) {
+        long long len, su, order;
+        cin >> len >> su >> order;
+        for (const auto &x : get(len, su, order)) {
+            cout << x << " ";
+        }cout << "\n";
     } else {
-        cout << 0 << "\n";
+        long long sz;
+        cin >> sz;
+        vector<long long> our(sz);
+        long long su = 0;
+        for (auto &x : our) {
+            cin >> x;
+            su += x;
+        }
+        long long l = 1, r = 1e18;
+        long long good = -1;
+        while (l <= r) {
+            long long mid = (l + r) >> 1;
+            if (get(sz, su, mid) > our) {
+                l = mid + 1;
+            } else {
+                good = mid;
+                r = mid - 1;
+            }
+        }
+        cout << good << "\n";
     }
 }
