517e7e136139c6d5e11c3dd97a3631bd89a5b625
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-26 10:20:43 +0300

    modif dijkstra https://www.acwing.com/problem/content/description/2615/

--- sol.cpp
+++ sol.cpp
@@ -1,90 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-const ll MOD = (1ll << 61) - 1;
-ll mod(__int128 x) {
-    if (x >= 0 && x < MOD) {
-        return x;
-    }
-    if (x-MOD >= 0 && x-MOD < MOD) {
-        return x-MOD;
-    }
-    ll ret = (x & MOD) + (x >> 61);
-    if (ret >= MOD) {
-        ret -= MOD;
-    }
-    return ret;
-}
-ll mul(ll a, ll b) {
-    return mod(__int128(a)*b);
-}
-ll sum(ll a, ll b) {
-    return mod(a+b);
-}
-ll powm(ll a, ll b) {
-    ll ans = 1;
-    while (b) {
-        if (b&1) {
-            ans = mul(ans, a);
-        }
-        b >>= 1;
-        a = mul(a, a);
-    }
-    return ans;
-}
-#include <bits/stdc++.h>
-using namespace std;
-vector<long long> manacher_odd(string &s) {
-    long long n = (long long) s.size();
-    vector<long long> d(n, 1);
-    long long l = 0, r = 0;
-    for (long long i = 1; i < n; i++) {
-        if (i < r)
-            d[i] = min(r - i + 1, d[l + r - i]);
-        while (i - d[i] >= 0 && i + d[i] < n && s[i - d[i]] == s[i + d[i]])
-            d[i]++;
-        if (i + d[i] - 1 > r)
-            l = i - d[i] + 1, r = i + d[i] - 1;
-    }
-    return d;
-}
-vector<long long> manacher_even(string &s) {
-    long long n = (long long) s.size();
-    vector<long long> d(n, 0);
-    long long l = -1, r = -1;
-    for (long long i = 0; i < n - 1; i++) {
-        if (i < r)
-            d[i] = min(r - i, d[l + r - i - 1]);
-        while (i - d[i] >= 0 && i + d[i] + 1 < n && s[i - d[i]] == s[i + d[i] + 1])
-            d[i]++;
-        if (i + d[i] > r)
-            l = i - d[i] + 1, r = i + d[i];
-    }
-    return d;
-}
 signed main() {
-    const ll base = 31;
-    const ll ibase = powm(base, MOD-2);
-    string s;
-    cin >> s;
-    int n = int(s.size());
-    s = string(s.end()-n/2, s.end()) + s + string(s.begin(), s.begin()+n/2);
-    auto d1 = manacher_odd(s);
-    auto d2 = manacher_even(s);
-    int maxi = 0;
-    for (long long i = 0; i < s.size(); i += 1) {
-        int chet = d2[i]*2;
-        int nechet = d1[i]*2-1;
-        if (nechet > n) {
-            maxi = max(maxi, n - (n % 2 == 0));
-        } else {
-            maxi = max(maxi, nechet);
+    int n;
+    cin >> n;
+    vector<vector<int>> vals(n);
+    int period = 1;
+    for (int i = 0; i < n; i += 1) {
+        int sz;
+        cin >> sz;
+        vals[i].resize(sz);
+        for (auto &x : vals[i]) {
+            cin >> x;
         }
-        if (chet > n) {
-            maxi = max(maxi, n - (n % 2 == 1));
-        } else {
-            maxi = max(maxi, chet);
+        period = lcm(period, sz);
+    }
+    vals.insert(vals.begin(), {0});
+    n = vals.size();
+//    cout << "P = " << period << "\n";
+    // [vertice][time%LCM] = min
+    struct Node {
+        int cell;
+        int time;
+        auto operator<(const Node &other) const {
+            return make_tuple(cell, time) < make_tuple(other.cell, other.time);
+        }
+    };
+    map<Node, int> damage_by_state;
+    set<Node> dijkstra_decided;
+    damage_by_state[{0, 0}] = 0;
+    set<pair<int,Node>> dijkstra_set;
+    dijkstra_set.insert({0, {0, 0}});
+    while (!dijkstra_set.empty()) {
+        auto [d, tp] = *dijkstra_set.begin();
+//        cout << d << "()\n";
+//        cout << tp.cell << " " << tp.time << "!!\n";
+//        int tmp;
+//        cin >> tmp;
+        dijkstra_set.erase(dijkstra_set.begin());
+        if (dijkstra_decided.count(tp)) {
+            continue;
+        }
+        dijkstra_decided.insert(tp);
+        int nxt_time = (tp.time + 1) % period;
+//        cout << nxt_time << "!\n";
+
+        int from = damage_by_state[tp];
+        for (int k = -1; k <= 1; k += 1) if (tp.cell+k >= 0 && tp.cell+k < n) {
+            auto to = Node{tp.cell+k, nxt_time};
+            if (!damage_by_state.count(to) || damage_by_state[to] >
+            from + vals[to.cell][nxt_time%vals[to.cell].size()]) {
+                damage_by_state[to] = from + vals[to.cell][nxt_time%vals[to.cell].size()];
+                dijkstra_set.insert({damage_by_state[to], to});
+            }
         }
     }
-    cout << maxi << endl;
+    int mini = 1e9;
+    for (const auto &[state, cost] : damage_by_state) {
+        if (state.cell == n-1) {
+            mini = min(mini, cost);
+        }
+    }
+    cout << mini << "\n";
 }
