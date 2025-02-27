607af7ff2161bd99f3f14d391b141ffd9936b1ca
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-27 15:29:40 +0300

    min_cut() Karp

--- good.cpp
+++ good.cpp
@@ -1,24 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-signed main() {
-    int tp;
-    cin >> tp;
-    if (tp == 0) {
-        int len, su, order;
-        cin >> len >> su >> order;
-        int curr_su = 0;
-        auto gen = [&] (int idx) -> void {
-            for (int nxt = )
-        };
-    } else {
-        int sz;
-        cin >> sz;
-        for (int i = 0; i < sz; i += 1) {
 
+signed main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> row(n);
+    vector<int> col(m);
+    for (auto &x : row) {
+        cin >> x;
+    }
+    for (auto &x : col) {
+        cin >> x;
+    }
+    vector<vector<int>> a(n, vector<int>(m));
+    for (auto &x : a) {
+        for (auto &y : x) {
+            cin >> y;
+        }
+    }
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            if (a[i][j] != -1) {
+
+            }
         }
     }
 }
--- sol.cpp
+++ sol.cpp
@@ -1,77 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <map>
-#include <cassert>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+vector<int> par;
+
 signed main() {
-    map<pair<long long,long long>, long long> mem;
-    auto number_by_len_and_su = [&] (auto f, long long len, long long su) -> long long {
-        if (mem.count({len, su})) {
-            return mem[{len, su}];
-        }
-        if (len == 0) {
-            if (su == 0) {
-                return mem[{len, su}] = 1;
-            }
-            return mem[{len, su}] = 0;
-        }
-        long long ret = 0;
-        for (long long rem = 0; rem <= su; rem += 1) {
-            ret += f(f, len-1, su-rem);
-        }
-        return mem[{len, su}] = ret;
-    };
-    auto get = [&] (long long len, long long su, long long order) -> vector<long long> {
-        vector<long long> ret;
-        order -= 1;
-        for (long long i = 0; i < len; i += 1) {
-            long long curr = su;
-            su -= curr;
-            while (curr) {
-                long long cnt = number_by_len_and_su(number_by_len_and_su, len-i-1, su);
-                if (order >= cnt) {
-                    curr -= 1;
-                    su += 1;
-                    order -= cnt;
-                } else {
-                    break;
+    int n, m;
+    cin >> n >> m;
+    int cap[1005][1005];
+    memset(cap, 0, sizeof(cap));
+    int src = 1;
+    int snk = n;
+    int no_expences_or_misses = 0;
+    for (int i = 0; i < m; i += 1) {
+        int u, v, profit;
+        cin >> u >> v >> profit;
+        cap[u][v] = profit;
+    }
+    auto min_cut = [&] () -> int {
+        vector<vector<int>> adj(n + 1);
+        for (int i = 0; i <= n; i += 1) {
+            for (int j = 0; j <= n; j += 1) {
+                if (cap[i][j] != 0) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
-            ret.push_back(curr);
         }
-        return ret;
-    };
-    long long tp;
-    cin >> tp;
-    if (tp == 0) {
-        long long len, su, order;
-        cin >> len >> su >> order;
-        for (const auto &x : get(len, su, order)) {
-            cout << x << " ";
-        }cout << "\n";
-    } else {
-        long long sz;
-        cin >> sz;
-        vector<long long> our(sz);
-        long long su = 0;
-        for (auto &x : our) {
-            cin >> x;
-            su += x;
-        }
-        long long l = 1, r = 1e18;
-        long long good = -1;
-        while (l <= r) {
-            long long mid = (l + r) >> 1;
-            if (get(sz, su, mid) > our) {
-                l = mid + 1;
-            } else {
-                good = mid;
-                r = mid - 1;
+        auto find_shortest = [&] () -> int {
+            par.assign(n + 1, -1);
+            queue<pair<int, int>> q;
+            q.push({1e18, src});
+            while (!q.empty()) {
+                auto [min_on_path, u] = q.front();
+                q.pop();
+                if (u == snk) {
+                    return min_on_path;
+                }
+                for (const auto &v : adj[u]) {
+                    if (cap[u][v] == 0 || par[v] != -1) {
+                        continue;
+                    }
+                    par[v] = u;
+                    q.push({min(min_on_path, cap[u][v]), v});
+                }
             }
+            return 0;
+        };
+        int tot = 0;
+        while (true) {
+            int add = find_shortest();
+            if (add == 0) {
+                break;
+            }
+            int go = snk;
+            while (go != src) {
+                int from = par[go];
+                cap[from][go] -= add;
+                cap[go][from] += add;
+                go = from;
+            }
+            tot += add;
         }
-        cout << good << "\n";
-    }
+        return tot;
+    };
+    cout << min_cut() << "\n";
 }
