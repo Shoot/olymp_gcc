c1c11fac26f50fa663922174074e4259e9d60b29
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-28 10:08:24 +0300

    src ->(inf-a[i][j]) i, j+n -> (inf-a[i][j]) snk, i->j(inf) if a[i][j] != -1

--- sol.cpp
+++ sol.cpp
@@ -1,30 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-signed main () {
+signed main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> cap(2*n+2, vector<int>(2*n+2));
-    for (int i = 1; i <= n; i += 1) {
-        cap[2*i][2*i+1] = 1;
+    vector<int> row(n);
+    vector<int> col(m);
+    for (auto &x : row) {
+        cin >> x;
     }
-    vector<set<int>> xx(n+1);
+    for (auto &x : col) {
+        cin >> x;
+    }
+    vector<vector<int>> a(n, vector<int>(m));
+    for (auto &x : a) {
+        for (auto &y : x) {
+            cin >> y;
+        }
+    }
+    vector<vector<int>> cap(n+m+2, vector<int>(n+m+2));
+    int src = n + m;
+    int snk = n + m + 1;
+    for (int i = 0; i < n; i += 1) {
+        cap[src][i] = row[i];
+    }
     for (int i = 0; i < m; i += 1) {
-        int u, v;
-        cin >> u >> v;
-        xx[u].insert(v);
-        xx[v].insert(u);
-        cap[2*u+1][2*v] = 1e9;
-        cap[2*v+1][2*u] = 1e9;
+        cap[i+n][snk] = col[i];
+    }
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            if (a[i][j] != -1) {
+                cap[src][i] -= a[i][j];
+                cap[j+n][snk] -= a[i][j];
+            } else {
+                cap[i][j+n] = 1e18;
+            }
+        }
     }
     vector<int> par;
-    vector<int> curr;
-    curr.reserve(1000);
-    auto min_cut = [&] (int src, int snk) -> int {
-        curr.clear();
-        vector<vector<int>> adj(2*n+2);
-        for (int i = 0; i <= 2*n+1; i += 1) {
-            for (int j = 0; j <= 2*n+1; j += 1) {
+    auto min_cut = [&] () -> int {
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i += 1) {
+            for (int j = 0; j < n; j += 1) {
                 if (cap[i][j] != 0) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
@@ -32,9 +49,9 @@ signed main () {
             }
         }
         auto find_shortest = [&] () -> int {
-            par.assign(2*n+2, -1);
+            par.assign(n, -1);
             queue<pair<int, int>> q;
-            q.push({1e9, src});
+            q.push({1e18, src});
             while (!q.empty()) {
                 auto [min_on_path, u] = q.front();
                 q.pop();
@@ -66,29 +83,24 @@ signed main () {
             }
             tot += add;
         }
-        for (int i = 1; i <= n; i += 1) {
-            if (par[2*i+1] == -1 && par[2*i] != -1) {
-                curr.push_back(i);
-            }
-        }
         return tot;
     };
-    auto cap_copy = cap;
-    vector<int> ans(n);
-    iota(ans.begin(), ans.end(), 1);
-    for (int from = 1; from <= n; from += 1) {
-        for (int to = 1; to <= n; to += 1) if (from != to && !xx[from].count(to)) {
-            cap = cap_copy;
-            if (min_cut(from*2+1, to*2) != curr.size()) {
-                continue;
-            }
-            if (curr.size() < ans.size()) {
-                ans = curr;
+    auto prev = cap;
+    int N = n;
+    n = n + m + 2;
+    min_cut();
+    for (int i = 0; i < N; i += 1) {
+        for (int j = 0; j < m; j += 1) if (a[i][j] == -1) {
+            a[i][j] = prev[i][j+N] - cap[i][j+N];
+            if (a[i][j] < 0) {
+                cout << -1 << "\n";
+                return 0;
             }
         }
     }
-    cout << ans.size() << "\n";
-    for (const auto &x : ans) {
-        cout << x << " ";
-    }cout << "\n";
+    for (const auto &x : a) {
+        for (const auto &y : x) {
+            cout << y << " ";
+        }cout << "\n";
+    }
 }
