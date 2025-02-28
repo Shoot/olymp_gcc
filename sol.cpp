c73c25a56fd6d63794f90d6d454d73d41e7d713c
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-28 15:40:30 +0300

    rows&columns limits, fixed a_ij, maximise sum

--- sol.cpp
+++ sol.cpp
@@ -4,28 +4,41 @@ using namespace std;
 signed main() {
     int n, m;
     cin >> n >> m;
-    vector<vector<int>> cap(n+m+2, vector<int>(n+m+2));
-    int src = n+m;
-    int snk = n+m+1;
-    for (int i = 0; i < m; i += 1) {
-        cap[src][n+i] = 1;
+    vector<int> row(n);
+    vector<int> col(m);
+    for (auto &x : row) {
+        cin >> x;
     }
-    for (int i = 0; i < n; i += 1) {
-        cap[i][snk] = 1;
+    for (auto &x : col) {
+        cin >> x;
     }
-    for (int i = 0; i < n; i += 1) {
-        int sz;
-        cin >> sz;
-        for (int j = 0; j < sz; j += 1) {
-            int x;
-            cin >> x;
-            x -= 1;
-            cap[n+x][i] = 1e18;
+    vector<vector<int>> a(n, vector<int>(m));
+    for (auto &x : a) {
+        for (auto &y : x) {
+            cin >> y;
         }
     }
-    n = n + m + 2;
+    vector<vector<int>> cap(n+m+2, vector<int>(n+m+2));
+    int src = n + m;
+    int snk = n + m + 1;
+    for (int i = 0; i < n; i += 1) {
+        cap[src][i] = row[i];
+    }
+    for (int i = 0; i < m; i += 1) {
+        cap[i+n][snk] = col[i];
+    }
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < m; j += 1) {
+            if (a[i][j] != -1) {
+                cap[src][i] -= a[i][j];
+                cap[j+n][snk] -= a[i][j];
+            } else {
+                cap[i][j+n] = 1e18;
+            }
+        }
+    }
+    vector<int> par;
     auto min_cut = [&] () -> int {
-        vector<int> par;
         vector<vector<int>> adj(n);
         for (int i = 0; i < n; i += 1) {
             for (int j = 0; j < n; j += 1) {
@@ -72,5 +85,22 @@ signed main() {
         }
         return tot;
     };
-    cout << min_cut() << "\n";
+    auto prev = cap;
+    int N = n;
+    n = n + m + 2;
+    min_cut();
+    for (int i = 0; i < N; i += 1) {
+        for (int j = 0; j < m; j += 1) if (a[i][j] == -1) {
+                a[i][j] = prev[i][j+N] - cap[i][j+N];
+                if (a[i][j] < 0) {
+                    cout << -1 << "\n";
+                    return 0;
+                }
+            }
+    }
+    for (const auto &x : a) {
+        for (const auto &y : x) {
+            cout << y << " ";
+        }cout << "\n";
+    }
 }
