373b26441717938a34df64696aa592b847deb30c
Shoot <11522779+Shoot@users.noreply.github.com>
2025-02-28 13:36:38 +0300

    номер ПСП

--- sol.cpp
+++ sol.cpp
@@ -1,61 +1,38 @@
 #include <iostream>
-#include <vector>
-#include <numeric>
-#include <algorithm>
 #include <map>
-#include <cassert>
 using namespace std;
 signed main() {
-    long long n, k;
-    map<pair<long long,long long>,long long> mem;
-    auto C = [&] (auto f, long long n, long long k) -> long long {
-        if (mem.count({n, k})) {
-            return mem[{n, k}];
+    long long n;
+    cin >> n;
+    n *= 2;
+    string s;
+    cin >> s;
+    long long balance = 0;
+    long long more_than = 0;
+    map<pair<long long,long long>, long long> mem;
+    auto number_by_balance_and_len = [&] (auto f, long long bal, long long len) -> long long {
+        if (mem.count({bal, len})) {
+            return mem[{bal, len}];
         }
-        if (k == 0) {
-            return mem[{n, k}] = 1;
+        if (abs(bal) > len) {
+            return mem[{bal, len}] = 0;
         }
-        if (n == 0) {
-            return mem[{n, k}] = 0;
-        }
-        return mem[{n, k}] = f(f, n-1, k)+f(f, n-1, k-1);
-    };
-    cin >> n >> k;
-    vector<long long> a(k);
-    for (auto &x : a) {
-        cin >> x;
-    }
-    auto gen = [&] (long long order) -> vector<long long> {
-        vector<long long> ret(k);
-        order -= 1;
-        long long curr = 1;
-        for (long long i = 0; i < k; i += 1) {
-            while (curr != n-(k-i-1) && order >= C(C, n-curr, k-i-1)) {
-                order -= C(C, n-curr, k-i-1);
-                curr += 1;
+        if (bal == 0) {
+            if (len == 0) {
+                return mem[{bal, len}] = 1;
             }
-            ret[i] = curr;
-            curr = curr + 1;
+            return mem[{bal, len}] = f(f, bal+1, len-1);
         }
-        return ret;
+        return mem[{bal, len}] = f(f, bal-1, len-1) + f(f, bal+1, len-1);
     };
-    long long good = -1;
-    long long l = 1, r = C(C, n, k);
-    while (l <= r) {
-        long long mid = (l + r) >> 1;
-        auto g = gen(mid);
-        if (g <= a) {
-            good = mid;
-            l = mid + 1;
+    for (long long i = 0; i < n; i += 1) {
+        if (s[i] == ')') {
+            balance -= 1;
+            long long rest = n - i - 1;
+            more_than += number_by_balance_and_len(number_by_balance_and_len, -(balance+2), rest);
         } else {
-            r = mid - 1;
+            balance += 1;
         }
     }
-    if (good == C(C, n, k)) {
-        cout << 0 << "\n";
-        return 0;
-    }
-    for (const auto &x : gen(good+1)) {
-        cout << x << " ";
-    }cout << "\n";
+    cout << more_than << "\n";
 }
