e859c3603f6458eb4a4caa40bb2e3ccadb1441d8
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-02 14:22:15 +0300

    FAST bitset w/ pragmas. Дана бинарная матрица. Возвести ее в квадрат по модулю 2.

--- sol.cpp
+++ sol.cpp
@@ -1,70 +1,31 @@
 #include <bits/stdc++.h>
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx2", "popcnt")
 using namespace std;
-long long N = 3e5+1;
-__int128 MOD = (1LL << 61) - 1;
-long long mul(long long a, long long b) {
-    __int128 val = __int128(a) * b;
-    val = (val >> 61) + (val & MOD);
-    if (val >= MOD) {
-        val -= MOD;
-    }
-    return val;
-}
 signed main() {
-    long long n;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<bitset<4'000>> g(4000);
+    vector<bitset<4'000>> ans(4000);
+    int n;
     cin >> n;
-    vector<long long> a(n);
-    for (auto &x : a) {
-        cin >> x;
-    }
-    vector<long long> number_of_multiples(N);
-    for (const auto &x : a) {
-        number_of_multiples[x] += 1;
-    }
-    for (long long i = 1; i < N; i += 1) {
-        for (long long j = 2*i; j < N; j += i) {
-            number_of_multiples[i] += number_of_multiples[j];
+    char c;
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < n; j += 1) {
+            cin >> c;
+            g[i][j] = c-'0';
         }
     }
-    vector<long long> fact(N);
-    fact[0] = 1;
-    for (long long i = 1; i < N; i += 1) {
-        fact[i] = mul(fact[i-1], i);
-    }
-    vector<long long> ifact(N);
-    auto powm = [&] (long long a, long long b) -> long long {
-        long long ans = 1;
-        while (b) {
-            if (b&1) {
-                ans = mul(ans, a);
-            }
-            b >>= 1;
-            a = mul(a, a);
-        }
-        return ans;
-    };
-    ifact.back() = powm(fact.back(), MOD-2);
-    for (long long i = N-2; i >= 0; i -= 1) {
-        ifact[i] = mul(ifact[i+1], i+1);
-    }
-    auto C = [&] (long long n, long long k) -> long long {
-        if (k > n) {
-            return 0;
-        }
-        return mul(mul(fact[n], ifact[n-k]), ifact[k]);
-    };
-    vector<vector<long long>> dp(24, vector<long long>(N));
-    for (long long sz = 1; sz <= 23; sz += 1) {
-        for (long long gcd = N-1; gcd >= 1; gcd -= 1) {
-            dp[sz][gcd] = C(number_of_multiples[gcd], sz);
-            for (long long j = 2 * gcd; j < N; j += gcd) {
-                ((dp[sz][gcd] -= dp[sz][j]) += MOD) %= MOD;
+    for (int i = 0; i < n; i += 1) {
+        for (int j = 0; j < n; j += 1) {
+            if (g[i][j]) {
+                ans[i] ^= g[j];
             }
         }
-        if (dp[sz][1]) {
-            cout << sz << "\n";
-            return 0;
-        }
     }
-    cout << -1 << "\n";
+    int cnt = 0;
+    for (const auto &x : ans) {
+        cnt += x.count();
+    }
+    cout << cnt << "\n";
 }
