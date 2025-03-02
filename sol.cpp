e4431ae10337b98048c0c9132a95f16050cb66b7
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-02 13:30:04 +0300

    dp[sz][gcd] — колво таких подмножеств. дп с выключениями посчитанных dp[sz][gcd] -= dp[sz][j], j % gcd = 0

--- sol.cpp
+++ sol.cpp
@@ -1,58 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
+long long N = 3e5+1;
+__int128 MOD = (1LL << 61) - 1;
+long long mul(long long a, long long b) {
+    __int128 val = __int128(a) * b;
+    val = (val >> 61) + (val & MOD);
+    if (val >= MOD) {
+        val -= MOD;
+    }
+    return val;
+}
 signed main() {
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(n);
-    iota(a.begin(), a.end(), 1);
-    vector<int> lp(1e7+1);
-    for (int i = 2; i < lp.size(); i += 1) {
-        if (lp[i] == 0) {
-            lp[i] = i;
-            for (int j = i*i; j < lp.size(); j += i) if (!lp[j]) {
-                lp[j] = i;
-            }
-        }
+    long long n;
+    cin >> n;
+    vector<long long> a(n);
+    for (auto &x : a) {
+        cin >> x;
     }
-    vector<int> primes;
-    primes.reserve(1000);
-    auto get_primes = [&] (const int x) -> void {
-        primes.clear();
-        int last_lp = 0;
-        int temp = x;
-        while (temp != 1) {
-            if (lp[temp] != last_lp) {
-                last_lp = lp[temp];
-                primes.push_back(last_lp);
-            }
-            temp /= lp[temp];
-        }
-    };
-    vector<int> cnt(n+1);
+    vector<long long> number_of_multiples(N);
     for (const auto &x : a) {
-        get_primes(x);
-        int sz = int(primes.size());
-        for (int m = 1; m < (1LL << sz); m += 1) {
-            int val = 1;
-            for (int bt = 0; bt < sz; bt += 1) if (m&(1LL << bt)) {
-                val *= primes[bt];
-            }
-            cnt[val] += 1;
+        number_of_multiples[x] += 1;
+    }
+    for (long long i = 1; i < N; i += 1) {
+        for (long long j = 2*i; j < N; j += i) {
+            number_of_multiples[i] += number_of_multiples[j];
         }
     }
-    auto get_number_of_not_coprime_with_x = [&] (const int x) -> int {
-        int tot = 0;
-        get_primes(x);
-        int sz = int(primes.size());
-        for (int m = 1; m < (1LL << sz); m += 1) {
-            int val = 1;
-            for (int bt = 0; bt < sz; bt += 1) if (m&(1LL << bt)) {
-                val *= primes[bt];
+    vector<long long> fact(N);
+    fact[0] = 1;
+    for (long long i = 1; i < N; i += 1) {
+        fact[i] = mul(fact[i-1], i);
+    }
+    vector<long long> ifact(N);
+    auto powm = [&] (long long a, long long b) -> long long {
+        long long ans = 1;
+        while (b) {
+            if (b&1) {
+                ans = mul(ans, a);
             }
-            clog << val << ": " << cnt[val] * (__builtin_popcount(m)%2?1:-1) << "\n";
-            tot += cnt[val] * (__builtin_popcount(m)%2?1:-1);
+            b >>= 1;
+            a = mul(a, a);
         }
-        return tot;
+        return ans;
     };
-    cout << n-get_number_of_not_coprime_with_x(k) << "\n";
+    ifact.back() = powm(fact.back(), MOD-2);
+    for (long long i = N-2; i >= 0; i -= 1) {
+        ifact[i] = mul(ifact[i+1], i+1);
+    }
+    auto C = [&] (long long n, long long k) -> long long {
+        if (k > n) {
+            return 0;
+        }
+        return mul(mul(fact[n], ifact[n-k]), ifact[k]);
+    };
+    vector<vector<long long>> dp(24, vector<long long>(N));
+    for (long long sz = 1; sz <= 23; sz += 1) {
+        for (long long gcd = N-1; gcd >= 1; gcd -= 1) {
+            dp[sz][gcd] = C(number_of_multiples[gcd], sz);
+            for (long long j = 2 * gcd; j < N; j += gcd) {
+                ((dp[sz][gcd] -= dp[sz][j]) += MOD) %= MOD;
+            }
+        }
+        if (dp[sz][1]) {
+            cout << sz << "\n";
+            return 0;
+        }
+    }
+    cout << -1 << "\n";
 }
