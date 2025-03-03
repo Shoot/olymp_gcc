e1a3af29980c1ff2ce172499ccdaf1784c1493d1
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-04 02:31:26 +0300

    Кол-во точек на треугольнике — битсет. Векторное произведение > 0, RayXShit, когда точка в левой полуплоскости относительно луча

--- sol.cpp
+++ sol.cpp
@@ -1,99 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx2", "popcnt")
 signed main() {
-    struct Node {
-        int max = 1e9;
-        int l = -1, r = -1;
-    };
-    vector<Node> tree;
-    tree.reserve(10'000'000);
-    auto get_max = [&] (auto f, int u, int tl, int tr, int l, int r) -> int {
-        if (u == -1) {
-            return 1e9;
-        }
-        if (tl == l && tr == r) {
-            return tree[u].max;
-        }
-        int tm = (tl + tr) >> 1;
-        int ret = -1e9;
-        if (l <= tm) {
-            ret = max(ret, f(f, tree[u].l, tl, tm, l, min(tm, r)));
-        }
-        if (r >= tm+1) {
-            ret = max(ret, f(f, tree[u].r, tm+1, tr, max(l, tm+1), r));
-        }
-        return ret;
-    };
-    auto recalc = [&] (int ul, int ur) -> int {
-        int val = -1e9;
-        if (ul != -1) {
-            val = max(val, tree[ul].max);
-        } else {
-            val = max(val, (int)1e9);
-        }
-        if (ur != -1) {
-            val = max(val, tree[ur].max);
-        } else {
-            val = max(val, (int)1e9);
-        }
-        tree.push_back({val, ul, ur});
-        return tree.size()-1;
-    };
-    auto set = [&] (auto f, int u, int tl, int tr, int pos, int val) -> int {
-        int currL = u == -1 ? -1 : tree[u].l;
-        int currR = u == -1 ? -1 : tree[u].r;
-        if (tl == tr) {
-            tree.push_back({val});
-            return tree.size()-1;
-        }
-        int tm = (tl + tr) >> 1;
-        if (pos <= tm) {
-            return recalc(f(f, currL, tl, tm, pos, val), currR);
-        } else {
-            return recalc(currL, f(f, currR, tm+1, tr, pos, val));
-        }
-    };
-    int n, L, R;
-    cin >> n >> L >> R;
-    vector<int> a(n);
-    for (auto &x : a) {
-        cin >> x;
+    vector<vector<bitset<255>>> a(255, vector<bitset<255>>(255));
+    int n;
+    cin >> n;
+    vector<pair<int,int>> pts(n);
+    for (auto &[x, y] : pts) {
+        cin >> x >> y;
     }
-    vector<int> trees(n+1);
-    trees[n] = -1;
-    for (int i = n-1; i >= 0; i -= 1) {
-        trees[i] = set(set, trees[i+1], 0, n, a[i], i);
-    }
-    clog << "SZ = " << tree.size() << "\n";
-    vector<vector<int>> pos(n+2);
     for (int i = 0; i < n; i += 1) {
-        pos[a[i]].push_back(i);
-    }
-    int zero = count(a.begin(), a.end(), 0);
-    for (int i = 0; i <= n; i += 1) {
-        pos[i].push_back(n);
-    }
-    for (int i = L; i <= R; i += 1) {
-        if (i == 0) {
-            cout << n-zero << " ";
-            continue;
-        }
-        int start = 0;
-        int tot = 0;
-        for (int j = 0; j < pos[i].size(); j += 1) {
-            while (true) {
-                int nxt = get_max(get_max, trees[start], 0, n, 0, i-1);
-                if (nxt < pos[i][j]) {
-                    tot += 1;
-                }
-                start = nxt + 1;
-                if (start >= pos[i][j]) {
-                    break;
+        for (int j = 0; j < n; j += 1) {
+            int dx_ray = pts[j].first - pts[i].first;
+            int dy_ray = pts[j].second - pts[i].second;
+            for (int k = 0; k < n; k += 1) {
+                int x = pts[k].first;
+                int y = pts[k].second;
+                int our_dx = x - pts[i].first;
+                int our_dy = y - pts[i].second;
+                int cross = dx_ray * our_dy - dy_ray * our_dx;
+                if (cross > 0) {
+                    a[i][j][k] = true;
                 }
             }
-            start = pos[i][j] + 1;
         }
-        cout << tot << " ";
-    }cout << "\n";
-    clog << "SZ = " << tree.size() << "\n";
+    }
+    int cnt = 0;
+    for (int i = 0; i < n; i += 1) {
+        for (int j = i+1; j < n; j += 1) {
+            for (int k = j+1; k < n; k += 1) {
+                if (!a[i][j][k]) {
+                    cnt += (a[i][k] & a[k][j] & a[j][i]).none();
+                } else {
+                    cnt += (a[i][j] & a[j][k] & a[k][i]).none();
+                }
+            }
+        }
+    }
+    cout << cnt << "\n";
 }
