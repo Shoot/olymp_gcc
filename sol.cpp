7e4d1f12d6f07ad48b97f7c975120292b8fcbbdf
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-05 12:44:45 +0300

    by myself, lyceum Petya's contest найти количество перестановок, MEXидентичных данной.

--- sol.cpp
+++ sol.cpp
@@ -1,42 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("O3", "unroll-loops")
-#pragma GCC target("avx2", "popcnt")
+#define int long long
+#define placed_before max_pos_on_pref[i-1]
+#define placed_after min_pos_on_pref[i-1]
+constexpr int MOD = 1e9+7;
 signed main() {
-    vector<vector<bitset<255>>> a(255, vector<bitset<255>>(255));
     int n;
     cin >> n;
-    vector<pair<int,int>> pts(n);
-    for (auto &[x, y] : pts) {
-        cin >> x >> y;
-    }
+    vector<int> pos(n);
     for (int i = 0; i < n; i += 1) {
-        for (int j = 0; j < n; j += 1) {
-            int dx_ray = pts[j].first - pts[i].first;
-            int dy_ray = pts[j].second - pts[i].second;
-            for (int k = 0; k < n; k += 1) {
-                int x = pts[k].first;
-                int y = pts[k].second;
-                int our_dx = x - pts[i].first;
-                int our_dy = y - pts[i].second;
-                int cross = dx_ray * our_dy - dy_ray * our_dx;
-                if (cross > 0) {
-                    a[i][j][k] = true;
-                }
+        int x;
+        cin >> x;
+        pos[x] = i;
+    }
+    vector<int> fenw(1e6+10);
+    auto set_place = [&] (int idx) -> void {
+        for (idx = idx + 1; idx < fenw.size(); idx += idx & -idx) {
+            fenw[idx] += 1;
+        }
+    };
+    auto get_free_places = [&] (int idx) -> int {
+        for (int idx_ = idx + 1, ones = 0; ; idx_ -= idx_ & -idx_) {
+            if (idx_ == 0) {
+                return idx+1-ones;
             }
+            ones += fenw[idx_];
+        }
+    };
+    vector<int> max_pos_on_pref(n, pos[0]), min_pos_on_pref(n, pos[0]);
+    for (int i = 1; i < n; i += 1) {
+        min_pos_on_pref[i] = min(pos[i], min_pos_on_pref[i-1]);
+        max_pos_on_pref[i] = max(pos[i], max_pos_on_pref[i-1]);
+    }
+    int ans = 1;
+    set_place(pos[0]);
+    for (int i = 1; i < n; i += 1) {
+        if (placed_before < pos[i] || placed_after > pos[i]) {
+            set_place(pos[i]);
+        } else {
+            (ans *= get_free_places(placed_before)-get_free_places(placed_after-1)) %= MOD;
+            set_place(pos[i]);
         }
     }
-    int cnt = 0;
-    for (int i = 0; i < n; i += 1) {
-        for (int j = i+1; j < n; j += 1) {
-            for (int k = j+1; k < n; k += 1) {
-                if (!a[i][j][k]) {
-                    cnt += (a[i][k] & a[k][j] & a[j][i]).none();
-                } else {
-                    cnt += (a[i][j] & a[j][k] & a[k][i]).none();
-                }
-            }
-        }
-    }
-    cout << cnt << "\n";
+    cout << ans << "\n";
 }
