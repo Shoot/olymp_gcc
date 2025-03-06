614b6f586a58038ccfff9167b84a7c2fe4f99d75
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-06 10:44:31 +0300

    revise lazy SegmentTree

--- sol.cpp
+++ sol.cpp
@@ -1,89 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
+const int MOD = 1e9+7;
 signed main() {
-    int n;
-    cin >> n;
-    vector<int> leafs;
-    vector<vector<int>> adj(n+1);
-    for (int i = 1; i <= n; i += 1) {
-        int sz;
-        cin >> sz;
-        adj[i].resize(sz);
-        for (auto &x : adj[i]) {
-            cin >> x;
-        }
-    }
-    vector<int> rightest_child(n+1, -1);
-    vector<int> leftest_child(n+1, n);
-    vector<int> d(n+1);
-    vector<vector<int>> binup(20, vector<int>(n+1));
-    binup[0][1] = 1;
-    auto dfs = [&] (auto f, int u) -> void {
-        if (adj[u].empty()) {
-            leafs.push_back(u);
-            rightest_child[u] = leafs.size()-1;
-            leftest_child[u] = leafs.size()-1;
+    int n, m;
+    cin >> n >> m;
+    vector<int> tot(m+1, 1);
+    struct Node {
+        int val = 0;
+        int lz = 0;
+    };
+    vector<Node> ST(4e6+10);
+    auto relax = [&] (int u, int tl, int tr) -> void {
+        ST[2*u+1].lz += ST[u].lz;
+        ST[2*u+2].lz += ST[u].lz;
+        int tm = (tl + tr) >> 1;
+        int szl = tm-tl+1;
+        int szr = tr-(tm+1)+1;
+        ST[2*u+1].val += ST[u].lz*szl;
+        ST[2*u+2].val += ST[u].lz*szr;
+        ST[u].lz = 0;
+    };
+    auto incr = [&] (auto f, int u, int tl, int tr, int l, int r, int val) -> void {
+        if (tl == l && tr == r) {
+            ST[u].val += val;
+            ST[u].lz += val;
             return;
         }
-        for (const auto &x : adj[u]) {
-            d[x] = d[u] + 1;
-            binup[0][x] = u;
-            f(f, x);
-            rightest_child[u] = max(rightest_child[u], rightest_child[x]);
-            leftest_child[u] = min(leftest_child[u], leftest_child[x]);
+        relax(u, tl, tr);
+        int tm = (tl + tr) >> 1;
+        if (l <= tm) {
+            f(f, 2*u+1, tl, tm, l, min(r, tm), val);
+        }
+        if (r >= tm+1) {
+            f(f, 2*u+2, tm+1, tr, max(l, tm+1), r, val);
+        }
+        ST[u].val += ST[2*u+1].val + ST[2*u+2].val;
+    };
+    auto get = [&] (auto f, int u, int tl, int tr, int pos) -> int {
+        if (tl == tr) {
+            return ST[u].val;
+        }
+        relax(u, tl, tr);
+        int tm = (tl + tr) >> 1;
+        if (pos <= tm) {
+            return f(f, 2*u+1, tl, tm, pos);
+        } else {
+            return f(f, 2*u+2, tm+1, tr, pos);
         }
     };
-    dfs(dfs, 1);
-    for (int i = 1; i < 20; i += 1) {
-        for (int u = 1; u <= n; u += 1) {
-            binup[i][u] = binup[i-1][binup[i-1][u]];
+    for (int i = 0; i < n; i += 1) {
+        int l, r;
+        cin >> l >> r;
+        incr(incr, 0, 1, m, l, r, 1);
+        for (int j = 1; j <= m; j += 1) {
+            (tot[j] *= r / j - (l-1) / j) %= MOD;
         }
     }
-    auto LCA = [&] (int u, int v) -> int {
-        if (d[u] < d[v]) {
-            swap(u, v);
+    cout << get(get, 0, 1, m, 3) << "!\n";
+    for (int i = m; i >= 1; i -= 1) {
+        for (int j = i + i; j <= m; j += i) {
+            ((tot[i] -= tot[j]) += MOD) %= MOD;
         }
-        int diff = d[u] - d[v];
-        for (int i = 0; i < 20; i += 1) {
-            if (diff&(1<<i)) {
-                u = binup[i][u];
-            }
-        }
-        assert(d[u] == d[v]);
-        if (u == v) {
-            return u;
-        }
-        for (int i = 19; i >= 0; i -= 1) {
-            if (binup[i][u] != binup[i][v]) {
-                u = binup[i][u];
-                v = binup[i][v];
-            }
-        }
-        assert(u != v);
-        assert(binup[0][u] == binup[0][v]);
-        return binup[0][u];
-    };
-    int q;
-    cin >> q;
-    set<tuple<int,int, int>> st;
-    for (int i = 0; i < q; i += 1) {
-        int tp, u;
-        cin >> tp >> u;
-        if (tp == 1) {
-            st.insert({rightest_child[u], d[u], u});
-            st.insert({leftest_child[u], d[u], u});
-        } else {
-            st.erase({rightest_child[u], d[u], u});
-            st.erase({leftest_child[u], d[u], u});
-        }
-        if (st.empty()) {
-            cout << 0 << "\n";
-        } else if (st.size() > 1) {
-            auto temp = LCA(leafs[get<0>(*st.begin())], leafs[get<0>(*st.rbegin())]);
-            cout << temp << "\n";
-        } else {
-            cout << get<1>(*st.begin()) << "\n";
-        }
+    }
+    for (int i = 1; i <= m; i += 1) {
+        cout << tot[i] << "\n";
     }
 }
