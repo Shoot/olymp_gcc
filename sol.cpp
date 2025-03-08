c4f2eb816b99db95b4838f83c6b7d5d2bfc61f72
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-09 01:38:50 +0300

    Аккуратные переходы чтобы не использовать два раза потомков. Перебор подмасок всех подмасок за 3^n https://codeforces.com/gym/105239/problem/C

--- sol.cpp
+++ sol.cpp
@@ -1,69 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int T;
-    cin >> T;
-    vector<int> component;
-    vector<pair<bool,bool>> comps;
-    component.reserve(1000);
-    comps.reserve(1000);
-    for (int tt = 0; tt < T; tt += 1) {
-        int m;
-        cin >> m;
-        vector<int> inDegree(1001), outDegree(1001);
-        vector<vector<int>> adj(1001);
-        for (int i = 0; i < m; ++i) {
-            int u, v;
-            cin >> u >> v;
-            outDegree[u]++;
-            inDegree[v]++;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        auto in_og = inDegree, out_og = outDegree;
-        auto dfs = [&] (auto f, int u) -> void {
-            if (!inDegree[u] && !outDegree[u]) {
-                return;
-            }
-            component.push_back(u);
-            inDegree[u] = 0;
-            outDegree[u] = 0;
-            for (const auto &v : adj[u]) {
-                f(f, v);
-            }
-        };
-        int tot = 0;
-        comps.clear();
-        for (int i = 1; i <= 1000; i += 1) {
-            if (!inDegree[i] && !outDegree[i]) {
-                continue;
-            }
-            component.clear();
-            dfs(dfs, i);
-            tot += 1;
-            bool need1 = false;
-            bool need2 = false;
-            for (const auto &x : component) {
-                need1 |= in_og[x] > out_og[x];
-                need2 |= in_og[x] < out_og[x];
-            }
-            comps.push_back({need1,need2});
-        }
-        int fix1 = 0;
-        int fix2 = 0;
-        for (int i = 0; i < comps.size(); i += 1) {
-            fix1 -= comps[i].first;
-            fix2 -= comps[i].second;
-        }
-        for (int i = 1; i <= 1000; i += 1) {
-            if (in_og[i] > out_og[i]) {
-                fix1 += in_og[i] - out_og[i];
-            } else if (in_og[i] < out_og[i]) {
-                fix2 += out_og[i] - in_og[i];
-            }
-        }
-        cout << m + 1 + tot - 1 + (fix1+fix2)/2 << "\n";
+#define int long long
+int dp[2][1 << 10][1001];
+signed main() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> w(n+1);
+    for (int i = 1; i <= n; i += 1) {
+        cin >> w[i];
     }
+    vector<int> c(n+1);
+    for (int i = 1; i <= n; i += 1) {
+        cin >> c[i];
+    }
+    vector<vector<int>> adj(n+1);
+    for (int i = 0; i < n-1; i += 1) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    bitset<10'000> seen;
+    auto dfs = [&] (auto f, int u) -> void {
+        seen[u] = true;
+        dp[1][1 << (c[u]-1)][u] = w[u];
+        for (const auto &x : adj[u]) {
+            if (!seen[x]) {
+                f(f, x);
+                // Подмаски 3^n - итого O(n*(3^n)) - норм (1000*59049=6e7)
+                // Подмаски 4^n - итого O(n*(4^n)) - не норм (1000*1048576=1e9)
+                // Всего подмасок всех подмасок = (1 + x)^n = \sum_{k=0}^n \binom{n}{k} x^k = 3^n
+                for (int from = (1 << 10) - 1; from >= 0; from -= 1) {
+                    int other = ((1 << 10) - 1) ^ from;
+                    for (int add = other; add; (add-=1)&=other /*Перебираем подмаски add маски other*/) {
+                        for (int add_on = 0; add_on <= 1; add_on += 1) {
+                            dp[0][from | add][u] = max(dp[0][from | add][u], dp[add_on][add][x] + dp[0][from][u]);
+                        }
+                    }
+                }
+                for (int from = (1 << 10) - 1; from >= 0; from -= 1) {
+                    int other = ((1 << 10) - 1) ^ from;
+                    for (int add = other; add; (add-=1)&=other /*Перебираем подмаски add маски other*/) {
+                        dp[1][from|add][u] = max(dp[1][from|add][u], dp[0][add][x]+dp[1][from][u]);
+                    }
+                }
+            }
+        }
+    };
+    dfs(dfs, 1);
+    int maxi = -1;
+    for (int on = 0; on <= 1; on += 1) {
+        for (int colors_subset = 0; colors_subset < (1 << 10); colors_subset += 1) {
+            maxi = max(maxi, dp[on][colors_subset][1]);
+        }
+    }
+    cout << maxi << "\n";
 }
