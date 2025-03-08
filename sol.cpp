8dda18eebc512a3b84d30bd5dd3436e7706847d7
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-08 22:32:52 +0300

    FIX https://www.spoj.com/problems/PRIMIT/ добавить ребра чтобы появился эйлеров путь — after counting missing deg(abs(in-out)), we fix two units by one edge. also connect components and remember you dont need to have ingoing in source and outgoing in sink (one src and snk for each component)

--- sol.cpp
+++ sol.cpp
@@ -1,121 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    string A;
-    string B;
-    cin >> A >> B;
-    reverse(A.begin(), A.end());
-    reverse(B.begin(), B.end());
-    while (A.size() < B.size()) {
-        A.push_back('0');
-    }
-    while (B.size() < A.size()) {
-        B.push_back('0');
-    }
-    reverse(A.begin(), A.end());
-    reverse(B.begin(), B.end());
-    auto sub = [&] (string a, string b) -> string {
-        bool carry = false;
-        string result(a.size(), 'x');
-        for (int i = a.size(); i >= 0; i -= 1) {
-            int ai = a[i] - '0';
-            int bi = b[i] - '0';
-            ai -= carry;
-            if (ai < bi) {
-                ai += 10;
-                carry = true;
-            } else {
-                carry = false;
-            }
-            result[i] = '0' + (ai - bi);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    vector<int> component;
+    vector<pair<bool,bool>> comps;
+    component.reserve(1000);
+    comps.reserve(1000);
+    for (int tt = 0; tt < T; tt += 1) {
+        int m;
+        cin >> m;
+        vector<int> inDegree(1001), outDegree(1001);
+        vector<vector<int>> adj(1001);
+        for (int i = 0; i < m; ++i) {
+            int u, v;
+            cin >> u >> v;
+            outDegree[u]++;
+            inDegree[v]++;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        return result;
-    };
-    auto calc = [&] (string x, string y) -> string {
-        string result = x;
-        for (int i = 0; i < x.size(); i += 1) {
-            int su = (x[i] - '0') + (y[i] - '0');
-            if (su >= 10) {
-                for (int j = i; j < x.size(); j += 1) {
-                    result[j] = '0';
-                }
-                if (i == 0) {
-                    result.insert(result.begin(), '1');
-                } else {
-                    result[i-1] += 1;
-                }
-                bool carry = false;
-                for (int j = result.size(); j >= 0; j -= 1) {
-                    result[j] += carry;
-                    if (result[j] >= '0'+10) {
-                        result[j] -= 10;
-                        carry = true;
-                    } else {
-                        carry = false;
-                    }
-                }
-                if (carry) {
-                    result.insert(result.begin(), '1');
-                }
-                int su10idx = -1;
-                while (y.size() < result.size()) {
-                    y.insert(y.begin(), '0');
-                }
-                for (int j = 0; j < result.size(); j += 1) {
-                    if ((result[j]-'0')+(y[j]-'0') == 10) {
-                        su10idx = j;
-                    }
-                }
-                if (su10idx != -1) {
-                    result[su10idx] = '0';
-                    int not9 = su10idx-1;
-                    for (; not9 >= 0; not9 -= 1) {
-                        if ((result[not9]-'0')+(y[not9]-'0') == 9) {
-                            result[not9] = '0';
-                        } else {
-                            break;
-                        }
-                    }
-                    if (not9 == -1) {
-                        result.insert(result.begin(), '1');
-                    } else {
-                        result[not9] += 1;
-                    }
-                }
-                carry = false;
-                for (int j = result.size(); j >= 0; j -= 1) {
-                    result[j] += carry;
-                    if (result[j] >= '0'+10) {
-                        result[j] -= 10;
-                        carry = true;
-                    } else {
-                        carry = false;
-                    }
-                }
-                if (carry) {
-                    result.insert(result.begin(), '1');
-                }
-                while (result.size() > x.size()) {
-                    x.insert(x.begin(), '0');
-                }
-                return sub(result, x);
+        auto in_og = inDegree, out_og = outDegree;
+        auto dfs = [&] (auto f, int u) -> void {
+            if (!inDegree[u] && !outDegree[u]) {
+                return;
+            }
+            component.push_back(u);
+            inDegree[u] = 0;
+            outDegree[u] = 0;
+            for (const auto &v : adj[u]) {
+                f(f, v);
+            }
+        };
+        int tot = 0;
+        comps.clear();
+        for (int i = 1; i <= 1000; i += 1) {
+            if (!inDegree[i] && !outDegree[i]) {
+                continue;
+            }
+            component.clear();
+            dfs(dfs, i);
+            tot += 1;
+            bool need1 = false;
+            bool need2 = false;
+            for (const auto &x : component) {
+                need1 |= in_og[x] > out_og[x];
+                need2 |= in_og[x] < out_og[x];
+            }
+            comps.push_back({need1,need2});
+        }
+        int fix1 = 0;
+        int fix2 = 0;
+        for (int i = 0; i < comps.size(); i += 1) {
+            fix1 -= comps[i].first;
+            fix2 -= comps[i].second;
+        }
+        for (int i = 1; i <= 1000; i += 1) {
+            if (in_og[i] > out_og[i]) {
+                fix1 += in_og[i] - out_og[i];
+            } else if (in_og[i] < out_og[i]) {
+                fix2 += out_og[i] - in_og[i];
             }
         }
-        return sub(result, x);
-    };
-    string add_to_A = calc(A, B);
-    string add_to_B = calc(B, A);
-    reverse(add_to_A.begin(), add_to_A.end());
-    reverse(add_to_B.begin(), add_to_B.end());
-    while (add_to_A.size() < add_to_B.size()) add_to_A.push_back('0');
-    while (add_to_A.size() > add_to_B.size()) add_to_B.push_back('0');
-    reverse(add_to_A.begin(), add_to_A.end());
-    reverse(add_to_B.begin(), add_to_B.end());
-    auto ans = min(add_to_A, add_to_B);
-    reverse(ans.begin(), ans.end());
-    while (ans.size() > 1 && ans.back() == '0') {
-        ans.pop_back();
+        cout << m + 1 + tot - 1 + (fix1+fix2)/2 << "\n";
     }
-    reverse(ans.begin(), ans.end());
-    cout << ans << "\n";
 }
