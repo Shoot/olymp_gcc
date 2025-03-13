54bd0fe7aa6c8f080fd42349b8d9d8ab8b852d88
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-13 18:52:42 +0300

    sum/max/min over subsets dp,  уникальный путь от сабсета то надсета — док-во

--- sol.cpp
+++ sol.cpp
@@ -1,57 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-//#pragma GCC optimize("O3", "unroll-loops")
-//#pragma GCC target("avx2", "popcnt")
+#define int long long
 signed main() {
-    int n;
-    cin >> n;
-    string s;
-    cin >> s;
-    int ans = 0;
-    // maximise b
-    // aaabbb
-    // 123210
-    vector<vector<int>> a(300);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(1<<k);
     for (int i = 0; i < n; i += 1) {
-        a[s[i]].push_back(i);
-    }
-    for (char one = 'a'; one <= 'z'; one += 1) {
-        for (char another = 'a'; another <= 'z'; another += 1) {
-            if (one == another) {
-                continue;
-            }
-            int DELM = 0;
-            int last = -1;
-            int DEL = 0;
-            int DELm1 = 0;
-            int DELMAX = 0;
-            int DELMAXm1 = 0;
-            int it1 = 0, it2 = 0;
-            for (int j = 0; j < a[one].size()+a[another].size(); j += 1) {
-                int i = -1;
-                if (it2 == a[another].size() || it1 != a[one].size() && a[one][it1] < a[another][it2]) {
-                    i = a[one][it1++];
-                } else {
-                    i = a[another][it2++];
-                }
-
-                DEL = (s[i] == another) - (s[i] == one);
-                DEL += DELm1;
-                DELm1 = DEL;
-                DELMAX = max(DEL, DELMAXm1);
-                if (s[i] == another) {
-                    if (i) {
-                        DELM = DELMAXm1;
-                    }
-                    last = i;
-                }
-                if (last == -1) {
-                    continue;
-                }
-                ans = max(ans, -DEL+DELM);
-                DELMAXm1 = DELMAX;
+        string s;
+        cin >> s;
+        int su = 0;
+        for (int j = 0; j < k; j += 1) {
+            if (s[j] == '+') {
+                su += 1 << j;
             }
         }
+        a[su] += 1;
     }
-    cout << ans << "\n";
+    auto b = a;
+    for (int bt = 0; bt < k; bt += 1) {
+        for (int i = 0; i < (1 << k); i += 1) {
+            if (!(i&(1<<bt))) continue;
+            a[i] += a[i^(1<<bt)];
+        }
+    }
+    int su = 0;
+    for (int i = 0; i < (1 << k); i += 1) {
+        if (b[i]) {
+            su += (a[i] - b[i])*b[i];
+        }
+    }
+    cout << su << "\n";
 }
