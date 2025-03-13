36076c49d893db04860d14e40788790205be91b1
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-13 20:23:40 +0300

    https://codeforces.com/contest/125/problem/E перебираем инкремент для инцидентных ребер, чтобы вошло ровно kgit add .

--- sol.cpp
+++ sol.cpp
@@ -2,32 +2,96 @@
 using namespace std;
 #define int long long
 signed main() {
-    int n, k;
-    cin >> n >> k;
-    vector<int> a(1<<k);
-    for (int i = 0; i < n; i += 1) {
-        string s;
-        cin >> s;
-        int su = 0;
-        for (int j = 0; j < k; j += 1) {
-            if (s[j] == '+') {
-                su += 1 << j;
+    int n, m, k;
+    cin >> n >> m >> k;
+    struct Edge {
+        int u, v, w, idx;
+        auto operator<=>(const Edge& other) const {
+            if (w != other.w) {
+                return w <=> other.w;
+            }
+            return min(u, v) <=> min(other.u, other.v);
+        }
+    };
+    vector<Edge> edges(m);
+    for (int i = 0; i < m; i += 1) {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+        edges[i].idx = i+1;
+    }
+    bitset<1'00'001> used;
+    auto voshlo = [&] (int x, bool final) -> int {
+        used.reset();
+        vector<int> p(n+1), sz(n+1, 1);
+        auto find = [&] (auto f, int u) -> int {
+            return p[u] = p[u] == u?u:f(f, p[u]);
+        };
+        auto merge = [&] (int u, int v) -> void {
+            u = find(find, u);
+            v = find(find, v);
+            if (sz[u] > sz[v]) {
+                swap(u, v);
+            }
+            p[u] = v;
+            sz[v] += sz[u];
+        };
+        iota(p.begin(), p.end(), 0);
+        for (auto &[u, v, w, idx] : edges) {
+            if (min(u, v) == 1) {
+                w += x;
             }
         }
-        a[su] += 1;
-    }
-    auto b = a;
-    for (int bt = 0; bt < k; bt += 1) {
-        for (int i = 0; i < (1 << k); i += 1) {
-            if (!(i&(1<<bt))) continue;
-            a[i] += a[i^(1<<bt)];
+        sort(edges.begin(), edges.end());
+        int ret = 0;
+        if (final) {
+            n = n-1;
+        }
+        for (const auto &[u, v, w, idx] : edges) {
+            if (find(find, u) != find(find, v)) {
+                if (final && !k && min(u, v) == 1) {
+                    continue;
+                }
+                if (final) {
+                    n -= 1;
+                }
+                used[idx] = true;
+                if (min(u, v) == 1) {
+                    if (final) {
+                        k -= 1;
+                    }
+                    ret += 1;
+                }
+                merge(u, v);
+            }
+        }
+        for (auto &[u, v, w, idx] : edges) {
+            if (min(u, v) == 1) {
+                w -= x;
+            }
+        }
+        return ret;
+    };
+    // перебираем инкремент для инцидентных ребер, чтобы вошло ровно k!!
+    int good = -1e9;
+    int l = -1e5-1, r = 1e5+1;
+    while (l <= r) {
+        int mid = (l + r) >> 1;
+        int got = voshlo(mid, false);
+        if (got < k) {
+            r = mid - 1;
+        } else {
+            good = mid;
+            l = mid + 1;
         }
     }
-    int su = 0;
-    for (int i = 0; i < (1 << k); i += 1) {
-        if (b[i]) {
-            su += (a[i] - b[i])*b[i];
-        }
+    voshlo(good, true);
+    if (k || n) {
+        cout << -1 << "\n";
+        return 0;
     }
-    cout << su << "\n";
+    cout << used.count() << "\n";
+    for (int i = 1; i <= m; i += 1) {
+        if (used[i]) {
+            cout << i << " ";
+        }
+    }cout << "\n";
 }
