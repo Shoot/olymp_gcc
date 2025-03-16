f9ba423d2688bb158512da91cc22bb999848156b
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-16 17:28:31 +0300

    HLD на ребрах: прибавляем на пути кроме LCA. Оффлайн это d[u]++, d[v]++, d[LCA]-=2. Значение ребра это сумма в поддереве низшей вершины, соответствующей ребру. А в онлайн значени это просто значение в низшей вершине https://codeforces.com/contest/191/problem/C.

--- sol.cpp
+++ sol.cpp
@@ -1,118 +1,144 @@
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast,unroll-loops")
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n;
     cin >> n;
-    vector<vector<pair<int,int>>> adj(n+1+4*n+10);
-    int FIRST = -1;
-    int LAST = -1;
-    auto build = [&] (auto f, int u, int tl, int tr) -> void {
-        if (tl == tr) {
-            if (tl == 1) {
-                FIRST = n+1+u;
+    vector<vector<int>> adj(n+1);
+    vector<pair<int,int>> edges(n-1);
+    for (int i = 0; i < n-1; i += 1) {
+        int u, v;
+        cin >> u >> v;
+        edges[i] = {u,v};
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    vector<int> sz(n+1);
+    vector<int> p(n+1, -1e9);
+    vector<int> tin(n+1, -1), tout(n+1, -1);
+    int t = 0;
+    auto dfs = [&] (auto f, int u) -> void {
+        sz[u] = 1;
+        for (auto &x : adj[u]) {
+            if (x != p[u]) {
+                p[x] = u;
+                f(f, x);
+                sz[u] += sz[x];
+                if (adj[u].front() == p[u] || sz[x] > sz[adj[u].front()]) {
+                    swap(x, adj[u].front());
+                }
             }
-            if (tl == n) {
-                LAST = n+1+u;
-            }
-            adj[n+1+u].push_back({1, tl});
-            return;
-        }
-        int tm = (tl + tr) >> 1;
-        f(f, 2*u+1, tl, tm);
-        f(f, 2*u+2, tm+1, tr);
-        adj[n+1+u].push_back({0, n+1+2*u+1});
-        adj[n+1+u].push_back({0, n+1+2*u+2});
-    };
-    build(build, 0, 1, n);
-    auto add_edges = [&] (auto f, int u, int tl, int tr, int l, int r, int i) -> void {
-        if (l == tl && r == tr) {
-            adj[i].push_back({0, n+1+u});
-            return;
-        }
-        int tm = (tl + tr) >> 1;
-        if (l <= tm) {
-            f(f, 2*u+1, tl, tm, l, min(r, tm), i);
-        }
-        if (r >= tm+1) {
-            f(f, 2*u+2, tm+1, tr, max(l, tm+1), r, i);
         }
     };
+    dfs(dfs, 1);
     for (int i = 1; i <= n; i += 1) {
-        int l, r;
-        cin >> l >> r;
-        add_edges(add_edges, 0, 1, n, l, r, i);
-    }
-    vector<vector<pair<int,int>>> iadj(n+1+4*n+10);
-    for (int i = 0; i < adj.size(); i += 1) {
-        for (const auto &[w, x] : adj[i]) {
-            iadj[x].push_back({w, i});
-        }
-    }
-    deque<int> q1;
-    vector<int> dist1(n+1+4*n+10, 1e8);
-    dist1[FIRST] = 0;
-    q1.push_back({FIRST});
-    while (!q1.empty()) {
-        auto u = q1.front();
-        q1.pop_front();
-        for (const auto &[w, x] : iadj[u]) {
-            if (dist1[x] > dist1[u] + w) {
-                dist1[x] = dist1[u] + w;
-                if (w) {
-                    q1.push_back(x);
-                } else {
-                    q1.push_front(x);
-                }
-            }
-        }
-    }
-    deque<int> qn;
-    vector<int> distn(n+1+4*n+10, 1e8);
-    distn[LAST] = 0;
-    qn.push_back({LAST});
-    while (!qn.empty()) {
-        auto u = qn.front();
-        qn.pop_front();
-        for (const auto &[w, x] : iadj[u]) {
-            if (distn[x] > distn[u] + w) {
-                distn[x] = distn[u] + w;
-                if (w) {
-                    qn.push_back(x);
-                } else {
-                    qn.push_front(x);
-                }
-            }
-        }
-    }
-    priority_queue<pair<int,int>> q;
-    vector<int> finaldist(n+1+4*n+10, 1e9);
-    for (int i = 1; i < n+1+4*n+10; i += 1) {
-        finaldist[i] = dist1[i] + distn[i] + 1;
-        q.push({-finaldist[i], i});
-    }
-    vector<bool> seen(n+1+4*n+10);
-    while (!q.empty()) {
-        auto [d, u] = q.top();
-        q.pop();
-        if (seen[u]) {
+        if (adj[i].size() == 1) {
             continue;
         }
-        seen[u] = true;
-        for (const auto &[w, x] : iadj[u]) {
-            if (finaldist[x] > finaldist[u] + w) {
-                finaldist[x] = finaldist[u] + w;
-                q.push({-finaldist[x], x});
+        assert(p[i] != adj[i][0]);
+    }
+    vector<int> top(n+1, -1);
+    auto hld = [&] (auto f, int u) -> void {
+        tin[u] = t++;
+        for (const auto &x : adj[u]) {
+            if (x != p[u]) {
+                if (x == adj[u][0]) {
+                    top[x] = top[u];
+                } else {
+                    top[x] = x;
+                }
+                f(f, x);
             }
         }
+        tout[u] = t;
+    };
+    top[1] = 1;
+    hld(hld, 1);
+    auto second_inside = [&] (int a, int b) -> bool {
+        return tin[a] <= tin[b] && tout[b] <= tout[a];
+    };
+    struct Node {
+        int su = 0;
+        int lz = 0;
+    };
+    vector<Node> ST(4*n+10);
+    auto push = [&] (int u, int tl, int tr) -> void {
+        int tm = (tl + tr) >> 1;
+        int szl = tm-tl+1;
+        int szr = tr-(tm+1)+1;
+        ST[2*u+1].lz += ST[u].lz;
+        ST[2*u+1].su += ST[u].lz*szl;
+        ST[2*u+2].lz += ST[u].lz;
+        ST[2*u+2].su += ST[u].lz*szr;
+        ST[u].lz = 0;
+    };
+    auto inc = [&] (auto f, int u, int tl, int tr, int l, int r, int val) -> void {
+        if (tl == l && tr == r) {
+            ST[u].lz += val;
+            ST[u].su += val*(tr-tl+1);
+            return;
+        }
+        push(u, tl, tr);
+        int tm = (tl + tr) >> 1;
+        if (l <= tm) {
+            f(f, 2*u+1, tl, tm, l, min(r, tm), val);
+        }
+        if (r >= tm+1) {
+            f(f, 2*u+2, tm+1, tr, max(l, tm+1), r, val);
+        }
+        ST[u].su = ST[2*u+1].su + ST[2*u+2].su;
+    };
+    auto gt = [&] (auto f, int u, int tl, int tr, int l, int r) -> int {
+        if (tl == l && tr == r) {
+            return ST[u].su;
+        }
+        push(u, tl, tr);
+        int tm = (tl + tr) >> 1;
+        int ret = 0;
+        if (l <= tm) {
+            ret += f(f, 2*u+1, tl, tm, l, min(r, tm));
+        }
+        if (r >= tm+1) {
+            ret += f(f, 2*u+2, tm+1, tr, max(l, tm+1), r);
+        }
+        return ret;
+    };
+    auto up = [&] (int& a, const int& b, int& ans) {
+        while (!second_inside(top[a], b)) {
+            ans += gt(gt, 0, 0, n-1, tin[top[a]], tin[a]);
+            a = p[top[a]];
+        }
+    };
+    auto upinc = [&] (int& a, const int& b, int x) {
+        while (!second_inside(top[a], b)) {
+            inc(inc, 0, 0, n-1, tin[top[a]], tin[a], x);
+            a = p[top[a]];
+        }
+    };
+    auto sum_on_path = [&] (int u, int v) -> int {
+        int ans = 0;
+        up(u, v, ans);
+        up(v, u, ans);
+        ans += gt(gt, 0, 0, n - 1, min(tin[u], tin[v]), max(tin[u], tin[v]));
+        return ans;
+    };
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i += 1) {
+        int u, v;
+        cin >> u >> v;
+        upinc(u, v, 1);
+        upinc(v, u, 1);
+        if (u != v) {
+            inc(inc, 0, 0, n-1, min(tin[u], tin[v])+!!!!!!!!!!!!!!!!!!!!!!!!!!!!1, max(tin[u], tin[v]), 1);
+        }
     }
-    int k;
-    cin >> k;
-    for (int i = 0; i < k; i += 1) {
-        int x;
-        cin >> x;
-        cout << (finaldist[x]>=1e8?-1:finaldist[x]) << "\n";
-    }
+    for (auto &[u, v] : edges) {
+        // u -> v !!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if (tin[u] > tin[v]) {
+            swap(u, v);
+        }
+        cout << sum_on_path(v, v) << " ";
+    }cout << "\n";
 }
