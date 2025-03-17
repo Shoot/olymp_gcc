cec6f0e028cd44bda2c338d54c2b18664bc1eb3e
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-17 15:57:31 +0300

    https://judge.yosupo.jp/problem/dynamic_graph_vertex_add_component_sum DCP, += on v_i, sum on comp https://github.com/Shoot/olymp_gcc/blob/7feb72fbba0efc2318f6ef4388edf87258f98fdc/sol.cpp — another DCP

--- sol.cpp
+++ sol.cpp
@@ -1,43 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 signed main() {
     int n, q;
     cin >> n >> q;
-    vector<pair<int,int>> val_and_idx(n);
-    vector<int> a(n);
-    for (int i = 0; i < n; i += 1) {
-        cin >> a[i];
-        val_and_idx[i] = {a[i], i};
+    vector<int> comp_su(n);
+    for (auto &x : comp_su) {
+        cin >> x;
     }
-    sort(val_and_idx.begin(), val_and_idx.end());
-    vector<vector<int>> tree(4*n+10);
-    auto build = [&] (auto f, int u, int tl, int tr) -> void {
-        if (tl == tr) {
-            tree[u] = {val_and_idx[tl].second};
+    vector<pair<int,int>> edges(q, {-1,-1});
+    map<pair<int,int>,int> idx;
+    vector<int> edge_end(q, -1);
+    struct Query {
+        int tp;
+        int edge_idx;
+        int u;
+        int inc;
+    };
+    vector<vector<Query>> tree(4*q+10);
+    auto add_edge = [&] (auto f, int u, int tl, int tr, int l, int r, Query qq) -> void {
+        if (tl == l && tr == r) {
+            tree[u].push_back(qq);
             return;
         }
         int tm = (tl + tr) >> 1;
-        f(f, 2*u+1, tl, tm);
-        f(f, 2*u+2, tm+1, tr);
-        tree[u].resize(tr-tl+1);
-        merge(tree[2*u+1].begin(), tree[2*u+1].end(), tree[2*u+2].begin(), tree[2*u+2].end(), tree[u].begin());
-    };
-    build(build, 0, 0, n-1);
-    auto go = [&] (auto f, int u, int tl, int tr, int l, int r, int k) -> int {
-        if (tl == tr) {
-            return tree[u].front();
+        if (l <= tm) {
+            f(f, 2*u+1, tl, tm, l, min(r, tm), qq);
         }
-        int tm = (tl + tr) >> 1;
-        int left = upper_bound(tree[2*u+1].begin(), tree[2*u+1].end(), r)
-                 - lower_bound(tree[2*u+1].begin(), tree[2*u+1].end(), l);
-        if (left > k) {
-            return f(f, 2*u+1, tl, tm, l, r, k);
+        if (r >= tm+1) {
+            f(f, 2*u+2, tm+1, tr, max(l, tm+1), r, qq);
         }
-        return f(f, 2*u+2, tm+1, tr, l, r, k-left);
     };
-    for (int qq = 0; qq < q; qq += 1) {
-        int l, r, k;
-        cin >> l >> r >> k;
-        cout << a[go(go, 0, 0, n-1, l, r-1, k)] << "\n";
+    for (int i = 0; i < q; i += 1) {
+        int tp;
+        cin >> tp;
+        if (tp == 0) {
+            int u, v;
+            cin >> u >> v;
+            edges[i] = {u, v};
+            idx[{u,v}] = i;
+            idx[{v,u}] = i;
+        }
+        if (tp == 1) {
+            int u, v;
+            cin >> u >> v;
+            edge_end[idx[{u,v}]] = i;
+        }
+        if (tp == 2) {
+            int u, x;
+            cin >> u >> x;
+            add_edge(add_edge, 0, 0, q-1, i, q-1, {2, -1, u, x});
+        }
+        if (tp == 3) {
+            int u;
+            cin >> u;
+            add_edge(add_edge, 0, 0, q-1, i, i, {3, -1, u, -1});
+        }
     }
+    for (int i = 0; i < q; i += 1) {
+        if (edges[i].first != -1) {
+            add_edge(add_edge, 0, 0, q-1, i, edge_end[i]!=-1?edge_end[i]:q-1, {0, i, -1, -1});
+        }
+    }
+    vector<int> p(n);
+    iota(p.begin(), p.end(), 0);
+    vector<int> sz(n, 1);
+    vector<pair<int&,int>> history;
+    auto find = [&] (auto f, int u) -> int {
+        return p[u] == u ? u : f(f, p[u]);
+    };
+    auto add = [&] (auto f, int u, int inc) -> void {
+        u = find(find, u);
+        history.push_back({comp_su[u], comp_su[u]});
+        comp_su[u] += inc;
+    };
+    auto unite = [&] (int u, int v) -> void {
+        u = find(find, u);
+        v = find(find, v);
+        if (u != v) {
+            if (sz[u] > sz[v]) {
+                swap(u, v);
+            }
+            history.push_back({p[u], p[u]});
+            p[u] = v;
+            history.push_back({sz[v], sz[v]});
+            sz[v] += sz[u];
+            history.push_back({comp_su[v], comp_su[v]});
+            comp_su[v] += comp_su[u];
+        }
+    };
+    auto dfs = [&] (auto f, int u, int tl, int tr) -> void {
+        int b4 = history.size();
+        for (const auto &[tp, edge_idx, U, inc] : tree[u]) {
+            if (tp == 0) {
+                unite(edges[edge_idx].first, edges[edge_idx].second);
+            }
+            if (tp == 2) {
+                add(add, U, inc);
+            }
+        }
+        for (const auto &[tp, edge_idx, U, inc] : tree[u]) {
+            if (tp == 3) {
+                cout << comp_su[find(find, U)] << "\n";
+            }
+        }
+        if (tl != tr) {
+            int tm = (tl + tr) >> 1;
+            f(f, 2*u+1, tl, tm);
+            f(f, 2*u+2, tm+1, tr);
+        }
+        while (history.size() > b4) {
+            history.back().first = history.back().second;
+            history.pop_back();
+        }
+    };
+    dfs(dfs, 0, 0, q-1);
 }
