56dda9b8423c2d54d20911b2f386c1e89580ffb0
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-17 16:03:53 +0300

    D. MiTM find equal hash string https://drive.google.com/file/d/1LYaNk1bV70p3j2VjKeuGPzY8bov8EjJp/view?usp=sharing

--- sol.cpp
+++ sol.cpp
@@ -1,72 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 signed main() {
-    int n, m;
-    cin >> n >> m;
-    vector<vector<char>> a(n, vector<char>(m));
-    vector<vector<int>> tp(n, vector<int>(m));
-    for (int i = 0; i < n; i += 1) {
-        for (int j = 0; j < m; j += 1) {
-            cin >> a[i][j];
-            if (a[i][j] == '/') {
-                tp[i][j] = 1;
-            } else {
-                tp[i][j] = -1;
+    mt19937 mt(time(0));
+    for (int i = 1; i <= 10; i += 1) {
+        ofstream cout("output"+to_string(i)+".txt");
+        ifstream cin("input"+to_string(i)+".txt");
+        string s;
+        cin >> s;
+        std::cout << s << ":D\n";
+        int base, m;
+        cin >> base >> m;
+        auto powm = [&] (int a, int b) -> int {
+            int res = 1;
+            while (b) {
+                if (b&1) {
+                    res = (__int128(res) * a) % m;
+                }
+                a = (__int128(a) * a) % m;
+                b >>= 1;
             }
-        }
-    }
-    struct Node {
-        int i;
-        int j;
-        int tp;
-        auto operator<(const Node& other) const {
-            return tie(i, j, tp) < tie(other.i, other.j, other.tp);
-        }
-    };
-    set<pair<int,Node>> st;
-    st.insert({tp[0][0] != -1, {0, 0, -1}});
-    map<Node,int> dist;
-    dist[{0, 0, -1}] = tp[0][0] != -1;
-    struct Transition {
-        int di;
-        int dj;
-        int nwtp;
-    };
-    map<int,vector<Transition>> go;
-    go[1] = {{1,-1,1},{0,-1,-1},{1,0,-1},
-             {-1,0,-1},{-1,1,1},{0,1,-1}};
-    go[-1] = {{-1,-1,-1},{1,1,-1},{-1,0,1},
-              {0,-1,1},{0,1,1},{1,0,1}};
-    set<Node> seen;
-    while (!st.empty()) {
-        auto [nvm, u] = *st.begin();
-        st.erase(st.begin());
-        if (seen.count(u)) {
-            continue;
-        }
-        seen.insert(u);
-        for (const auto &[di,dj,nwtp] : go[u.tp]) {
-            int ni = u.i + di;
-            int nj = u.j + dj;
-            if (ni < 0 || ni >= n || nj < 0 || nj >= m) {
-                continue;
+            return res;
+        };
+        int n = s.size();
+        auto nw = s;
+        auto h = [&] () -> int {
+            int res = 0;
+            for (const auto &x : nw) {
+                res = (__int128(res) * base + (x-'a'+1)) % m;
             }
-            int w = tp[ni][nj] != nwtp;
-            if (!dist.count({ni,nj,nwtp})||dist[{ni,nj,nwtp}]>dist[u]+w) {
-                dist[{ni,nj,nwtp}]=dist[u]+w;
-                st.insert({dist[{ni,nj,nwtp}], {ni,nj,nwtp}});
+            return res;
+        };
+        auto target = h();
+        int tries = 0;
+        vector<map<int,string>> mp(100);
+        auto dfs = [&] (auto f, int idx) -> bool {
+            if (idx == n) {
+//                if (h() == target) {
+//                    std::cout << nw << "!\n";
+//                    std::cout << "!!!\n";
+//                    return true;
+//                }
+                mp[n][h()]=nw;
+                return false;
             }
+            vector<char> go(26);
+            iota(go.begin(), go.end(), 'a');
+            shuffle(go.begin(), go.end(), mt);
+            for (const auto &x : go) {
+                if (tries > 1e8) {
+                    return false;
+                }
+                nw[idx] = x;
+                tries += 1;
+                if (f(f, idx+1)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+        std::cout << target << "xD\n";
+        for (int sz = 1; ; sz += 1) {
+            n = sz;
+            nw.resize(sz);
+            tries = 0;
+            if (dfs(dfs, 0)) {
+                cout << nw << "\n";
+                break;
+            }
+            bool done = false;
+            for (const auto &[h1, s1] : mp[sz]) {
+//                std::cout << "have " << h1 << "\n";
+                for (int appendsz = 1; appendsz <= sz; appendsz += 1) {
+                    int nwhash = (__int128(h1) * powm(base, appendsz))%m;
+                    int need = ((target - nwhash) + m) % m;
+//                    std::cout << "need " << need << "\n";
+                    if (mp[appendsz].count(need)) {
+                        cout << s1 << mp[appendsz][need] << "\n";
+                        std::cout << "HOOO\n";
+                        done = true;
+                        break;
+                    }
+                }
+                if (done) {
+                    break;
+                }
+            }
+            if (done) {
+                break;
+            }
+            std::cout << "bruh " << sz << "\n";
+            std::cout << "tr " << tries << "\n";
         }
+        dfs(dfs, 0);
     }
-    int mini = 1e9;
-    for (const auto &[u, d] : dist) {
-        if (u.i == n-1 && u.j == m-1 && u.tp == -1) {
-            mini = min(mini, d);
-        }
-    }
-    if (mini==1e9) {
-        cout << "NO SOLUTION\n";
-        return 0;
-    }
-    cout << mini << "\n";
 }
