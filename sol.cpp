4584063a9059d260443e6b4e893ec5ae0fd2e62d
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-17 13:32:58 +0300

    https://judge.yosupo.jp/problem/range_kth_smallest k-я порядковая статистика на отрезке: сортим массив пар (val, idx), в вершине merge sort tree храним список индексов элементов которые бы были там в отсортированном массиве. На этих индексах смотрим lower_bound и спускаемся от корня к листу. Также можно не делать lower_bound log раз, а предпосчитать его. То есть предпосчитать все nlog запросов (fractional cascading)

--- sol.cpp
+++ sol.cpp
@@ -1,39 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 signed main() {
-    int n, m;
-    cin >> n >> m;
+    int n, q;
+    cin >> n >> q;
+    vector<pair<int,int>> val_and_idx(n);
     vector<int> a(n);
-    for (auto &x : a) {
-        cin >> x;
+    for (int i = 0; i < n; i += 1) {
+        cin >> a[i];
+        val_and_idx[i] = {a[i], i};
     }
-    vector<int> bymask(1<<21, (1<<21)-1);
-    auto add = [&] (auto f, int u, int x) -> void {
-        if (bymask[u] == (bymask[u]&=x)) {
+    sort(val_and_idx.begin(), val_and_idx.end());
+    vector<vector<int>> tree(4*n+10);
+    auto build = [&] (auto f, int u, int tl, int tr) -> void {
+        if (tl == tr) {
+            tree[u] = {val_and_idx[tl].second};
             return;
         }
-        // &= можно релаксировать (с импактом) только k раз, ведь биты только зануляются (хотя бы на 1 каждый раз)
-        int from = u;
-        while (u) {
-            f(f, from^(u&-u), x);
-            u^=u&-u;
-        }
+        int tm = (tl + tr) >> 1;
+        f(f, 2*u+1, tl, tm);
+        f(f, 2*u+2, tm+1, tr);
+        tree[u].resize(tr-tl+1);
+        merge(tree[2*u+1].begin(), tree[2*u+1].end(), tree[2*u+2].begin(), tree[2*u+2].end(), tree[u].begin());
     };
-    for (const auto &x : a) {
-        add(add, x, x);
-    }
-    int i = 0;
-    while (bymask[i] == i) {
-        i += 1;
-    }
-    cout << i << " ";
-    for (int j = 0; j < m; j += 1) {
-        int x;
-        cin >> x;
-        add(add, x, x);
-        while (bymask[i] == i) {
-            i += 1;
+    build(build, 0, 0, n-1);
+    auto go = [&] (auto f, int u, int tl, int tr, int l, int r, int k) -> int {
+        if (tl == tr) {
+            return tree[u].front();
         }
-        cout << i << " ";
-    }cout << "\n";
+        int tm = (tl + tr) >> 1;
+        int left = upper_bound(tree[2*u+1].begin(), tree[2*u+1].end(), r)
+                 - lower_bound(tree[2*u+1].begin(), tree[2*u+1].end(), l);
+        if (left > k) {
+            return f(f, 2*u+1, tl, tm, l, r, k);
+        }
+        return f(f, 2*u+2, tm+1, tr, l, r, k-left);
+    };
+    for (int qq = 0; qq < q; qq += 1) {
+        int l, r, k;
+        cin >> l >> r >> k;
+        cout << a[go(go, 0, 0, n-1, l, r-1, k)] << "\n";
+    }
 }
