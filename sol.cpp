e9652209fb9aced66bb32571622b961df62909a3
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-17 16:02:25 +0300

    B. вертеть провода чтобы появился путь: https://drive.google.com/file/d/1LYaNk1bV70p3j2VjKeuGPzY8bov8EjJp/view?usp=sharing

--- sol.cpp
+++ sol.cpp
@@ -1,60 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 signed main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n;
-    cin >> n;
-    vector<int> a(n);
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> a(n, vector<char>(m));
+    vector<vector<int>> tp(n, vector<int>(m));
     for (int i = 0; i < n; i += 1) {
-        cin >> a[i];
-    }
-    vector<int> b;
-    for (int i = 0; i < 3; i += 1)
-        for (const auto &x : a) {
-            b.push_back(x);
-        }
-    vector<int> pref(b.size());
-    for (int i = 0; i < b.size(); i += 1) {
-        pref[i] = b[i];
-        if (i) {
-            pref[i] += pref[i-1];
+        for (int j = 0; j < m; j += 1) {
+            cin >> a[i][j];
+            if (a[i][j] == '/') {
+                tp[i][j] = 1;
+            } else {
+                tp[i][j] = -1;
+            }
         }
     }
-    int we_do = n/2+n%2;
-    int maxi = 0;
-    vector<int> tree(4*(3*n)+10, 1e9);
-    auto mineq = [&] (auto f, int u, int tl, int tr, int l, int r, int val) -> void {
-        if (tl == l && tr == r) {
-            tree[u] = min(tree[u], val);
-            return;
-        }
-        int tm = (tl + tr) >> 1;
-        if (l <= tm) {
-            f(f, 2*u+1, tl, tm, l, min(r, tm), val);
-        }
-        if (r >= tm+1) {
-            f(f, 2*u+2, tm+1, tr, max(l,tm+1), r, val);
+    struct Node {
+        int i;
+        int j;
+        int tp;
+        auto operator<(const Node& other) const {
+            return tie(i, j, tp) < tie(other.i, other.j, other.tp);
         }
     };
-    auto gt = [&] (auto f, int u, int tl, int tr, int pos) -> int {
-        if (tl == tr) {
-            return tree[u];
-        }
-        int tm = (tl + tr) >> 1;
-        if (pos <= tm) {
-            return min(tree[u], f(f, 2*u+1, tl, tm, pos));
-        } else {
-            return min(tree[u], f(f, 2*u+2, tm+1, tr, pos));
-        }
+    set<pair<int,Node>> st;
+    st.insert({tp[0][0] != -1, {0, 0, -1}});
+    map<Node,int> dist;
+    dist[{0, 0, -1}] = tp[0][0] != -1;
+    struct Transition {
+        int di;
+        int dj;
+        int nwtp;
     };
-    for (int start = 0; start+we_do-1 < 3*n; start += 1) {
-        int val = pref[start+we_do-1]-(start?pref[start-1]:0);
-        mineq(mineq, 0, 0, 3*n-1, start, start+we_do-1, val);
+    map<int,vector<Transition>> go;
+    go[1] = {{1,-1,1},{0,-1,-1},{1,0,-1},
+             {-1,0,-1},{-1,1,1},{0,1,-1}};
+    go[-1] = {{-1,-1,-1},{1,1,-1},{-1,0,1},
+              {0,-1,1},{0,1,1},{1,0,1}};
+    set<Node> seen;
+    while (!st.empty()) {
+        auto [nvm, u] = *st.begin();
+        st.erase(st.begin());
+        if (seen.count(u)) {
+            continue;
+        }
+        seen.insert(u);
+        for (const auto &[di,dj,nwtp] : go[u.tp]) {
+            int ni = u.i + di;
+            int nj = u.j + dj;
+            if (ni < 0 || ni >= n || nj < 0 || nj >= m) {
+                continue;
+            }
+            int w = tp[ni][nj] != nwtp;
+            if (!dist.count({ni,nj,nwtp})||dist[{ni,nj,nwtp}]>dist[u]+w) {
+                dist[{ni,nj,nwtp}]=dist[u]+w;
+                st.insert({dist[{ni,nj,nwtp}], {ni,nj,nwtp}});
+            }
+        }
     }
-    for (int i = n; i-n < n; i += 1) {
-        int mini = 1e9;
-        maxi = max(maxi, gt(gt, 0, 0, 3*n-1, i));
+    int mini = 1e9;
+    for (const auto &[u, d] : dist) {
+        if (u.i == n-1 && u.j == m-1 && u.tp == -1) {
+            mini = min(mini, d);
+        }
     }
-    cout << maxi << "\n";
+    if (mini==1e9) {
+        cout << "NO SOLUTION\n";
+        return 0;
+    }
+    cout << mini << "\n";
 }
