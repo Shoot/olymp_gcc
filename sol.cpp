a836693f4da9b06300f7e65621b6755be4ee9f7d
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-18 19:57:12 +0300

    for (int i = l; i <= r; i += 1) ans += not_coprime_to_L[i] < l && not_coprime_to_R[i] > r; INSTEAD OF (4D->TWO 3DS) use funny inc(L[i]+1, 1),inc(i+1, -1) ЗАДАЧА: количество чисел на отрезке, что они копрайм со всеми остальными на отрезке — запросы оффлайн

--- sol.cpp
+++ sol.cpp
@@ -1,54 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
-#pragma GCC optimize("Ofast,unroll-loops")
-mt19937 mt(time(0));
 signed main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n = 1e9, m = 1e9;
-    cin >> n >> m;
-    int k = 7000;
-    cin >> k;
-    vector<pair<int,int>> pts(k);
-    for (auto &[x, y] : pts) {
-        cin >> x >> y;
-//        x = mt();
-//        y = mt();
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
     }
-    int maxi = 1;
-    sort(pts.begin(), pts.end());
-    vector<bitset<7000>> ok(7000);
-    for (auto &x : ok) {
-        x.set();
-    }
-//    int OPS = 0;
-    for (int i = 0; i < k; i += 1) {
-        for (int j = ok[i]._Find_next(i); j < k; j = ok[i]._Find_next(j)) {
-            ok[i][j] = false;
-            int dx = pts[j].first - pts[i].first;
-            int dy = pts[j].second - pts[i].second;
-            int cnt = 2;
-            int last = j;
-            for (int f = ok[j]._Find_next(j); f < k; f = ok[last]._Find_next(f)) {
-                if ((pts[f].first - pts[last].first) < dx || (pts[f].first - pts[last].first) == dx && (pts[f].second - pts[last].second) < dy) {
-                    f = lower_bound(pts.begin()+f+1, pts.end(), pair<int,int>{pts[last].first+dx, pts[last].second+dy}) - pts.begin();
-                    if (!ok[last][f]) {
-                        f = ok[last]._Find_next(f);
-                    }
-                }
-                if (f >= k || (pts[f].first - pts[last].first) > dx || (pts[f].first - pts[last].first) == dx && (pts[f].second - pts[last].second) > dy) {
-                    break;
-                }
-//                OPS += 1;
-                ok[last][f] = false;
-                if ((pts[f].first - pts[last].first) == dx && (pts[f].second - pts[last].second) == dy) {
-                    cnt += 1;
-                    last = f;
+    vector<int> lp(2e5+1);
+    for (long long i = 2; i < lp.size(); i += 1) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            for (long long j = i*i; j < lp.size(); j += i) {
+                if (!lp[j]) {
+                    lp[j] = i;
                 }
             }
-            maxi = max(maxi, cnt);
         }
     }
-    cout << maxi << "\n";
-//    clog << OPS << "\n";
+    vector<vector<int>> shit(2e5+1);
+    for (int i = 1; i <= 2e5; i += 1) {
+        int last_lp = 0;
+        int temp = i;
+        while (temp != 1) {
+            if (lp[temp] != last_lp) {
+                last_lp = lp[temp];
+                shit[i].push_back(last_lp);
+            }
+            temp /= lp[temp];
+        }
+    }
+    vector<int> last_mul_of(2e5+1, -1);
+    vector<int> not_coprime_to_L(n, -1);
+    for (int i = 0; i < n; i += 1) {
+        for (const auto &x : shit[a[i]]) {
+            not_coprime_to_L[i] = max(not_coprime_to_L[i], last_mul_of[x]);
+            last_mul_of[x] = i;
+        }
+    }
+    last_mul_of.assign(2e5+1, n);
+    vector<int> not_coprime_to_R(n, n);
+    for (int i = n-1; i >= 0; i -= 1) {
+        for (const auto &x : shit[a[i]]) {
+            not_coprime_to_R[i] = min(not_coprime_to_R[i], last_mul_of[x]);
+            last_mul_of[x] = i;
+        }
+    }
+    map<int,vector<pair<int,int>>> by_r;
+    for (int i = 0; i < n; i += 1) {
+        by_r[not_coprime_to_R[i]].push_back({not_coprime_to_L[i], i});
+    }
+    struct Query {
+        int l;
+        int r;
+        int idx;
+    };
+    vector<vector<Query>> Q_by_r(n+1);
+    vector<Query> Q;
+    for (int ii = 0; ii < q; ii += 1) {
+        int l, r;
+        cin >> l >> r;
+        l -= 1;
+        r -= 1;
+        Q_by_r[r].push_back({l,r,ii});
+//        int ans = 0;
+//        for (int i = l; i <= r; i += 1) {
+//            ans += not_coprime_to_L[i] < l && not_coprime_to_R[i] > r;
+//        }
+//        cout << ans << "\n";
+    }
+    vector<int> ans(q);
+    vector<int> fenw(n+10);
+    auto inc = [&] (int idx, int val) -> void {
+        for (; idx < fenw.size(); idx += idx & -idx) {
+            fenw[idx] += val;
+        }
+    };
+    auto gt = [&] (int idx) -> int {
+        int ret = 0;
+        for (; idx; idx -= idx & -idx) {
+            ret += fenw[idx];
+        }
+        return ret;
+    };
+    for (int ii = 0; ii <= n; ii += 1) {
+        if (ii < n) {
+            inc((not_coprime_to_L[ii]+1)+2, 1);
+            inc((ii+1)+2, -1);
+        }
+        for (const auto &[Li, i] : by_r[ii]) {
+            inc((Li+1)+2, -1);
+            inc((i+1)+2, 1);
+        }
+        for (const auto &[l, r, idx] : Q_by_r[ii]) {
+            ans[idx] = gt(l+2);
+        }
+    }
+    for (const auto &x : ans) {
+        cout << x << "\n";
+    }
 }
