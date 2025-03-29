09e6461d626901e1d8f4eeb26a31994f7bcf77a0
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-29 16:15:40 +0300

    Peterson tree?

--- good.cpp
+++ good.cpp
@@ -1,35 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#ifdef LO
-#pragma GCC optimize("trapv")
-#endif
 signed main() {
-#ifndef LO
-    clog << "FIO\n";
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-#endif
-    int T;
-    cin >> T;
-    for (int tt = 0; tt < T; tt += 1) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (auto &x : a) {
-            cin >> x;
-        }
-        vector<int> b(n);
-        for (auto &y : b) {
-            cin >> y;
-        }
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-        for (const auto &x : a) {
-            cout << x << " ";
-        }cout << "\n";
-        for (const auto &x : b) {
-            cout << x << " ";
-        }cout << "\n";
+    // Запросы
+    //0  is_sorted(a.begin()+l, a.begin()+r+1)
+    //1  sort(a.begin()+l, a.begin()+r+1)
+    //2  return a[i]
+    //3  a[i] = x
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
     }
+    struct Q {
+        int tp;
+        int l;
+        int r;
+        int i;
+        int x;
+    };
+    vector<Q> qs;
+    for (int ii = 0; ii < q; ii += 1) {
+        int l, r;
+        cin >> l >> r;
+        l -= 1;
+        r -= 1;
+        qs.push_back({0, l, r});
+        qs.push_back({1, l, r});
+    }
+    auto gosimple = [&] () -> void {
+        auto b = a;
+        for (const auto &[tp, l, r, nvm1, nvm2] : qs) {
+            if (tp == 0) {
+                cout << (is_sorted(b.begin()+l, b.begin()+r+1)?"YES":"NO") << "\n";
+            }
+            if (tp == 1) {
+                sort(b.begin()+l, b.begin()+r+1);
+            }
+        }
+    };
+    auto gofast = [&] () -> void {
+        auto b = a;
+        vector<vector<int>> vv;
+        set<int> starts;
+        vv.push_back({b[0]});
+        starts.insert(0);
+        for (int i = 1; i < n; i += 1) {
+            if (b[i-1] <= b[i]) {
+                vv.back().push_back(b[i]);
+            } else {
+                starts.insert(i);
+                vv.push_back({b[i]});
+            }
+        }
+        for (const auto &[tp, l, r, nvm1, nvm2] : qs) {
+            if (tp == 0) {
+                if (*(--starts.upper_bound(r)) <= l) {
+                    cout << "YES\n";
+                } else {
+                    cout << "NO\n";
+                }
+            }
+            if (tp == 1) {
+
+            }
+        }
+    };
+    gosimple();
+    cout << "----\n";
+    gofast();
 }
--- sol.cpp
+++ sol.cpp
@@ -1,148 +1,135 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 signed main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, q;
-    cin >> n >> q;
-    vector<int> a(n+1);
-    for (int i = 1; i <= n; i += 1) {
-        cin >> a[i];
-    }
-    vector<vector<int>> adj(n+1);
-    for (int i = 0; i < n-1; i += 1) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    vector<int> sz(n+1);
-    vector<int> p(n+1, -1e9);
-    vector<int> tin(n+1, -1), tout(n+1, -1);
-    int t = 0;
-    auto dfs = [&] (auto f, int u) -> void {
-        sz[u] = 1;
-        for (auto &x : adj[u]) {
-            if (x != p[u]) {
-                p[x] = u;
-                f(f, x);
-                sz[u] += sz[x];
-                if (adj[u].front() == p[u] || sz[x] > sz[adj[u].front()]) {
-                    swap(x, adj[u].front());
-                }
-            }
-        }
-    };
-    dfs(dfs, 1);
-    for (int i = 1; i <= n; i += 1) {
-        if (adj[i].size() == 1) {
-            continue;
-        }
-        assert(p[i] != adj[i][0]);
-    }
-    vector<int> top(n+1, -1);
-    auto hld = [&] (auto f, int u) -> void {
-        tin[u] = t++;
-        for (const auto &x : adj[u]) {
-            if (x != p[u]) {
-                if (x == adj[u][0]) {
-                    top[x] = top[u];
-                } else {
-                    top[x] = x;
-                }
-                f(f, x);
-            }
-        }
-        tout[u] = t;
-    };
-    top[1] = 1;
-    hld(hld, 1);
-    auto second_inside = [&] (int a, int b) -> bool {
-        return tin[a] <= tin[b] && tout[b] <= tout[a];
-    };
+    atomic<int> right_total = 0;
+    int total = 0;
+    vector<thread> threads;
     struct Node {
-        int suff = 0;
-        int pref = 0;
-        int overall = 0;
-        int whole = 0;
+        int p;
+        bool iam;
+        array<atomic<bool>, 2> want;
+        int l = -1e9;
+        int r = -1e9;
+        atomic<bool> victim;
+        Node() {};
     };
-    auto gomerge = [&] (const Node& a, const Node& b) -> Node {
-        Node ret = Node();
-        ret.whole = a.whole + b.whole;
-        ret.pref = max(a.pref, a.whole+b.pref);
-        ret.suff = max(b.suff, a.suff+b.whole);
-        ret.overall = max({a.overall, b.overall, a.suff+b.pref});
-        return ret;
-    };
-    vector<Node> ST(4*n+10);
-    auto st = [&] (auto f, int u, int tl, int tr, int pos, int val) -> void {
-        if (tl == tr) {
-            ST[u].pref = max(0ll,val);
-            ST[u].suff = max(0ll,val);
-            ST[u].overall = max(0ll,val);
-            ST[u].whole = val;
-            return;
-        }
-        int tm = (tl + tr) >> 1;
-        if (pos <= tm) {
-            f(f, 2*u+1, tl, tm, pos, val);
-        } else {
-            f(f, 2*u+2, tm+1, tr, pos, val);
-        }
-        ST[u] = gomerge(ST[2*u+1], ST[2*u+2]);
-    };
-    auto gt = [&] (auto f, int u, int tl, int tr, int l, int r) -> Node {
-        if (tl == l && tr == r) {
-            return ST[u];
-        }
-        int tm = (tl + tr) >> 1;
-        Node ret = Node();
-        if (l <= tm) {
-            ret = gomerge(ret, f(f, 2*u+1, tl, tm, l, min(r, tm)));
-        }
-        if (r >= tm+1) {
-            ret = gomerge(ret, f(f, 2*u+2, tm+1, tr, max(l, tm+1), r));
-        }
-        return ret;
-    };
-    auto up = [&] (int& a, const int& b, Node& ans) -> Node {
-        vector<pair<int,int>> temp;
-        while (!second_inside(top[a], b)) {
-            temp.push_back({tin[top[a]], tin[a]});
-            a = p[top[a]];
-        }
-        for (const auto &[x, y] : temp) {
-            ans = gomerge(gt(gt, 0, 0, n-1, x, y), ans);
-        }
-        return ans;
-    };
-    for (int i = 1; i <= n; i += 1) {
-        st(st, 0, 0, n-1, tin[i], a[i]);
-    }
-    for (int i = 0; i < q; i += 1) {
-        int tp;
-        cin >> tp;
-        if (tp == 1) {
-            int u, x;
-            cin >> u >> x;
-            st(st, 0, 0, n-1, tin[u], x);
-        } else {
-            int u, v;
-            cin >> u >> v;
-            Node from_u = Node();
-            Node from_v = Node();
-            up(u, v, from_u);
-            up(v, u, from_v);
-            if (tin[u] < tin[v]) {
-                swap(from_u, from_v);
-                swap(u, v);
+    struct P_Mutex {
+        int N;
+        array<Node, 1000> tree;
+        vector<pair<int, bool>> get_v;
+        mutex perfect;
+        atomic<int> curr = 0;
+        void build(int l, int r, int p, bool iam) {
+            if (l == r) {
+                get_v[l] = {p, iam};
+                return;
             }
-            // u @ bottom
-            swap(from_v.pref, from_v.suff); // reverse one half (i choose to reverse the one that goes to the top)
-            auto shit = gt(gt, 0, 0, n-1, min(tin[u], tin[v]), max(tin[u], tin[v]));
-            from_u = gomerge(shit, from_u);
-            cout << gomerge(from_v,from_u).overall << "\n";
+            tree[curr].iam = iam;
+            tree[curr].p = p;
+            int m = (l + r) >> 1;
+            int v = curr;
+            curr += 1;
+            tree[v].l = curr;
+            build(l, m, v, 0);
+            tree[v].r = curr;
+            build(m+1, r, v, 1);
         }
+        P_Mutex(int cnt) {
+            N = cnt;
+            get_v.resize(N);
+            build(0, N-1, -1, 0);
+        }
+        void lock(int index) {
+//            perfect.lock();
+            int curr_v = get_v[index].first;
+            int side = get_v[index].second;
+            while (curr_v != -1) {
+                tree[curr_v].want[side].store(true);
+                tree[curr_v].victim.store(side);
+                while (tree[curr_v].want[1-side].load() && tree[curr_v].victim.load() == side) {
+
+                }
+                side = tree[curr_v].iam;
+                curr_v = tree[curr_v].p;
+            }
+        }
+        void unlock(int index) {
+//            perfect.unlock();
+            int curr_v = 0;
+            int l = 0, r = N-1;
+            while (l != r) {
+//                cout << "hi " << l << " " << r << "\n";
+                int m = (l+r) >> 1;
+                if (index <= m) {
+                    r = m;
+                    tree[curr_v].want[0].store(false);
+                    curr_v = tree[curr_v].l;
+                } else {
+                    l = m+1;
+                    tree[curr_v].want[1].store(false);
+                    curr_v = tree[curr_v].r;
+                }
+            }
+        }
+    };
+    int TOT = 2;
+    volatile int each_liveness[TOT];
+    for (int i = 0; i < TOT; i += 1) {
+        each_liveness[i] = 0;
+    }
+    P_Mutex M(TOT+1);
+    auto f = [&] (int index) -> void {
+        cout << index << "!\n";
+        while (true) {
+            M.lock(index);
+            each_liveness[index] += 1;
+            right_total += 1;
+            total += 1;
+            M.unlock(index);
+        }
+    };
+    for (int i = 0; i < TOT; i += 1) {
+        threads.push_back(thread(f, i));
+        threads.back().detach();
+    }
+    while (true) {
+        this_thread::sleep_for(100ms);
+        M.lock(TOT);
+        cout << "1. " << right_total << " " << total << "   ";
+        int su = 0;
+        for (int i = 0; i < TOT; i += 1) {
+            su += each_liveness[i];
+        }
+        cout << su << "( ";
+        for (int i = 0; i < TOT; i += 1) {
+            cout << each_liveness[i] << " ";
+        }cout << ")\n";
+
+
+
+        this_thread::sleep_for(100ms);
+        cout << "2. " << right_total << " " << total << "   ";
+        su = 0;
+        for (int i = 0; i < TOT; i += 1) {
+            su += each_liveness[i];
+        }
+        cout << su << "( ";
+        for (int i = 0; i < TOT; i += 1) {
+            cout << each_liveness[i] << " ";
+        }cout << ")\n";
+
+
+        this_thread::sleep_for(100ms);
+        cout << "3. " << right_total << " " << total << "   ";
+        su = 0;
+        for (int i = 0; i < TOT; i += 1) {
+            su += each_liveness[i];
+        }
+        cout << su << "(";
+        for (int i = 0; i < TOT; i += 1) {
+            cout << each_liveness[i] << " ";
+        }cout << ")\n";
+
+        M.unlock(TOT);
     }
 }
