d0b46510bd7271417dbfcbb1026036697d9b4bc0
Shoot <11522779+Shoot@users.noreply.github.com>
2025-03-30 21:32:21 +0300

    DisjointSparseTable O(1)https://codeforces.com/group/1rv4rhCsHp/contest/102954/problem/A

--- sol.cpp
+++ sol.cpp
@@ -1,194 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
+constexpr int MOD = 1e9+7;
 signed main() {
-    int n, q;
-    cin >> n >> q;
-    struct Node {
-        int l = -1;
-        int r = -1;
-        int su = 0;
-        int mn = 1e9;
-        int mx = -1e9;
-    };
-    vector<Node> tree;
-    auto su = [&] (int u) -> int {
-        return (u == -1 ? 0 : tree[u].su);
-    };
-    auto mn = [&] (int u) -> int {
-        return (u == -1 ? 1e9 : tree[u].mn);
-    };
-    auto mx = [&] (int u) -> int {
-        return (u == -1 ? -1e9 : tree[u].mx);
-    };
-    auto recalc = [&] (int u) -> void {
-        if (u == -1) {
-            return;
-        }
-        tree[u] = {tree[u].l, tree[u].r, su(tree[u].l)+su(tree[u].r),
-                                         min(mn(tree[u].l), mn(tree[u].r)),
-                                         max(mx(tree[u].l), mx(tree[u].r))};
-    };
-    auto nw = [&] () -> int {
-        tree.push_back({});
-        return tree.size()-1;
-    };
-    auto inc = [&] (auto f, int u, int tl, int tr, int pos) -> void {
-        if (tl == tr) {
-            tree[u].su += 1;
-            tree[u].mn = tl;
-            tree[u].mx = tl;
-            return;
-        }
-        int tm = (tl + tr) >> 1;
-        if (pos <= tm) {
-            if (tree[u].l == -1) {
-                tree[u].l = nw();
-            }
-            f(f, tree[u].l, tl, tm, pos);
-        } else {
-            if (tree[u].r == -1) {
-                tree[u].r = nw();
-            }
-            f(f, tree[u].r, tm+1, tr, pos);
-        }
-        recalc(u);
-    };
-    auto split = [&] (auto f, int u, int sz) -> int {
-        int ret = nw();
-        if (!sz) {
-            return ret;
-        }
-        if (su(u) == sz) {
-            swap(tree[u], tree[ret]);
-            return ret;
-        }
-        if (tree[u].l == -1 && tree[u].r == -1) {
-            tree[ret] = tree[u];
-            tree[u].su -= sz;
-            tree[ret].su = sz;
-            return ret;
-        }
-        if (su(tree[u].l) >= sz) {
-            tree[ret].l = f(f, tree[u].l, sz);
-            recalc(u);
-            recalc(ret);
-            return ret;
-        } else {
-            sz -= su(tree[u].l);
-            swap(tree[ret].l, tree[u].l);
-            tree[ret].r = f(f, tree[u].r, sz);
-            recalc(u);
-            recalc(ret);
-            return ret;
-        }
-    };
-    auto merge = [&] (auto f, int& u, int& v) -> void {
-        if (su(u) == 0) {
-            swap(u, v);
-            return;
-        }
-        if (su(v) == 0) {
-            return;
-        }
-        if (tree[u].l == -1 && tree[u].r == -1) {
-            // leaf
-            assert(tree[v].l == -1 && tree[v].r == -1);
-            tree[u].su += tree[v].su;
-            v = -1;
-            return;
-        }
-        // v is never used after
-        f(f, tree[u].l, tree[v].l);
-        f(f, tree[u].r, tree[v].r);
-        v = -1;
-        recalc(u);
-    };
-    int prev = 1e9;
-    vector<int> roots(1'000'000);
-    vector<int> LL(1'000'000); // starting_point_by_order
-    auto comp = [&] (auto const& a, auto const& b) {
-        return LL[a] < LL[b];
-    };
-    set<int, decltype(comp)> st(comp);
-    int C = 0;
-    for (int i = 0; i < n; i += 1) {
-        int x;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) {
         cin >> x;
-        if (x < prev) {
-            LL[C] = i;
-            st.insert(C);
-            roots[C] = nw();
-            C += 1;
-        }
-        prev = x;
-        inc(inc, roots[C-1], 1, 1e9, x);
     }
+    int n_ = n;
+    while (n&(n-1)) {
+        a.push_back(0);
+        n += 1;
+    }
+    vector<vector<pair<int,int>>> val_and_cnt(20, vector<pair<int,int>>(n));
+    auto go = [&] (auto f, int l, int r, int layer) -> void {
+        if (layer < 0) {
+            return;
+        }
+        int pos = l + (1 << layer) - 1;
+        auto curr = pair<int,int>{-1, 0};
+        for (int i = pos; i >= l; i -= 1) {
+            if (a[i] > curr.first) {
+                curr.first = a[i];
+                curr.second = 1;
+            } else {
+                curr.second += a[i] == curr.first;
+            }
+            val_and_cnt[layer][i] = curr;
+        }
+        curr = pair<int,int>{-1, 0};
+        for (int i = pos+1; i <= r; i += 1) {
+            if (a[i] > curr.first) {
+                curr.first = a[i];
+                curr.second = 1;
+            } else {
+                curr.second += a[i] == curr.first;
+            }
+            val_and_cnt[layer][i] = curr;
+        }
+        f(f, l, pos, layer-1);
+        f(f, pos+1, r, layer-1);
+    };
+    go(go, 0, n-1, 31-__builtin_clz(n)-1);
+//    for (const auto &x : val_and_cnt) {
+//        for (const auto &y : x) {
+//            cout << y.first << "," << y.second << " ";
+//        }cout << "\n";
+//    }
+    int q, x, y, z, t;
+    cin >> q >> x >> y >> z >> t;
+    int last = 0;
+    int tot = 1;
     for (int ii = 0; ii < q; ii += 1) {
-//        for (const auto &x : st) {
-//            cout << x << ": " << LL[x] << ": " << su(roots[x]) << ":D\n";
-//        }
-        int l, r;
-        cin >> l >> r;
-        l -= 1;
-        r -= 1;
-        LL[999'999] = l;
-        auto it1 = (--st.upper_bound(999'999));
-        int ordl = *it1;
-        LL[999'999] = r;
-        auto it2 = (--st.upper_bound(999'999));
-        int ordr = *it2;
-        if (ordl == ordr) {
-            cout << "YES\n";
-            continue;
+        int l = (last*x+y)%n_;
+        int r = (last*z+t)%n_;
+        if (l > r) {
+            swap(l, r);
         }
-        cout << "NO\n";
-        int ret = split(split, roots[ordl], l-LL[ordl]);
-        swap(ret, roots[ordl]);
-        int pref = split(split, roots[ordr], r-LL[ordr]+1);
-        merge(merge, ret, pref);
-        it1++;
-        while (it1 != it2) {
-            merge(merge, ret, roots[*it1]);
-            auto d = it1;
-            it1++;
-            st.erase(d);
-        }
-        if (l == LL[ordl]) {
-            st.erase(ordl); // если левый плохой то удалили
-        }
-        it2++;
-        if (r == n-1 || (it2 != st.end() && LL[*it2] == r+1)) {
-            // если правый плохой то удалили
-            st.erase(ordr);
+        if (l == r) {
+            last = 1;
         } else {
-            // иначе подвинули
-            st.erase(ordr);
-            LL[ordr] = r+1;
-            st.insert(ordr);
-        }
-        assert(su(ret) == r-l+1);
-        LL[C] = l;
-//        cout << C << "!!\n";
-        st.insert(C);
-        roots[C] = ret;
-        auto ml = st.lower_bound(C);
-        if (ml != st.begin()) {
-            ml--;
-            if (tree[roots[*ml]].mx <= tree[roots[C]].mn) {
-                merge(merge, roots[*ml], roots[C]);
-                st.erase(C);
+            int bt = 31-__builtin_clz(l^r);
+            auto one = val_and_cnt[bt][l];
+            auto another = val_and_cnt[bt][r];
+            if (one.first < another.first) {
+                swap(one, another);
+            }
+            if (one.first == another.first) {
+                last = one.second + another.second;
+            } else {
+                last = one.second;
             }
         }
-        auto it = st.upper_bound(C);
-        assert(it != st.begin());
-        it--;
-        auto mr = st.upper_bound(C);
-        if (mr != st.end()) {
-            if (tree[roots[*it]].mx <= tree[roots[*mr]].mn) {
-                merge(merge, roots[*it], roots[*mr]);
-                st.erase(*mr);
-            }
-        }
-        C += 1;
-    }
+        (tot *= last) %= MOD;
+    }cout << tot << "\n";
 }
+//1 2 2 2 | 1 2 1
