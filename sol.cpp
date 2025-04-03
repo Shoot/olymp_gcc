b219f2bd2b883a8f4c1bc65564df61e184f7e03c
Shoot <11522779+Shoot@users.noreply.github.com>
2025-04-03 11:25:53 +0300

    kth order stats (persistent ST), ЕЩЕ: Колво чисел на отрезке на отрезке (git add .) можно оффлайн персистентное до — запрос в деревьях r и в l-1 Дерево отвечает за префикс, дерево над массивом cnt[i].

--- sol.cpp
+++ sol.cpp
@@ -1,70 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
 signed main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
     int n;
     cin >> n;
-    struct Event {
-        bool tp;
-        int inc;
-        int32_t pos;
+    struct Node {
+        int val;
+        int l;
+        int r;
     };
-    vector<Event> events, events2;
-    events2.reserve(5e6);
-    events.reserve(3e6);
-    vector<int32_t> a(n);
+    vector<Node> nodes;
+    nodes.reserve(15'500'000);
+    auto nw = [&] (int val) -> int {
+        nodes.push_back({val, -1, -1});
+        return nodes.size()-1;
+    };
+    auto val = [&] (int u) -> int {
+        return u == -1 ? 0 : nodes[u].val;
+    };
+    auto recalc = [&] (int L, int R) -> int {
+        int u = nw(val(L)+val(R));
+        nodes[u].l = L;
+        nodes[u].r = R;
+        return u;
+    };
+    auto inc = [&] (auto f, int u, int tl, int tr, int pos) -> int {
+        int val = u == -1 ? 0 : nodes[u].val;
+        int L = u == -1 ? -1 : nodes[u].l;
+        int R = u == -1 ? -1 : nodes[u].r;
+        if (tl == tr) {
+            return nw(val+1);
+        }
+        int tm = (tl + tr) >> 1;
+        if (pos <= tm) {
+            return recalc(f(f, L, tl, tm, pos), R);
+        } else {
+            return recalc(L, f(f, R, tm+1, tr, pos));
+        }
+    };
+    auto getk = [&] (auto f, int ul, int ur, int tl, int tr, int k) -> int {
+        if (tl == tr) {
+            return tl;
+        }
+        int left_treesum = val(ur == -1?-1:nodes[ur].l) - val(ul == -1?-1:nodes[ul].l);
+        int tm = (tl + tr) >> 1;
+        if (left_treesum >= k) {
+            return f(f, ul == -1?-1:nodes[ul].l, ur == -1?-1:nodes[ur].l, tl, tm, k);
+        } else {
+            return f(f, ul == -1?-1:nodes[ul].r, ur == -1?-1:nodes[ur].r, tm+1, tr, k-left_treesum);
+        }
+    };
+    vector<int> trees(n+1);
+    trees[0] = -1;
+    vector<int> a(n);
     for (auto &x : a) {
         cin >> x;
     }
+    for (int i = 1; i <= n; i += 1) {
+        trees[i] = inc(inc, trees[i-1], 0, 1e9, a[i-1]);
+    }
+    int prevans = 0;
     int q;
     cin >> q;
-    for (int ii = 0; ii < q; ii += 1) {
-        int l, r, d;
-        cin >> l >> r >> d;
-        l -= 1;
-        r -= 1;
-        events.push_back({0, d, (int32_t)l});
-        events.push_back({0, -d, (int32_t)r+1});
-        events2.push_back({0, -(r-l+1)*d, (int32_t)r+1});
-//        events.push_back({0, (r-l+1)*d, (int32_t)r+1+1});
-    }
-    vector<int> nw(n);
-    for (int32_t i = 0; i < n; i += 1) {
-        events.push_back({1, 0, i});
-    }
-    sort(events.begin(), events.end(), [](const Event & one, const Event& other) {
-        return tie(one.pos, one.tp) < tie(other.pos, other.tp);
-    });
-    int curr = 0;
-    for (const auto &[tp, inc, pos] : events) {
-        if (tp == 0) {
-            curr += inc;
-        } else {
-            nw[pos] = curr;
-        }
-    }
-    events.clear();
-    for (int32_t i = 0; i < n; i += 1) {
-        events2.push_back({0, nw[i], i});
-    }
-    for (int32_t i = 0; i < n; i += 1) {
-        events2.push_back({1, 0, i});
-    }
-    for (int32_t i = 0; i < n; i += 1) {
-        events2.push_back({0, a[i], i});
-        events2.push_back({0, -a[i], i+1});
-    }
-    sort(events2.begin(), events2.end(), [](const Event & one, const Event& other) {
-        return tie(one.pos, one.tp) < tie(other.pos, other.tp);
-    });
-    curr = 0;
-    for (const auto &[tp, inc, pos] : events2) {
-        if (tp == 0) {
-            curr += inc;
-        } else {
-            cout << curr << " ";
-        }
-    }cout << "\n";
+    long long su = 0;
+    for (int qq = 0; qq < q; qq += 1) {
+        int LL, RR, KK;
+        cin >> LL >> RR >> KK;
+        int l = (LL + prevans) % n;
+        int r = (RR + prevans) % n;
+        int ans = getk(getk, trees[l], trees[r+1], 0, 1e9, KK);
+        su += ans;
+        prevans = ans;
+    }cout << su << "\n";
 }
