edf28a4f60bc225583a25b799e133cfbe581bdb2
Shoot <11522779+Shoot@users.noreply.github.com>
2025-05-01 16:46:34 +0300

    Triples eolymp

--- sol.cpp
+++ sol.cpp
@@ -12,56 +12,20 @@ signed main() {
     cin.tie(0);
 #endif
     int TT = 1;
-//    cin >> TT;
-    mt19937 mt(time(0));
+    cin >> TT;
     for (int tt = 0; tt < TT; tt += 1) {
-        int n;
-        cin >> n;
-        vector<pair<int,int>> pts;
-        if (n&1) {
-            pts.push_back({0, 0});
-            pts.push_back({n/2+1, 0});
-            pts.push_back({0, n-n/2+1});
-        } else if (n&3) {
-            pts.push_back({0, 1});
-            pts.push_back({1, 0});
-            pts.push_back({n/2+2, 0});
-            pts.push_back({n/2+1, 1});
+        int n, k;
+        cin >> n >> k;
+        int mini = 0;
+        int maxi = n;
+        while (n) {
+            mini += n % 3;
+            n /= 3;
+        }
+        if (mini <= k && k <= maxi && (k-mini)%2==0) {
+            cout << "Yes\n";
         } else {
-            pts.push_back({0, 1});
-            pts.push_back({1, 0});
-            pts.push_back({n/2+1, 0});
-            pts.push_back({n/2+1, 1});
-            pts.push_back({n/2+1+2, 1-2});
-        }
-        while (pts.size() < 5) {
-            int x, y;
-            while (true) {
-                x = mt() % (int)1e3;
-                y = mt() % (int)1e3;
-                bool good = true;
-                for (const auto &[ox, oy] : pts) {
-                    good &= __gcd(abs(ox-x), abs(oy-y)) == 1;
-                }
-//                cout << x << " " << y << good << "\n";
-                if (good) {
-                    break;
-                }
-            }
-            pts.push_back({x, y});
-        }
-        int cnt = 0;
-        for (const auto &[x, y] : pts) {
-            for (const auto &[ox, oy] : pts) {
-                if (x == ox && y == oy) continue;
-                cnt += __gcd(abs(ox-x), abs(oy-y)) - 1;
-            }
-        }
-        cnt /= 2;
-//        cout << cnt << "!\n";
-        assert(cnt == n);
-        for (const auto &[x, y] : pts) {
-            cout << x << " " << y << "\n";
+            cout << "No\n";
         }
     }
 }
