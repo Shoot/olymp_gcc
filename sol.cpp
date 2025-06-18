ee1315aca3df3d358b9da634f7724a01d69c0112
Shoot <11522779+Shoot@users.noreply.github.com>
2025-06-19 00:21:29 +0300

    запросы к стеку минимумов — gt_r с помощью стека, а за ним gt_l бинпоиском для стека на момент gt_r

--- sol.cpp
+++ sol.cpp
@@ -1,5 +1,7 @@
-#include <bits/stdc++.h>
+#include <bits/extc++.h>
 using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define int long long
 #define YES(x) cout << (x?"YES\n":"NO\n")
 #define NO(x) cout << (x?"NO\n":"YES\n")
@@ -9,7 +11,7 @@ using namespace std;
 #ifndef LO
 #pragma GCC optimize("Ofast,unroll-loops")
 #endif
-constexpr int MOD = (119<<23)+1;
+//constexpr int MOD = (119<<23)+1;
 //constexpr int MOD = 967276608647009887ll;
 //constexpr int MOD = 1e9+7;
 signed main() {
@@ -18,39 +20,70 @@ signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 #endif
-    int T = 1;
-    cin >> T;
+    int T=1;
+//    cin >> T;
     for (int tt = 0; tt < T; tt += 1) {
-        int p, s;
-        cin >> p >> s;
-        int g = __gcd(p, s);
-        p /= g, s /= g;
-        if (p%2) {
-            p *= 2, s *= 2;
+        int n;
+        cin >> n;
+        vector<int> a(n+1), a2(n+1);
+        for (int i = 1; i <= n; i += 1) {
+            cin >> a[i];
+            a2[i] = -a[i];
         }
-        bool done = false;
-        for (int S = s, P = p; S <= (int)5e4; S += s, P += p) {
-            // make rectangle with "eaten" part (not hole), p=const
-            int side1 = P/2/2, side2 = P/2/2+(P/2)%2;
-            // sides equal as possible for gt area variabiliy
-            if (S >= side1+side2-1 /* eaten as possible, looks like a 90deg corner */
-            && S <= side1*side2 /* not eaten at all */) {
-                /* start constructing from "90deg corner" to "not eaten at all" */
-                done = true;
-                cout << S << "\n";
-                for (int i = 0; i < side1; i += 1, S -= 1) {
-                    cout << i << " " << 0 << "\n";
+        vector<int> r_gt(n+1, n+1);
+        vector<int> st;
+        for (int i = n; i >= 1; i -= 1) {
+            while (!st.empty() && a[st.back()] <= a[i]) {
+                st.pop_back();
+            }
+            if (!st.empty()) {
+                r_gt[i] = st.back();
+            }
+            st.push_back(i);
+        }
+        vector<int> r_lt(n+1, n+1); // considering_only_values_after__r_gt
+        st.clear();
+        vector<vector<int>> qs(n+2);
+        for (int i = n; i >= 1; i -= 1) {
+            qs[r_gt[i]].push_back(i);
+        }
+        vector<int> st_vals;
+        for (int i = n; i >= 1; i -= 1) {
+            while (!st.empty() && a[st.back()] >= a[i]) {
+                st.pop_back();
+                st_vals.pop_back();
+            }
+            for (const auto &j : qs[i]) {
+                int it = upper_bound(st_vals.begin(), st_vals.end(), a[j])-st_vals.begin()-1;
+                if (it != -1) {
+                    r_lt[j] = st[it];
                 }
-                for (int i = 0; i < side1; i += 1) {
-                    for (int j = 1; j < side2 && S; j += 1, S -= 1) {
-                        cout << i << " " << j << "\n";
-                    }
-                }
-                break;
+            }
+            st.push_back(i);
+            st_vals.push_back(a[i]);
+        }
+        vector<vector<int>> max_r_lt(20, vector<int>(n+1));
+        vector<vector<int>> get_to(20, vector<int>(n+1));
+        max_r_lt[0] = r_lt;
+        get_to[0] = r_gt;
+        for (int i = 1; i < 20; i += 1) {
+            for (int j = 1; j <= n; j += 1) {
+                get_to[i][j] = (get_to[i-1][j]==n+1)?(n+1):get_to[i-1][get_to[i-1][j]];
+                max_r_lt[i][j]=(get_to[i-1][j]==n+1)?(n+1):max(max_r_lt[i-1][j], max_r_lt[i-1][get_to[i-1][j]]);
             }
         }
-        if (!done) {
-            cout << -1 << "\n";
+        int q;
+        cin >> q;
+        for (int ii = 0; ii < q; ii += 1) {
+            int l, r;
+            cin >> l >> r;
+            for (int bt = 19; bt >= 0; bt -= 1) {
+                if (max_r_lt[bt][l] <= r) {
+                    l = get_to[bt][l];
+                }
+            }
+            YES(max_r_lt[0][l] > r && get_to[0][l] <= r);
+            cout << endl;
         }
     }
 }
