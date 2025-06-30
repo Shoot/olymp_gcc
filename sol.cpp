c9d3e285682a3173c106103a0b1d5d4bce5eafc7
Shoot <11522779+Shoot@users.noreply.github.com>
2025-06-30 21:30:18 +0300

    persistent 2D segtree

--- sol.cpp
+++ sol.cpp
@@ -1,10 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     int n, q;
     cin >> n >> q;
+    int g1, g2;
+    cin >> g1 >> g2;
     struct Node {
         int val=0;
         int l=-1;
@@ -12,6 +15,7 @@ signed main() {
         int move_to_y; // -5 means that it is y node already
     };
     vector<Node> nodes;
+    nodes.reserve(6e7);
     auto l = [&] (int u) -> int {
         return (u == -1)?-1:nodes[u].l;
     };
@@ -61,35 +65,30 @@ signed main() {
         int goy = incy(incy, move_to_y(u), ytl, ytr, y, val);
         return mkx(gtval(L)+gtval(R), L, R, goy);
     };
-    auto gt_prev_val = [&] (auto f, int ux, int uy, int xtl, int xtr, int ytl, int ytr, int x) -> int {
-//        cout << x << " " << xtl << " " << xtr << " " << ytl << " " << ytr << " " << gtval(ux) << endl;
-        assert(ux != -1);
-        assert(uy != -1);
+    auto gt_prev_val = [&] (auto f, int u, int xtl, int xtr, int ytl, int ytr, int x) -> int {
         if (xtl == xtr) {
-            if (nodes[uy].move_to_y != -5) {
-                uy = nodes[uy].move_to_y;
+            if (nodes[u].move_to_y != -5) {
+                u = nodes[u].move_to_y;
             }
             if (ytl == ytr) {
                 return ytl;
             } else {
                 int tm = (ytl + ytr) >> 1;
-                if (gtval(l(uy))) {
-                    return f(f, ux, l(uy), xtl, xtr, ytl, tm, x);
+                if (gtval(l(u))) {
+                    return f(f, l(u), xtl, xtr, ytl, tm, x);
                 }
-                assert(gtval(r(uy)));
-                return f(f, ux, r(uy), xtl, xtr, tm+1, ytr, x);
+                return f(f, r(u), xtl, xtr, tm+1, ytr, x);
             }
         } else {
             int tm = (xtl + xtr) >> 1;
             if (x <= tm) {
-                return f(f, l(ux), l(ux), xtl, tm, ytl, ytr, x);
+                return f(f, l(u), xtl, tm, ytl, ytr, x);
             } else {
-                return f(f, r(ux), r(ux), tm+1, xtr, ytl, ytr, x);
+                return f(f, r(u), tm+1, xtr, ytl, ytr, x);
             }
         }
     };
     auto gt = [&] (auto f, int u, int xtl, int xtr, int ytl, int ytr, int xl, int xr, int yl, int yr) -> int {
-//        cout << xl << " " << xr << " " << xtl << " " << xtr << " " << ytl << " " << ytr << "\n";
         if (u == -1) {
             return 0ll;
         }
@@ -98,7 +97,6 @@ signed main() {
                 u = nodes[u].move_to_y;
             }
             if (ytl == yl && ytr == yr) {
-//                cout << xtl << " " << xtr << " " << ytl << " " << ytr << "!\n";
                 return gtval(u);
             } else {
                 int tm = (ytl + ytr) >> 1;
@@ -125,9 +123,13 @@ signed main() {
     };
     vector<int> roots(q+1, -1);
     for (int i = 1; i <= n; i += 1) {
-        roots[0] = incx(incx, roots[0], 1, n, -1e9, 1e9, i, 0, 1);
+        roots[0] = incx(incx, roots[0], 1, n, 0, min(n, 100), i, 0, 1);
     }
-//    cout << gt(gt, roots[0], 1, n, -1e9, 1e9, 1, 5, 0, 0) << "!!!!!!!\n";
+    int ra = 0;
+    auto gen = [&] () -> int {
+        (ra = g1*ra+g2) %= n;
+        return ra;
+    };
     for (int i = 1; i <= q; i += 1) {
         int tp, from;
         cin >> tp >> from;
@@ -135,14 +137,24 @@ signed main() {
         if (tp == 1) {
             int idx, val;
             cin >> idx >> val;
-            int prev_val = gt_prev_val(gt_prev_val, roots[i], roots[i], 1, n, -1e9, 1e9, idx);
-            roots[i] = incx(incx, roots[i], 1, n, -1e9, 1e9, i, prev_val, -1);
-            roots[i] = incx(incx, roots[i], 1, n, -1e9, 1e9, i, val, 1);
+            int prev_val = gt_prev_val(gt_prev_val, roots[i], 1, n, 0, min(n, 100), idx);
+            roots[i] = incx(incx, roots[i], 1, n, 0, min(n, 100), idx, prev_val, -1);
+            roots[i] = incx(incx, roots[i], 1, n, 0, min(n, 100), idx, val, 1);
         }
         if (tp == 2) {
-            int iL, iR, valL, valR;
-            cin >> iL >> iR >> valL >> valR;
-            cout << gt(gt, roots[i], 1, n, -1e9, 1e9, iL, iR, valL, valR) << "\n";
+            int valL, valR;
+            cin >> valL >> valR;
+            int iL = gen()+1;
+            int iR = gen()+1;
+            if (!(iL <= iR)) {
+                swap(iL, iR);
+            }
+            int ans = gt(gt, roots[i], 1, n, 0, min(n, 100), iL, iR, valL, valR);
+            cout << ans << "\n";
+            g1 += ans;
+            if (g1 >= n) {
+                g1 -= n;
+            }
         }
     }
 }
