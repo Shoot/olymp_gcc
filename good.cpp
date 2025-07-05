149a07e7478b06075acaffa9cf5f421074c4b4a3
Shoot <11522779+Shoot@users.noreply.github.com>
2025-07-05 22:05:01 +0300

    array-to-matchings -> matching-to-arrays https://www.youtube.com/watch?v=VbeMIgosAw4

--- good.cpp
+++ good.cpp
@@ -1,157 +1,63 @@
+//#include <bits/extc++.h>
 #include <bits/stdc++.h>
 using namespace std;
+//using namespace __gnu_pbds;
+//template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define int long long
+//#define double long double
+#define YES(x) cout << (x?"YES\n":"NO\n")
+#define ALICE(x) cout << (x?"Alice\n":"Bob\n")
+#define BOB(x) cout << (x?"Bob\n":"Alice\n")
+#define NO(x) cout << (x?"NO\n":"YES\n")
+#ifdef LO
+#pragma GCC optimize("trapv")
+#endif
+#ifndef LO
+#pragma GCC optimize("Ofast,unroll-loops")
+#endif
+//constexpr int MOD = (119<<23)+1;
+//constexpr int MOD = 967276608647009887ll;
+//constexpr int MOD = 1e9+7;
+constexpr int INF = 1e18;
 signed main() {
+#ifndef LO
+    clog << "FIO\n";
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, q;
-    cin >> n >> q;
-    int g1, g2;
-    cin >> g1 >> g2;
-    struct Node {
-        int val=0;
-        int l=-1;
-        int r=-1;
-        int move_to_y; // -5 means that it is y node already
-    };
-    vector<Node> nodes;
-    auto l = [&] (int u) -> int {
-        return (u == -1)?-1:nodes[u].l;
-    };
-    auto r = [&] (int u) -> int {
-        return (u == -1)?-1:nodes[u].r;
-    };
-    auto gtval = [&] (int u) -> int {
-        return (u == -1)?0:nodes[u].val;
-    };
-    auto mky = [&] (int val, int l, int r) -> int {
-        nodes.push_back({val, l, r, -5});
-        return nodes.size()-1;
-    };
-    auto mkx = [&] (int val, int l, int r, int move_to_y) -> int {
-        nodes.push_back({val, l, r, move_to_y});
-        return nodes.size()-1;
-    };
-    auto move_to_y = [&] (int u) -> int {
-        return (u==-1)?-1:nodes[u].move_to_y;
-    };
-    auto incy = [&] (auto f, int u, int ytl, int ytr, int y, int val) -> int {
-        if (ytl == ytr) {
-            return mky(gtval(u) + val, -1, -1);
-        } else {
-            int tm = (ytl + ytr) >> 1;
-            if (y <= tm) {
-                int nwl = f(f, l(u), ytl, tm, y, val);
-                return mky(gtval(nwl)+gtval(r(u)), nwl, r(u));
-            } else {
-                int nwr = f(f, r(u), tm+1, ytr, y, val);
-                return mky(gtval(l(u))+gtval(nwr), l(u), nwr);
-            }
-        }
-    };
-    auto incx = [&] (auto f, int u, int xtl, int xtr, int ytl, int ytr, int x, int y, int val) -> int {
-        if (xtl == xtr) {
-            int goy = incy(incy, move_to_y(u), ytl, ytr, y, val);
-            return mkx(gtval(goy), -1, -1, goy);
-        }
-        int tm = (xtl + xtr) >> 1;
-        int L = l(u), R = r(u);
-        if (x <= tm) {
-            L = f(f, l(u), xtl, tm, ytl, ytr, x, y, val);
-        } else {
-            R = f(f, r(u), tm+1, xtr, ytl, ytr, x, y, val);
-        }
-        int goy = incy(incy, move_to_y(u), ytl, ytr, y, val);
-        return mkx(gtval(L)+gtval(R), L, R, goy);
-    };
-    auto gt_prev_val = [&] (auto f, int ux, int uy, int xtl, int xtr, int ytl, int ytr, int x) -> int {
-        if (xtl == xtr) {
-            if (nodes[uy].move_to_y != -5) {
-                uy = nodes[uy].move_to_y;
-            }
-            if (ytl == ytr) {
-                return ytl;
-            } else {
-                int tm = (ytl + ytr) >> 1;
-                if (gtval(l(uy))) {
-                    return f(f, ux, l(uy), xtl, xtr, ytl, tm, x);
+#endif
+#ifdef LO
+    cout << unitbuf;
+#endif
+    int T;
+    cin >> T;
+    for (int tt = 0; tt < T; tt += 1) {
+        int n, MOD;
+        cin >> n >> MOD;
+        vector<int> cnt_free(n+1, 1);
+        vector<int> ans(n+1);
+        for (int i = 1; i <= n; i += 1) {
+            vector<int> nw(n+1, 0);
+            for (int use_here = 0; use_here <= i; use_here += 1) {
+//                if (i == 1) {
+//                    if (use_here) {
+//                        continue;
+//                    }
+//                }
+//                if (i == 2 && use_here > 1) {
+//                    continue;
+//                }
+//                cout << i << ": " << use_here << ": " << cnt_free[use_here]*(use_here==0?1:(use_here))%MOD << "\n";
+//                if (use_here) {
+                (ans[i] += cnt_free[use_here]*(use_here==0?1:(use_here))%MOD) %= MOD;
+//                }
+                for (int j = 0; j <= n; j += 1) {
+                    if (j != use_here || j == 0) {
+                        (nw[j] += cnt_free[use_here]*(use_here==0?1:(use_here))%MOD) %= MOD;
+                    }
                 }
-                return f(f, ux, r(uy), xtl, xtr, tm+1, ytr, x);
             }
-        } else {
-            int tm = (xtl + xtr) >> 1;
-            if (x <= tm) {
-                return f(f, l(ux), l(ux), xtl, tm, ytl, ytr, x);
-            } else {
-                return f(f, r(ux), r(ux), tm+1, xtr, ytl, ytr, x);
-            }
+            cnt_free = nw;
         }
-    };
-    auto gt = [&] (auto f, int u, int xtl, int xtr, int ytl, int ytr, int xl, int xr, int yl, int yr) -> int {
-        if (u == -1) {
-            return 0ll;
-        }
-        if (xtl == xl && xtr == xr) {
-            if (nodes[u].move_to_y != -5) {
-                u = nodes[u].move_to_y;
-            }
-            if (ytl == yl && ytr == yr) {
-                return gtval(u);
-            } else {
-                int tm = (ytl + ytr) >> 1;
-                int ret = 0;
-                if (yl <= tm) {
-                    ret += f(f, l(u), xtl, xtr, ytl, tm, xl, xr, yl, min(yr, tm));
-                }
-                if (yr >= tm+1) {
-                    ret += f(f, r(u), xtl, xtr, tm+1, ytr, xl, xr, max(yl, tm+1), yr);
-                }
-                return ret;
-            }
-        } else {
-            int tm = (xtl + xtr) >> 1;
-            int ret = 0;
-            if (xl <= tm) {
-                ret += f(f, l(u), xtl, tm, ytl, ytr, xl, min(xr, tm), yl, yr);
-            }
-            if (xr >= tm+1) {
-                ret += f(f, r(u), tm+1, xtr, ytl, ytr, max(xl, tm+1), xr, yl, yr);
-            }
-            return ret;
-        }
-    };
-    vector<int> roots(q+1, -1);
-    for (int i = 1; i <= n; i += 1) {
-        roots[0] = incx(incx, roots[0], 1, n, -1e9, 1e9, i, 0, 1);
+        cout << ans[n] << "\n";
     }
-    int ra = 0;
-    auto gen = [&] () -> int {
-        (ra = g1*ra+g2) %= n;
-        return ra;
-    };
-    for (int i = 1; i <= q; i += 1) {
-        int tp, from;
-        cin >> tp >> from;
-        roots[i] = roots[from];
-        if (tp == 1) {
-            int idx, val;
-            cin >> idx >> val;
-            int prev_val = gt_prev_val(gt_prev_val, roots[i], roots[i], 1, n, -1e9, 1e9, idx);
-            roots[i] = incx(incx, roots[i], 1, n, -1e9, 1e9, idx, prev_val, -1);
-            roots[i] = incx(incx, roots[i], 1, n, -1e9, 1e9, idx, val, 1);
-        }
-        if (tp == 2) {
-            int valL, valR;
-            cin >> valL >> valR;
-            int iL = gen()+1;
-            int iR = gen()+1;
-            if (!(iL <= iR)) {
-                swap(iL, iR);
-            }
-            g1 += gt(gt, roots[i], 1, n, -1e9, 1e9, iL, iR, valL, valR);
-            if (g1 >= n) {
-                g1 -= n;
-            }
-        }
-    }
-    cout << g1 << "\n";
 }
--- sol.cpp
+++ sol.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 //using namespace __gnu_pbds;
 //template <typename T> using ordered_set =  tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-#define int unsigned long long
+#define int long long
+//#define double long double
 #define YES(x) cout << (x?"YES\n":"NO\n")
 #define ALICE(x) cout << (x?"Alice\n":"Bob\n")
 #define BOB(x) cout << (x?"Bob\n":"Alice\n")
@@ -27,82 +28,52 @@ signed main() {
 #ifdef LO
     cout << unitbuf;
 #endif
-//    int T;
-//    cin >> T;
-    for (int ii = 0; ; ii += 1) {
-        string tp=(ii&1?"decode":"encode");
-//        cin >> tp;
-        auto swap = [&] (int x) -> int {
-            return ULLONG_MAX^x;
-        };
-        if (tp == "encode") {
-            ifstream cin("input.txt");
-            ofstream cout("my.ans");
-            int n;
-            cin >> n;
-            vector<int> a(n);
-            for (auto &x : a) {
-                cin >> x;
+    int T;
+    cin >> T;
+    for (int tt = 0; tt < T; tt += 1) {
+        int n, MOD;
+        cin >> n >> MOD;
+//        vector<int> cnt_free(n+1, 1);
+//        vector<int> ans(n+1);
+//        for (int i = 1; i <= n; i += 1) {
+//            vector<int> nw(n+1, 0);
+//            for (int use_here = 0; use_here <= i; use_here += 1) {
+////                if (i == 1) {
+////                    if (use_here) {
+////                        continue;
+////                    }
+////                }
+////                if (i == 2 && use_here > 1) {
+////                    continue;
+////                }
+////                cout << i << ": " << use_here << ": " << cnt_free[use_here]*(use_here==0?1:(use_here))%MOD << "\n";
+////                if (use_here) {
+//                    (ans[i] += cnt_free[use_here]*(use_here==0?1:(use_here))%MOD) %= MOD;
+////                }
+//                for (int j = 0; j <= n; j += 1) {
+//                    if (j > use_here || j == 0) {
+//                        (nw[j] += cnt_free[use_here]*(use_here==0?1:(use_here))%MOD) %= MOD;
+//                    }
+//                }
+//            }
+//            cnt_free = nw;
+//        }
+//        cout << ans[n] << "\n";
+        vector<vector<int>> dp(n+1, vector<int>(n+1));
+        dp[n][0] = 1;
+        int ans = 0;
+        // m_i — matching
+        // number of arrays is    П_{m_i != 0} m_i     =     a(m)
+        // we simply want \Sigma_{every possible m} a(m)
+        for (int cand = n; cand >= 1; cand -= 1) {
+            for (int occ = 0; occ <= n-cand; occ += 1) {
+                (dp[cand-1][occ] += dp[cand][occ]) %= MOD;
+                (dp[cand-1][occ+1] += dp[cand][occ]*(n+1-cand-occ)*cand) %= MOD;
             }
-            vector<int> b(16);
-            for (int i = 0; i < n; i += 1) {
-                if (max(a[i], swap(a[i])) == a[i]) {
-                    b[i/64] |= 1ull << i%64;
-                }
-            }
-            int y=0;
-            for (int i = 0; i < 16; i += 1) {
-                if (max(b[i], swap(b[i]))==b[i]) {
-                    y |= 1ull << i;
-                }
-            }
-            a.push_back(y);
-            for (const auto &x : b) {
-                a.push_back(x);
-            }
-            a.push_back(n);
-            cout << a.size() << "\n";
-            mt19937 mt(time(0));
-            uniform_int_distribution<int> go(0, 1);
-            for (auto &x : a) {
-                if (go(mt)) {
-                    x = swap(x);
-                }
-                cout << x << " ";
-            }cout << "\n";
         }
-        if (tp == "decode") {
-            ifstream cin("my.ans");
-            int n;
-            cin >> n;
-            vector<int> a(n);
-            for (auto &x : a) {
-                cin >> x;
-            }
-            n = a.back();
-            n = min(n, swap(n));
-            a.pop_back();
-            vector<int> b;
-            for (int i = 0; i < 16; i += 1) {
-                b.push_back(a.back());
-                a.pop_back();
-            }
-            map<int,signed> cnt;
-            int best = min(a[n], swap(a[n]));
-            for (int i = 0; i < 16; i += 1) { // 1 - maxi, 0 - mini
-                if (best&(1<<i)) {
-                    b[i] = max(b[i], swap(b[i]));
-                } else {
-                    b[i] = min(b[i], swap(b[i]));
-                }
-            }
-            for (int i = 0; i < n; i += 1) {
-                if (b[i/64]&(1ull<<(i%64))) {
-                    cout << max(a[i], swap(a[i])) << " ";
-                } else {
-                    cout << min(a[i], swap(a[i])) << " ";
-                }
-            }cout << "\n";
+        for (int occ = 0; occ <= n; occ += 1) {
+            (ans += dp[/*cand=*/0][occ]) %= MOD;
         }
+        cout << ans << "\n";
     }
 }
