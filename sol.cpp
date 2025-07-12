8ccfb1ef5788061d0f02151a662788943ea22f05
Shoot <11522779+Shoot@users.noreply.github.com>
2025-07-12 17:18:15 +0300

    second on path in log(deg), два центроида cf таска

--- sol.cpp
+++ sol.cpp
@@ -28,70 +28,84 @@ signed main() {
 #ifdef LO
     cout << unitbuf;
 #endif
-//    int T;
-//    cin >> T;
-//    for (int tt = 0; tt < T; tt += 1) {
-//    }
-    int n;
-    cin >> n;
-    vector<int> _2(n+1, 1);
-    for (int i = 1; i <= n; i += 1) {
-        _2[i] = _2[i-1]*2%MOD;
-    }
-    struct Node {
-        int val=0;
-        int subtree_sum=0;
-        int dp=0;
-        vector<int> ch;
-        Node() {
-            ch.assign(2, -1);
+    int T;
+    cin >> T;
+    for (int tt = 0; tt < T; tt += 1) {
+        int n;
+        cin >> n;
+        vector<vector<int>> adj(n+1);
+        vector<int> d(n+1);
+        vector<int> P(n+1);
+        d[1] = 1;
+        for (int i = 2; i <= n; i += 1) {
+            cin >> P[i];
+            d[i] = d[P[i]]+1;
+            adj[P[i]].push_back(i);
         }
-    };
-    vector<Node> nds;
-    nds.push_back({});
-    auto mknode = [&] () -> int {
-        nds.push_back({});
-        return nds.size()-1;
-    };
-    auto gtsz = [&] (int u) -> int {
-        return u == -1 ? 0 : nds[u].subtree_sum;
-    };
-    auto updsz = [&] (int u) -> void {
-        nds[u].subtree_sum = nds[u].val;
-        for (const auto &x : nds[u].ch) {
-            nds[u].subtree_sum += gtsz(x);
-        }
-    };
-    auto upd_dp = [&] (int u) -> void { // returns колво способов выбрать подмножество строк
-        // из поддерева в боре чтобы все ок было для всех infinity строк которые имеют u как префикс
-        int ret = 1;
-        for (const auto &x : nds[u].ch) { // в каждом из сыновей
-            (ret *= (x==-1?0:nds[x].dp)) %= MOD;
-        }
-//        ret += (1ll << (nds[u].subtree_sum-nds[u].val)) * ((1ll << nds[u].val)-1/*отняли тк пустое не подходит*/); // что-то в родителе
-        (ret += _2[nds[u].subtree_sum-nds[u].val] * (_2[nds[u].val]-1) % MOD) %= MOD;
-        nds[u].dp = ret;
-    };
-    for (int i = 0; i < n; i += 1) {
-        // dp[u] = П_{(v in ch[u])}dp[v]  +   (u != 0)*(2^(subtree_sz[u]-1))
-        string s;
-        cin >> s;
-        auto dfs = [&] (auto f, int u, int j) -> void {
-            if (j == s.size()) {
-                nds[u].val += 1;
-                updsz(u);
-                upd_dp(u);
-                return;
+        vector<int> tree(n);
+        auto add = [&] (int idx, int inc) -> void {
+            for (idx += 1; idx-1 < tree.size(); idx += idx&-idx) {
+                tree[idx-1] += inc;
             }
-            int& to = nds[u].ch[s[j]-'A'];
-            if (to == -1) {
-                to = mknode();
-            }
-            f(f, to, j+1);
-            updsz(u);
-            upd_dp(u);
         };
-        dfs(dfs, 0, 0);
-        cout << nds[0].dp << "\n";
+        auto gt = [&] (int idx) -> int {
+            int ret = 0;
+            for (idx += 1; idx > 0; idx -= idx&-idx) {
+                ret += tree[idx-1];
+            }
+            return ret;
+        };
+        vector<int> tin(n+1), tout(n+1);
+        int t = 0;
+        auto dfs = [&] (auto f, int u) -> void {
+            tin[u] = t++;
+            for (const auto &x : adj[u]) {
+                f(f, x);
+            }
+            tout[u] = t-1;
+        };
+        dfs(dfs, 1);
+        auto inc_v = [&] (int u) -> void {
+            add(tin[u], 1);
+        };
+        auto gt_v = [&] (int u) -> int {
+            return gt(tout[u]) - gt(tin[u]-1);
+        };
+
+        int c = 1, greatest_c_ch = 0;
+        inc_v(1);
+        for (int i = 2; i <= n; i += 1) {
+            inc_v(i);
+            if (tin[i] > tin[c] && tout[i] <= tout[c]) {
+                int l = 0, r = adj[c].size()-1;
+                int good = -1; // = second_on_path(c, /* -> */ i)
+                while (l <= r) {
+                    int mid = (l + r) >> 1;
+                    if (tout[adj[c][mid]] >= tout[i]) {
+                        good = mid;
+                        r = mid-1;
+                    } else {
+                        l = mid+1;
+                    }
+                }
+                assert(good != -1);
+                int u = adj[c][good];
+                if (gt_v(u) > i/2) {
+                    c = u;
+                    greatest_c_ch = i/2;
+                } else {
+                    greatest_c_ch = max(greatest_c_ch, gt_v(u));
+                }
+            } else {
+                int u = P[c];
+                if (i-gt_v(c) > i/2) {
+                    c = u;
+                    greatest_c_ch = i/2;
+                } else {
+                    greatest_c_ch = max(greatest_c_ch, i-gt_v(c));
+                }
+            }
+            cout << i-2*greatest_c_ch << " ";
+        }cout << "\n";
     }
 }
